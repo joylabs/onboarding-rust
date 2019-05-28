c0a95f3a54d11194ab2a9d467cefb1d937de6f0b
Dave Taylor <david.taylor@joylabs.com>
2019-05-28 17:40:32 -0600

    I've added the solution to week 1, problem 1

--- tests/week1/exercise1/mod.rs
+++ tests/week1/exercise1/mod.rs
@@ -1,6 +1,7 @@
-use onboarding_rust::week1::exercise1::hello_world;
+use onboarding_rust::week1::exercise1::exercise_one;
 
 #[test]
 fn test_week1_exercise1() {
-    assert_eq!(0, hello_world());
+
+    assert_eq!(vec![2, 4, 3, 1], exercise_one(vec![3, 1, 2, 4]));
 }
