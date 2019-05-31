356535b3e134eb84aaa1559e4beeb4b36b7cbc24
citlali9 <citlali.91094@gmail.com>
2019-05-31 10:29:58 -0500

    exercise1

--- src/week1/exercise1/mod.rs
+++ src/week1/exercise1/mod.rs
@@ -1,3 +1,14 @@
-pub fn hello_world() -> i64 {
-    0
+pub fn hello_world(v: Vec<i64>) -> Vec<i64> {
+    let mut x: Vec<i64> = Vec::new();
+    for &i in v.iter() {
+        if i % 2 == 0 {
+            x.push(i);
+        }
+    }
+    for &i in v.iter() {
+        if i % 2 != 0 {
+            x.push(i);
+        }
+    }
+    x
 }
