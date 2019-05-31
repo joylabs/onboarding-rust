822b2c65f2e4b5f41b31bdf126177409570bb3d1
citlali9 <citlali.91094@gmail.com>
2019-05-31 10:29:58 -0500

    exercise1

--- tests/week1/exercise1/mod.rs
+++ tests/week1/exercise1/mod.rs
@@ -2,5 +2,5 @@ use onboarding_rust::week1::exercise1::hello_world;
 
 #[test]
 fn test_week1_exercise1() {
-    assert_eq!(0, hello_world());
+    assert_eq!(vec![2, 4, 3, 1], hello_world(vec![3, 1, 2, 4]));
 }
