d9aa86c91ab7f3af827f633afed907e59a5e1d54
citlali9 <citlali.91094@gmail.com>
2019-06-03 18:58:12 -0500

    change the solution

--- src/week1/exercise5/mod.rs
+++ src/week1/exercise5/mod.rs
@@ -1,18 +1,11 @@
 pub fn exercise_5(n: i64) -> Vec<String> {
     let mut vector: Vec<String> = vec![];
     for i in 1..=n {
-        if i % 3 == 0 && i % 5 == 0 {
-            // println!("FizzBuzz");
-            vector.push("FizzBuzz".to_string());
-        } else if i % 3 == 0 {
-            // println!("Fizz");
-            vector.push("Fizz".to_string());
-        } else if i % 5 == 0 {
-            //println!("Buzz");
-            vector.push("Buzz".to_string());
-        } else {
-            //println!("{}", i);
-            vector.push(i.to_string());
+        match i {
+            i if i % 3 == 0 && i % 5 == 0 => vector.push("FizzBuzz".to_string()),
+            i if i % 3 == 0 => vector.push("Fizz".to_string()),
+            i if i % 5 == 0 => vector.push("Buzz".to_string()),
+            _ => vector.push(i.to_string()),
         }
     }
     vector
