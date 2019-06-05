3d4812393b99b0d21ccd2b9aebc69f340d7aa714
Nuria Reyes <nuriatonantzin@gmail.com>
2019-06-05 13:21:22 -0600

    Adding solution for exercise 1

--- tests/week1/exercise1/mod.rs
+++ tests/week1/exercise1/mod.rs
@@ -1,6 +1,6 @@
-use onboarding_rust::week1::exercise1::hello_world;
+use onboarding_rust::week1::exercise1::order_numbers;
 
 #[test]
 fn test_week1_exercise1() {
-    assert_eq!(0, hello_world());
+    assert_eq!(vec![2, 4, 3, 1], order_numbers(vec![3, 1, 2, 4]));
 }
