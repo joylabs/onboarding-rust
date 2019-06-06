9f996a73455a3b79ef33ac2e646b2bdd62de3b85
citlali9 <citlali.91094@gmail.com>
2019-06-06 07:21:50 -0500

    citlalli-e2

---
+++ src/week1/exercise2/mod.rs
@@ -0,0 +1,16 @@
+pub fn exercise2(input: Vec<Vec<i64>>) -> Vec<Vec<i64>> {
+
+    let mut result = vec![];
+    let rows = input.len();
+    let columns = input[0].len();
+
+    for a in 0..columns {
+      let mut new_row = vec![];
+      for b in 0..rows {
+          new_row.push(input[b][a]);
+     }
+     result.push(new_row);
+    }
+    result
+
+}
