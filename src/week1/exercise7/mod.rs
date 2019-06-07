d26f7de417f2383ad5139728ce85301b885fe7e0
citlali9 <citlali.91094@gmail.com>
2019-06-07 08:01:43 -0500

    change the solution

--- src/week1/exercise7/mod.rs
+++ src/week1/exercise7/mod.rs
@@ -1,5 +1,14 @@
 pub fn exercise7(mut v: Vec<i32>) -> Vec<i32> {
     let size = v.len() - 1;
-    v[size] = v.get(size).unwrap() + 1;
+    
+    let add_to_last_position = v.get(size).unwrap() + 1;
+
+    if add_to_last_position < 10 {
+        v[size] = v.get(size).unwrap() + 1;
+        return v;
+    }
+    let my_char: Vec<char> = add_to_last_position.to_string().chars().collect();
+    v[size] = my_char[0].to_digit(10).unwrap() as i32;
+    v.push(my_char[1].to_digit(10).unwrap() as i32);
     v
 }
--- tests/week1/exercise7/mod.rs
+++ tests/week1/exercise7/mod.rs
@@ -5,11 +5,19 @@ fn test_1_week1_exercise7() {
     let a = vec! [1, 2, 3];
     let b = vec! [1, 2, 4];
     assert_eq!(b, exercise7(a));
+
 }
 
 #[test]
 fn test_2_week1_exercise7() {
     let a = vec! [4,3,2,1];
     let b = vec! [4,3,2,2];
+    assert_eq!(b, exercise7(a));
+}
+
+#[test]
+fn test_3_week1_exercise7() {
+    let a = vec! [9];
+    let b = vec! [1,0];
     assert_eq!(b, exercise7(a));
 }
