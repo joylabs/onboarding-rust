b7594366a8d6622e7f4631d675b792ca6cf21276
Nuria Reyes <nuria.reyes@joylabs.com>
2019-06-10 16:53:29 -0600

    changing variable names to snake case

--- src/week1/exercise2/mod.rs
+++ src/week1/exercise2/mod.rs
@@ -4,11 +4,11 @@ pub fn matrix_transpose(input: Vec<Vec<i64>>) -> Vec<Vec<i64>> {
     let columns = input[0].len();
 
     for a in 0..columns {
-        let mut newRow = vec![];
+        let mut new_row = vec![];
         for b in 0..rows {
-            newRow.push(input[b][a]);
+            new_row.push(input[b][a]);
         }
-        result.push(newRow);
+        result.push(new_row);
     }
     result
 }
--- src/week1/exercise3/mod.rs
+++ src/week1/exercise3/mod.rs
@@ -1,15 +1,15 @@
 pub fn matrix_flip_inverse(input: Vec<Vec<i64>>) -> Vec<Vec<i64>> {
-    
+
     let mut result = vec![];
     let rows = input.len();
     let columns = input[0].len();
 
     for a in 0..rows {
-        let mut newRow = vec![];
-        for b in (0..columns).rev(){
-            newRow.push(1-input[a][b]);
+        let mut new_row = vec![];
+        for b in (0..columns).rev() {
+            new_row.push(1 - input[a][b]);
         }
-        result.push(newRow);
+        result.push(new_row);
     }
     result
 }
