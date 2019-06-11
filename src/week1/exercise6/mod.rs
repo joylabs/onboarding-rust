ef19d82317e321b56d00350b835501a4331272cb
Nuria Reyes <nuria.reyes@joylabs.com>
2019-06-11 09:34:31 -0600

    Starting to work on exercise 6

--- src/week1/exercise6/mod.rs
+++ src/week1/exercise6/mod.rs
@@ -1,5 +1,11 @@
 pub fn plus_one(input: Vec<i8>) -> Vec<i8> {
    let mut output: Vec<i8> = vec![];
+   let length = input.len() - 1;
+   let carry_on = true;
+
+   for i in length..=0 {
+      if carry_on {}
+   }
 
    output
 }
