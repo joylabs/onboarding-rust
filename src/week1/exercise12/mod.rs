897d336c4fe15d84ad926e2a5dd25b6f9a6337d5
Andres Morales Reyes <sharker@Andress-MacBook-Pro.local>
2019-06-12 15:32:48 -0600

    delete split_whitespace useless for loop

--- src/week1/exercise12/mod.rs
+++ src/week1/exercise12/mod.rs
@@ -2,19 +2,15 @@ pub fn palindrome(input: String) -> bool {
     let mut word = String::new();
     let mut reverse_word = String::new();
 
-
-    for i in input.split_whitespace() {
-        for j in i.chars() {
-            if j.is_alphanumeric() {
-                match j {
-                    'á' => reverse_word.push('a'),
-                    'é' => reverse_word.push('e'),
-                    'í' => reverse_word.push('i'),
-                    'ó' => reverse_word.push('o'),
-                    'ú' | 'ü' => reverse_word.push('u'),
-                    _ => reverse_word.push(j),
-                }
-
+    for j in input.chars() {
+        if j.is_alphanumeric() {
+            match j {
+                'á' => reverse_word.push('a'),
+                'é' => reverse_word.push('e'),
+                'í' => reverse_word.push('i'),
+                'ó' => reverse_word.push('o'),
+                'ú' | 'ü' => reverse_word.push('u'),
+                _ => reverse_word.push(j),
             }
         }
     }
