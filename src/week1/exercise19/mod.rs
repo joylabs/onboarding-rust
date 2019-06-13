00201bcfeb73b95b71c150ed407560870a5230c6
citlali9 <citlali.91094@gmail.com>
2019-06-13 10:59:29 -0500

    exercise19

---
+++ src/week1/exercise19/mod.rs
@@ -0,0 +1,3 @@
+pub fn exercise19(){
+    
+}
---
+++ tests/week1/exercise19/mod.rs
@@ -0,0 +1,7 @@
+use onboarding_rust::week1::exercise17::exercise17;
+
+#[test]
+fn test_1_week1_exercise19() {
+    let a = 19;
+    assert_eq!(true, exercise19(a));
+}
