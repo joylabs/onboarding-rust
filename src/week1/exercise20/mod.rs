e0761adbabdc88fcf6a4571be6aecc8bcda2ef14
Nuria Reyes <nuria.reyes@joylabs.com>
2019-06-13 23:07:03 -0600

    Adding first solution to exercise 20

--- src/week1/exercise20/mod.rs
+++ src/week1/exercise20/mod.rs
@@ -1,3 +1,14 @@
 pub fn is_anagram(input: Vec<&str>) -> bool {
-   false
+
+   if input[0].len() != input[1].len() {
+      return false;
+   }
+
+   let mut word_1: Vec<char> = input[0].chars().collect();
+   let mut word_2: Vec<char> = input[1].chars().collect();
+
+   word_1.sort();
+   word_2.sort();
+
+   word_1 == word_2
 }
