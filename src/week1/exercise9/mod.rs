6429b79fdf17e1a107d5aa8d5ae5f65700868c5f
citlali9 <citlali.91094@gmail.com>
2019-06-14 13:36:57 -0500

    use bitwise and

--- src/week1/exercise9/mod.rs
+++ src/week1/exercise9/mod.rs
@@ -1,8 +1,11 @@
 pub fn exercise9(i:i32)->bool{
-
-    let f :f32 = i as f32;
-    let f = f.log2();
-    let rest = f.floor() - f;
-
-    rest.abs()<1e-10
+    
+    if i< 1{
+        return false
+    }
+    //Bitwise AND &
+    if i & i-1 > 0{
+        return false
+    }
+    return true
 }
