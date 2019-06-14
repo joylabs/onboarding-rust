af76cb22cbb75bbb3e3da1c26487ef3cc4d9a3cb
citlali9 <citlali.91094@gmail.com>
2019-06-14 13:47:07 -0500

    clippy-error

--- src/week1/exercise9/mod.rs
+++ src/week1/exercise9/mod.rs
@@ -1,11 +1,11 @@
 pub fn exercise9(i:i32)->bool{
-    
+
     if i< 1{
         return false
     }
     //Bitwise AND &
-    if i & i-1 > 0{
+    if i & (i-1) > 0{
         return false
     }
-    return true
+    true
 }
