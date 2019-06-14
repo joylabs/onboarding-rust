8fc4ddc57e7e733db59ea79839e450bdf33c2f13
Andres Morales Reyes <sharker@Andress-MacBook-Pro.local>
2019-06-14 15:07:34 -0600

    reverse string ex9

--- tests/week1/exercise9/mod.rs
+++ tests/week1/exercise9/mod.rs
@@ -2,18 +2,16 @@ use onboarding_rust::week1::exercise9::reverse_string;
 
 #[test]
 fn test_week1_exercise9_example1() {
-    
-    let mut input = vec!["h","e","l","l","o"];
-    let expected = vec!["o","l","l","e","h"];
+    let mut input = vec!["h", "e", "l", "l", "o"];
+    let expected = vec!["o", "l", "l", "e", "h"];
     reverse_string(&mut input);
     assert_eq!(expected, input);
 }
 
 #[test]
 fn test_week1_exercise9_example2() {
-    
-    let mut input = vec!["H","a","n","n","a","h"];
-    let expected = vec!["h","a","n","n","a","H"];
+    let mut input = vec!["H", "a", "n", "n", "a", "h"];
+    let expected = vec!["h", "a", "n", "n", "a", "H"];
     reverse_string(&mut input);
     assert_eq!(expected, input);
 }
