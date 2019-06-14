39b18babc54cf11bfe4e5e5316fd0f2cc82a32cb
citlali9 <citlali.91094@gmail.com>
2019-06-14 11:44:23 -0500

    refactoring code

--- src/week1/exercise21/mod.rs
+++ src/week1/exercise21/mod.rs
@@ -1,48 +1,11 @@
-pub fn exercise21(s1: String, s2: String) -> bool {
+pub fn exercise21(s1: &str, s2: &str) -> bool {
+
     let mut s2: Vec<char> = s2.chars().collect();
-    for i in s1.chars() {
-        for j in 0..s2.len() {
-            if i == s2[j] {
-                s2.remove(j);
-                break;
-            }
-        }
-    }
-    s2.is_empty()
+    let mut s1: Vec<char> = s1.chars().collect();
+
+    s2.sort();
+    s1.sort();
+
+    s2 == s1
 }
 
-// pub fn is_anagram(input: Vec<&str>) -> bool {
-//     if input[0].len() != input[1].len() {
-//         return false;
-//     }
-//     let sum_1 = input[0].chars().fold(1, |mut acc, c| {
-//         acc *= c as i64;
-//         acc
-//     });
-
-//     let sum_2 = input[1].chars().fold(1, |mut acc, c| {
-//         acc *= c as i64;
-//         acc
-//     });
-
-//     sum_1 == sum_2
-// }
-
-// pub fn is_anagram_original(input: Vec<&str>) -> bool {
-//    let mut sum_1 = 1;
-//    let mut sum_2 = 1;
-
-//    if input[0].len() != input[1].len() {
-//       return false;
-//    }
-
-//    for w in input[0].chars() {
-//       sum_1 *= w as i32;
-//    }
-
-//    for w in input[1].chars() {
-//       sum_2 *= w as i32;
-//    }
-//    println!("sum1: {} sum2: {}", sum_1, sum_2);
-//    sum_1 == sum_2
-// }
--- tests/week1/exercise21/mod.rs
+++ tests/week1/exercise21/mod.rs
@@ -2,22 +2,22 @@ use onboarding_rust::week1::exercise21::exercise21;
 
 #[test]
 fn test_1_week1_exercise21() {
-    let a = "anagram".to_string();
-    let b = "nagaram".to_string();
+    let a = "anagram";
+    let b = "nagaram";
     assert_eq!(true, exercise21(a, b));
 }
 
 #[test]
 fn test_2_week1_exercise21() {
-    let a = "rat".to_string();
-    let b = "car".to_string();
+    let a = "rat";
+    let b = "car";
     assert_eq!(false, exercise21(a, b));
 }
 
 
 #[test]
 fn test3_week1_exercise21() {
-    let a = "conversationalists".to_string();
-    let b = "conservationalists".to_string();
+    let a = "conversationalists";
+    let b = "conservationalists";
     assert_eq!(true, exercise21(a, b));
 }
