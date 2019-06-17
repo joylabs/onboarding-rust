d7dcde483a3a4556de250004b595050e6eebdafe
citlali9 <citlali.91094@gmail.com>
2019-06-17 18:02:10 -0500

    refactoring the solution

--- src/week1/exercise19/mod.rs
+++ src/week1/exercise19/mod.rs
@@ -1,21 +1,22 @@
 use std::collections::HashSet;
 
 pub fn exercise19(x: i32) -> bool {
-    let mut x = x;
-    let mut set = HashSet::new();
+    let mut number = x;
+    let mut seen = HashSet::new();
 
-    loop {
-        let mut j = 0;
-        for i in x.to_string().chars() {
-            j += i.to_digit(10).unwrap().pow(2);
-        }
-        if j == 1 {
-            return true;
-        } else if set.contains(&j) {
-            return false;
-        } else {
-            set.insert(j);
-            x = j as i32;
-        }
+    while !seen.contains(&number){
+        println!("{}",x );
+        seen.insert(number);
+        number = is_happy(number);
+        if number == 1{return true} 
     }
+    false
+}
+
+fn is_happy(x:i32)->i32{
+    let mut squares_sum = 0;
+    for i in x.to_string().chars() {
+        squares_sum += i.to_digit(10).unwrap().pow(2);
+    }
+    squares_sum as i32
 }
--- tests/week1/exercise19/mod.rs
+++ tests/week1/exercise19/mod.rs
@@ -5,3 +5,15 @@ fn test_1_week1_exercise19() {
     let a = 19;
     assert_eq!(true, exercise19(a));
 }
+
+#[test]
+fn test_2_week1_exercise19() {
+    let a = 845;
+    assert_eq!(false, exercise19(a));
+}
+
+#[test]
+fn test_3_week1_exercise19() {
+    let a = 860;
+    assert_eq!(true, exercise19(a));
+}
