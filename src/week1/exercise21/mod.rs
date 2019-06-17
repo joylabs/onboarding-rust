0ba49e6632c3096aee2f17f17f7f114e72fba947
citlali9 <citlali.91094@gmail.com>
2019-06-16 22:50:48 -0500

    better

--- src/week1/exercise21/mod.rs
+++ src/week1/exercise21/mod.rs
@@ -1,11 +1,17 @@
 pub fn exercise21(s1: &str, s2: &str) -> bool {
+    if s1.len() != s2.len() {
+        return false;
+    }
 
-    let mut s2: Vec<char> = s2.chars().collect();
-    let mut s1: Vec<char> = s1.chars().collect();
+    let s2: Vec<char> = s2.chars().collect();
+    let s1: Vec<char> = s1.chars().collect();
 
-    s2.sort();
-    s1.sort();
+    let mut new_vector = vec![0; 256];
 
-    s2 == s1
+    for x in 0..s1.len() {
+        new_vector[s1[x] as usize] += 1;
+        new_vector[s2[x] as usize] -= 1;
+    }
+
+    !new_vector.into_iter().any(|x| x > 0)
 }
-
