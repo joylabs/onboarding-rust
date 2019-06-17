705a12c198318fbb3c7415c897372027464f584b
Nuria Reyes <nuria.reyes@joylabs.com>
2019-06-17 17:59:00 -0600

    Refacting code to implement the reverse vector function with a for loop

--- src/week1/exercise9/mod.rs
+++ src/week1/exercise9/mod.rs
@@ -1,3 +1,19 @@
-pub fn reverse_string(input: &mut Vec<char>) {
-   input.reverse()
+pub fn reverse_string(mut input: Vec<char>) -> Vec<char> {
+   let mut letter;
+   let mut last_index = input.len() - 1;
+
+   for i in 0..input.len() {
+      if last_index > i {
+         letter = input[i];
+         input[i] = input[last_index];
+         input[last_index] = letter;
+         last_index -= 1;
+      } else {
+         break;
+      }
+   }
+
+   println!("{:?}", input);
+
+   input
 }
--- tests/week1/exercise9/mod.rs
+++ tests/week1/exercise9/mod.rs
@@ -2,16 +2,14 @@ use onboarding_rust::week1::exercise9::reverse_string;
 
 #[test]
 fn test_week1_exercise9_example1() {
-    let mut input = vec!['h', 'e', 'l', 'l', 'o'];
+    let input = vec!['h', 'e', 'l', 'l', 'o'];
     let expected = vec!['o', 'l', 'l', 'e', 'h'];
-    reverse_string(&mut input);
-    assert_eq!(expected, input);
+    assert_eq!(expected, reverse_string(input));
 }
 
 #[test]
 fn test_week1_exercise9_example2() {
-    let mut input = vec!['H', 'a', 'n', 'n', 'a', 'h'];
+    let input = vec!['H', 'a', 'n', 'n', 'a', 'h'];
     let expected = vec!['h', 'a', 'n', 'n', 'a', 'H'];
-    reverse_string(&mut input);
-    assert_eq!(expected, input);
+    assert_eq!(expected, reverse_string(input));
 }
