84e9bbf169dc57daa9ba9ec37d00ada26b5f38d5
Nuria Reyes <nuria.reyes@joylabs.com>
2019-06-17 15:54:02 -0600

    Solution to exercise 13 worked with Andres

--- src/week1/exercise13/mod.rs
+++ src/week1/exercise13/mod.rs
@@ -1,3 +1,18 @@
-pub fn reverse_vowels(input: &str) -> &str {
-   input
+pub fn reverse_vowels(input: &str) -> String {
+   let mut vowels = input.chars().filter(|c| is_vowel(*c)).collect::<String>();
+
+   input.chars().map(|c|{
+      if is_vowel(c) {
+         vowels.pop().unwrap()
+      } else {
+         c
+      }
+   }).collect::<String>()
+}
+
+fn is_vowel(letter: char) -> bool {
+   match letter.to_ascii_lowercase() {
+      'a' | 'e' | 'i' | 'o' | 'u' => true,
+      _ => false,
+   }
 }
--- tests/week1/exercise13/mod.rs
+++ tests/week1/exercise13/mod.rs
@@ -3,13 +3,13 @@ use onboarding_rust::week1::exercise13::reverse_vowels;
 #[test]
 fn test_week1_exercise10_example1() {
     let input = "hello";
-    let expected = "holle";
+    let expected = "holle".to_string();
     assert_eq!(expected, reverse_vowels(input));
 }
 
 #[test]
 fn test_week1_exercise10_example2() {
     let input = "leetcode";
-    let expected = "leotcede";
+    let expected = "leotcede".to_string();
     assert_eq!(expected, reverse_vowels(input));
 }
