f5271a555d614fc8959e7c0ae8b90c3b388f9c3c
citlali9 <citlali.91094@gmail.com>
2019-06-16 20:09:06 -0500

    add break

--- src/week1/exercise20/mod.rs
+++ src/week1/exercise20/mod.rs
@@ -1,11 +1,23 @@
-pub fn exercise20(v: Vec<i32>, x:i32) -> Vec<i32> {
+pub fn exercise20(v: Vec<i32>, x: i32) -> Vec<i32> {
     let vv = v.len();
     for i in 0..vv {
         for j in 0..vv {
-            if x == v[j] + v[i] {
+            if x == v[j] + v[i] && j != i {
                 return vec![i as i32, j as i32];
             }
         }
     }
     vec![0, 0]
 }
+
+// fn ne_w(v: Vec<i32>, x: i32){
+
+//     v.into_iter().enumerate().filter_map(|(i,j)|
+//     { v.into_iter().enumerate().map(|(l,k)|
+//     {if i !=l &&  j+k == x{
+//         Some((i,l))
+//      }else{
+//          None
+//      }}.find_map(|o| o) } );
+
+// }
