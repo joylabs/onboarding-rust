8a5d5f66cb8732d25ca7ee997143e519993c98cd
Andres Morales Reyes <sharker@Andress-MacBook-Pro.local>
2019-06-17 13:15:26 -0600

    refactor is_div function

--- src/week1/exercise4/mod.rs
+++ src/week1/exercise4/mod.rs
@@ -2,26 +2,30 @@ pub fn self_dividing_numbers(input: Vec<i32>) -> Vec<i32> {
     let mut output: Vec<i32> = vec![];
 
     for x in input[0]..=input[1] {
-        let mut num = x;
-        let mut is_div = true;
 
-        while is_div && num > 0 {
-            let num_module = num % 10;
-
-            match num_module {
-                0 => {
-                    is_div = false;
-                    break;
-                }
-                a if { x % a == 0 } => is_div = true,
-                _ => is_div = false,
-            }
-            num /= 10;
-        }
-
-        if is_div {
+        if is_div(x) {
             output.push(x);
         }
     }
     output
+}
+
+fn is_div(i: i32) -> bool {
+    let mut num = i;
+    let mut is_div = true;
+
+    while is_div && num > 0 {
+        let num_module = num % 10;
+
+        match num_module {
+            0 => {
+                is_div = false;
+                break;
+            }
+            a if { i % a == 0 } => is_div = true,
+            _ => is_div = false,
+        }
+        num /= 10;
+    }
+    is_div
 }
