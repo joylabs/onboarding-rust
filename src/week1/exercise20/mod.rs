90d6fde435265fdd0d92e7014707aa7d53df1346
citlali9 <citlali.91094@gmail.com>
2019-06-18 11:15:51 -0500

    changed to get entry without an extra variable

--- src/week1/exercise20/mod.rs
+++ src/week1/exercise20/mod.rs
@@ -2,7 +2,7 @@
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 
-pub fn exercise20(v: Vec<i32>, x: i32) -> Vec<i32> {
+pub fn exercise20(v: Vec<i32>, x: i32) -> Vec<usize> {
 
     let mut key = HashMap::new();
     for (i, item) in v.iter().enumerate() {
@@ -12,9 +12,8 @@ pub fn exercise20(v: Vec<i32>, x: i32) -> Vec<i32> {
     for (j, item) in v.iter().enumerate() {
         let minus = x - item;
         if let Entry::Occupied(entry) = key.entry(minus) {
-            let x = *entry.get() as i32;
-            if x != j as i32 {
-                return vec![j as i32, x];
+            if *entry.get() != j {
+                return vec![j, *entry.get()];
             }
         }
     }
