183ddace4cc11effee216cd5274438d91c765089
Andres Morales Reyes <sharker@Andress-MacBook-Pro.local>
2019-06-18 11:22:36 -0600

    exercise 14 common prefix ready

--- src/week1/exercise14/mod.rs
+++ src/week1/exercise14/mod.rs
@@ -1,1 +1,18 @@
+pub fn longest_common_prefix(mut input: Vec<&str>) -> String {
 
+        let mut flag = true;
+        input.sort();
+        if input.is_empty() {
+                return "".to_string();
+        }
+        input[0].chars()
+                .zip(input.last().unwrap().chars())
+                .filter(|(x, y)| {
+                        if x != y {
+                                flag = false;
+                        }
+                        x == y && flag
+                })
+                .map(|(x, _)| x)
+                .collect::<String>()
+}
--- tests/week1/exercise14/mod.rs
+++ tests/week1/exercise14/mod.rs
@@ -1,1 +1,25 @@
+use onboarding_rust::week1::exercise14::longest_common_prefix;
 
+#[test]
+fn test_week1_exercise14_example1() {
+    let input = vec!["flower", "flow", "flight"];
+    let expected = "fl";
+
+    assert_eq!(expected, longest_common_prefix(input));
+}
+
+
+#[test]
+fn test_week1_exercise14_example2() {
+    let input = vec!["dog", "racecar", "car"];
+    let expected = "";
+
+    assert_eq!(expected, longest_common_prefix(input));
+}
+#[test]
+fn test_week1_exercise14_example3() {
+    let input = vec![];
+    let expected = "";
+
+    assert_eq!(expected, longest_common_prefix(input));
+}
