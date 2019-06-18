4eec4cdb5346632e30724cfd33f8a17ece47cad0
Nuria Reyes <nuria.reyes@joylabs.com>
2019-06-18 17:06:59 -0600

    Adding solution to exercise 15

--- src/week1/exercise15/mod.rs
+++ src/week1/exercise15/mod.rs
@@ -1,3 +1,9 @@
 pub fn get_complement_number(input: i32) -> i32 {
-   input
+   if input == 0 {
+      1
+   } else if input > 0 {
+      input >> 1
+   } else {
+      !input
+   }
 }
--- tests/week1/exercise15/mod.rs
+++ tests/week1/exercise15/mod.rs
@@ -13,3 +13,17 @@ fn test_week1_exercise15_example2() {
     let expected = 0;
     assert_eq!(expected, get_complement_number(input));
 }
+
+#[test]
+fn test_week1_exercise15_example3() {
+    let input = 0;
+    let expected = 1;
+    assert_eq!(expected, get_complement_number(input));
+}
+
+#[test]
+fn test_week1_exercise15_example4() {
+    let input = -1;
+    let expected = 0;
+    assert_eq!(expected, get_complement_number(input));
+}
