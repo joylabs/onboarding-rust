d8d2abdd62f40255411489b12b140e28f3d30d68
Nuria Reyes <nuria.reyes@joylabs.com>
2019-06-18 15:40:23 -0600

    Adding files to work on exercise 15

---
+++ src/week1/exercise15/mod.rs
@@ -0,0 +1,3 @@
+pub fn get_complement_number(input: i32) -> i32 {
+   input
+}
---
+++ tests/week1/exercise15/mod.rs
@@ -0,0 +1,15 @@
+use onboarding_rust::week1::exercise15::get_complement_number;
+
+#[test]
+fn test_week1_exercise15_example1() {
+    let input = 5;
+    let expected = 2;
+    assert_eq!(expected, get_complement_number(input));
+}
+
+#[test]
+fn test_week1_exercise15_example2() {
+    let input = 1;
+    let expected = 0;
+    assert_eq!(expected, get_complement_number(input));
+}
