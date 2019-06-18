2f11a29fd03c534722b773d1bfdcc48b19d9aced
Nuria Reyes <nuria.reyes@joylabs.com>
2019-06-18 13:55:14 -0600

    Adding solution to  exercise 14

--- src/week1/exercise14/mod.rs
+++ src/week1/exercise14/mod.rs
@@ -1,3 +1,30 @@
-pub fn find_common_prefix(input: Vec<&str>) -> &str {
-   ""
+pub fn find_common_prefix(mut input: Vec<&str>) -> String {
+   if input.is_empty() {
+      return "".to_string();
+   }
+
+   input.sort();
+
+   let first_item = input[0].chars().collect::<Vec<char>>();
+   let last_item = input[input.len() - 1].chars().collect::<Vec<char>>();
+
+   get_common_prefix(first_item, last_item)
+}
+
+fn get_common_prefix(first_item: Vec<char>, last_item: Vec<char>) -> String {
+   let mut is_different = false;
+
+   first_item
+      .iter()
+      .enumerate()
+      .fold("".to_string(), |mut acc, (i, c)| {
+         if !is_different {
+            if *c == last_item[i] {
+               acc.push_str(&c.to_string());
+            } else {
+               is_different = true;
+            }
+         }
+         acc
+      })
 }
--- tests/week1/exercise14/mod.rs
+++ tests/week1/exercise14/mod.rs
@@ -1,15 +1,22 @@
 use onboarding_rust::week1::exercise14::find_common_prefix;
 
 #[test]
-fn test_week1_exercise10_example1() {
-    let input = vec!["flower","flow","flight"];
-    let expected = "fl";
+fn test_week1_exercise14_example1() {
+    let input = vec!["flower", "flow", "flight"];
+    let expected = "fl".to_string();
     assert_eq!(expected, find_common_prefix(input));
 }
 
 #[test]
-fn test_week1_exercise10_example2() {
-    let input = vec!["dog","racecar","car"];
-    let expected = "";
+fn test_week1_exercise14_example2() {
+    let input = vec!["dog", "racecar", "car"];
+    let expected = "".to_string();
+    assert_eq!(expected, find_common_prefix(input));
+}
+
+#[test]
+fn test_week1_exercise14_example3() {
+    let input: Vec<&str> = vec![];
+    let expected = "".to_string();
     assert_eq!(expected, find_common_prefix(input));
 }
