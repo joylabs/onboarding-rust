f2111dc55d5d8e2612ce4f4a9cb39c489dc41300
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-06-19 08:04:40 -0500

    add map instead of fold

--- src/week1/exercise19/mod.rs
+++ src/week1/exercise19/mod.rs
@@ -4,16 +4,19 @@ pub fn exercise19(x: i32) -> bool {
     let mut number = x;
     let mut seen = HashSet::new();
 
-    while !seen.contains(&number){
+    while !seen.contains(&number) {
         seen.insert(number);
         number = is_happy(number);
-        if number == 1{return true} 
+        if number == 1 {
+            return true;
+        }
     }
     false
 }
 
-fn is_happy(x:i32)->i32{
-    
-    x.to_string().chars().fold(0,|mut acc, character|{
-         acc += character.to_digit(10).unwrap().pow(2) as i32; acc})
+fn is_happy(x: i32) -> i32 {
+    x.to_string()
+        .chars()
+        .map(|character| character.to_digit(10).unwrap().pow(2) as i32)
+        .sum()
 }
