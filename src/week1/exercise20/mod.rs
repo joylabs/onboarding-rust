07fa565b0e65cc0f2749e0926a3c19c9ff4812c2
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-06-19 07:53:23 -0500

    using better solution with map

--- src/week1/exercise20/mod.rs
+++ src/week1/exercise20/mod.rs
@@ -1,22 +1,17 @@
 
-use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 
-pub fn exercise20(v: Vec<i32>, x: i32) -> Vec<usize> {
+pub fn exercise20(v: Vec<i32>, x: i32) -> Vec<i32> {
+  let key: HashMap<i32, i32> = v
+    .iter()
+    .enumerate()
+    .map(|(i, item)| (*item, i as i32))
+    .collect();
 
-    let mut key = HashMap::new();
-    for (i, item) in v.iter().enumerate() {
-        key.insert(*item, i);
-    }
-
-    for (j, item) in v.iter().enumerate() {
-        let minus = x - item;
-        if let Entry::Occupied(entry) = key.entry(minus) {
-            if *entry.get() != j {
-                return vec![j, *entry.get()];
-            }
-        }
-    }
-    vec![0, 0]
+  v.iter()
+    .enumerate()
+    .find(|(i, item)| key.get(&(x - *item)).map_or(false, |j| *i as i32 != *j))
+    .map(|(i, item)| vec![i as i32, key[&(x - item)]])
+    .unwrap()
 }
 
--- tests/week1/exercise20/mod.rs
+++ tests/week1/exercise20/mod.rs
@@ -3,8 +3,8 @@ use onboarding_rust::week1::exercise20::exercise20;
 
 #[test]
 fn test_1_week1_exercise20() {
-    let a = vec![2, 7, 11, 15];
-    let b = vec![0, 1];
+    let a = vec![2, 11, 7, 15];
+    let b = vec![0, 2];
     let c = 9;
     assert_eq!(b, exercise20(a, c));
 }
