c4bed29a96bfac224c8a0fe0a37d4f291fc0c3aa
Andres Morales Reyes <sharker@Andress-MacBook-Pro.local>
2019-06-20 17:41:44 -0600

    other option ready :D

--- tests/week1/exercise14/mod.rs
+++ tests/week1/exercise14/mod.rs
@@ -23,3 +23,72 @@ fn test_week1_exercise14_example3() {
 
     assert_eq!(expected, longest_common_prefix(input));
 }
+#[test]
+fn test_week1_exercise14_example4() {
+    let input = vec![""];
+    let expected = "";
+
+    assert_eq!(expected, longest_common_prefix(input));
+}
+
+#[test]
+fn test_week1_exercise14_example5() {
+    let input = vec!["aa", "ab"];
+    let expected = "a";
+
+    assert_eq!(expected, longest_common_prefix(input));
+}
+
+#[test]
+fn test_week1_exercise14_example6() {
+    let input = vec!["a"];
+    let expected = "a";
+
+    assert_eq!(expected, longest_common_prefix(input));
+}
+#[test]
+fn test_week1_exercise14_example7() {
+    let input = vec!["aa"];
+    let expected = "aa";
+
+    assert_eq!(expected, longest_common_prefix(input));
+}
+
+#[test]
+fn test_week1_exercise14_example8() {
+    let input = vec!["c", "c"];
+    let expected = "c";
+
+    assert_eq!(expected, longest_common_prefix(input));
+}
+#[test]
+fn test_week1_exercise14_example9() {
+    let input = vec!["", ""];
+    let expected = "";
+
+    assert_eq!(expected, longest_common_prefix(input));
+}
+
+#[test]
+fn test_week1_exercise14_example10() {
+    let input = vec!["a", "a", "a"];
+    let expected = "a";
+
+    assert_eq!(expected, longest_common_prefix(input));
+}
+
+#[test]
+fn test_week1_exercise14_example11() {
+    let input = vec!["aa", "a"];
+    let expected = "a";
+
+    assert_eq!(expected, longest_common_prefix(input));
+}
+
+#[test]
+fn test_week1_exercise14_example12() {
+    let input = vec!["aaba", "aaa", "aa", "aa", "aa"];
+    let expected = "aa";
+
+    assert_eq!(expected, longest_common_prefix(input));
+}
