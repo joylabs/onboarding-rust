9696b79397c7ce04a8f2bc04036ac31dcce61a80
Miguel Bovadilla <miguel.bovadilla@joylabs.com>
2019-06-21 12:45:45 -0500

    First Commit: Exercise 1 - Sort Array by Parity

--- src/week1/exercise1/mod.rs
+++ src/week1/exercise1/mod.rs
@@ -1,3 +1,14 @@
-pub fn hello_world() -> i64 {
-    0
+pub fn array_by_parity(a: Vec<i32>) -> Vec<i32> {
+    let mut evens: Vec<i32> = vec![];
+    let mut odds: Vec<i32> = vec![];
+    for n in &a {
+        if n % 2 == 0 {
+            println!("Even: {}", n);
+            evens.push(*n);
+        } else {
+            println!("Odd: {}", n);
+            odds.push(*n);
+        }
+    }
+    [&evens[..], &odds[..]].concat()
 }
