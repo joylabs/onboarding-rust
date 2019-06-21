fc7e14c4ac6cb8fb5d2ce21fb9df8b575eab16d4
Nuria Reyes <nuria.reyes@joylabs.com>
2019-06-21 17:11:40 -0600

    Adding first solution to exercise 16

--- src/week1/exercise16/mod.rs
+++ src/week1/exercise16/mod.rs
@@ -1,3 +1,11 @@
 pub fn get_hamming_distance(input: (u32, u32)) -> u32 {
-   input.0
+   let num_difference = input.0 ^ input.1;
+   let num_string = format!("{:b}", num_difference);
+
+   num_string.chars().fold(0, |mut acc, n| {
+      if n == '1' {
+         acc += 1;
+      }
+      acc
+   })
 }
