49f04d466c8093502870f1be78e0f89f27473b2c
Miguel Bovadilla <miguel.bovadilla@joylabs.com>
2019-06-25 15:47:07 -0500

    Functional Approach using map()

--- src/week1/exercise11/mod.rs
+++ src/week1/exercise11/mod.rs
@@ -1,18 +1,6 @@
 pub fn reverse_words(s: String) -> String {
-    let v: Vec<&str> = s.split(' ').collect();
-    let mut output: String = String::from("");
-    // Iterate over new vector
-    for (i, word) in v.iter().enumerate() {
-        // String (to chars) to Vector
-        let mut current_word: Vec<char> = word.chars().collect();
-        // Reverse current word
-        current_word.reverse();
-        let reversed_string: String = current_word.into_iter().collect();
-        output += &reversed_string;
-        // Check if last word
-        if i < v.len() - 1 {
-            output += " ";
-        }
-    }
-    output
+    s.split_whitespace()
+        .map(|word| word.chars().rev().collect::<String>())
+        .collect::<Vec<_>>()
+        .join(" ")
 }
