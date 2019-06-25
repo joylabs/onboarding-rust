2eab39be2625a30a5642a22b8549064104dafb1e
Miguel Bovadilla <miguel.bovadilla@joylabs.com>
2019-06-25 15:28:02 -0500

    Functional approach using map()

--- src/week1/exercise5/mod.rs
+++ src/week1/exercise5/mod.rs
@@ -1,15 +1,14 @@
 pub fn fizz_buzz(n: i32) -> Vec<String> {
-    let mut output: Vec<String> = vec![];
-    for number in 1..=n {
-        if number % 3 == 0 && number % 5 == 0 {
-            output.push(String::from("FizzBuzz"));
-        } else if number % 3 == 0 {
-            output.push(String::from("Fizz"));
-        } else if number % 5 == 0 {
-            output.push(String::from("Buzz"));
-        } else {
-            output.push(number.to_string());
-        }
-    }
-    output
+    (1..=n)
+        .map(|number| {
+            if number % 3 == 0 && number % 5 == 0 {
+                return "FizzBuzz".to_owned();
+            } else if number % 3 == 0 {
+                return "Fizz".to_owned();
+            } else if number % 5 == 0 {
+                return "Buzz".to_owned();
+            }
+            number.to_string()
+        })
+        .collect()
 }
