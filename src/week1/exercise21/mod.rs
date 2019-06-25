3a556fb22d43fbeeb8f33a7ed6f11a645f40a876
Nuria Reyes <nuria.reyes@joylabs.com>
2019-06-25 12:29:55 -0600

    Adding changes to refactor code for exercise 21 made with Citlalli

--- src/week1/exercise21/mod.rs
+++ src/week1/exercise21/mod.rs
@@ -28,16 +28,17 @@ pub fn is_friend_circle(input: Vec<Vec<i32>>) -> i32 {
    input
       .into_iter()
       .enumerate()
-      .fold(matrix_order, |mut friend_count, (i, row)| {
-         friend_count = get_one_count(i, row, &mut friend_helper);
-         friend_count
-      })
+      .for_each(|(i, row)| {
+         get_one_count(i, row, &mut friend_helper);
+      });
+
+   friend_helper.circle_count
 }
 
-fn get_one_count(i: usize, row: Vec<i32>, friend_helper: &mut FriendHelper) -> i32 {
+fn get_one_count(i: usize, row: Vec<i32>, friend_helper: &mut FriendHelper) {
    row.into_iter()
       .enumerate()
-      .fold(friend_helper.circle_count, |mut counter, (j, element)| {
+      .for_each(|(j, element)| {
          println!("element {}", element);
 
          let a = friend_helper.find(i);
@@ -46,9 +47,6 @@ fn get_one_count(i: usize, row: Vec<i32>, friend_helper: &mut FriendHelper) -> i
          if element == 1 && a != b {
             friend_helper.index_vector[a] = b as i32;
             friend_helper.circle_count -= 1;
-            counter -= 1;
          }
-
-         counter
-      })
+      });
 }
