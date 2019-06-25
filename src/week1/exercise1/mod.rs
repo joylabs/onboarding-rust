9c2e88b412ac233bedb2550e22ef6f289a9ee196
Miguel Bovadilla <miguel.bovadilla@joylabs.com>
2019-06-25 12:54:44 -0500

    Unnecessary printlns deleted

--- src/week1/exercise1/mod.rs
+++ src/week1/exercise1/mod.rs
@@ -3,10 +3,8 @@ pub fn array_by_parity(a: Vec<i32>) -> Vec<i32> {
     let mut odds: Vec<i32> = vec![];
     for n in &a {
         if n % 2 == 0 {
-            println!("Even: {}", n);
             evens.push(*n);
         } else {
-            println!("Odd: {}", n);
             odds.push(*n);
         }
     }
