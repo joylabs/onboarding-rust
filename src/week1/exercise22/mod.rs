e55d99dee30a5890667ebf92e9399c3ee32aa6a5
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-06-26 13:47:56 -0500

    update to software engineering feedback-3

--- src/week1/exercise22/mod.rs
+++ src/week1/exercise22/mod.rs
@@ -1,11 +1,11 @@
-struct FriendCircle {
+struct IndirectFriendCircle {
     index_vector: Vec<i32>,
     circle_count: i32,
 }
 
-impl FriendCircle {
-    fn new(matrix_order: i32) -> FriendCircle {
-        FriendCircle {
+impl IndirectFriendCircle {
+    fn new(matrix_order: i32) -> IndirectFriendCircle {
+        IndirectFriendCircle {
             index_vector: (0..matrix_order).map(|x| x).collect(),
             circle_count: matrix_order,
         }
@@ -23,25 +23,25 @@ impl FriendCircle {
 
 pub fn count_friend_circle(input: Vec<Vec<i32>>) -> i32 {
     let matrix_order = input.len() as i32;
-    let mut friend_circle = FriendCircle::new(matrix_order);
+    let mut indirect_friend_circle = IndirectFriendCircle::new(matrix_order);
 
     input.into_iter().enumerate().for_each(|(i, row)| {
-        get_one_count(i, row, &mut friend_circle);
+        get_one_count(i, row, &mut indirect_friend_circle);
     });
 
-    friend_circle.circle_count
+    indirect_friend_circle.circle_count
 }
 
-fn get_one_count(i: usize, row: Vec<i32>, friend_circle: &mut FriendCircle) {
+fn get_one_count(i: usize, row: Vec<i32>, indirect_friend_circle: &mut IndirectFriendCircle) {
     row.into_iter().enumerate().for_each(|(j, element)| {
         println!("element {}", element);
 
-        let a = friend_circle.find(i);
-        let b = friend_circle.find(j);
+        let a = indirect_friend_circle.find(i);
+        let b = indirect_friend_circle.find(j);
 
         if element == 1 && a != b {
-            friend_circle.index_vector[a] = b as i32;
-            friend_circle.circle_count -= 1;
+            indirect_friend_circle.index_vector[a] = b as i32;
+            indirect_friend_circle.circle_count -= 1;
         }
     });
 }
