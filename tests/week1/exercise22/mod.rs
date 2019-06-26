39a19f56118d98cb6d84f2ad8ce0d4ae71e147bd
Citlali09 <51176910+Citlali09@users.noreply.github.com>
2019-06-26 16:33:57 -0500

    update with count_friend_circle

--- tests/week1/exercise22/mod.rs
+++ tests/week1/exercise22/mod.rs
@@ -1,17 +1,17 @@
-use onboarding_rust::week1::exercise22::is_friend_circle;
+use onboarding_rust::week1::exercise22::count_friend_circle;
 
 #[test]
 fn test_1_week1_exercise22() {
     let a = vec![vec![1, 1, 0], vec![1, 1, 0], vec![0, 0, 1]];
     let b = 2;
-    assert_eq!(b, is_friend_circle(a));
+    assert_eq!(b, count_friend_circle(a));
 }
 
 #[test]
 fn test_2_week1_exercise22() {
     let a = vec![vec![1, 1, 0], vec![1, 1, 1], vec![0, 1, 1]];
     let b = 1;
-    assert_eq!(b, is_friend_circle(a));
+    assert_eq!(b, count_friend_circle(a));
 }
 
 #[test]
@@ -23,5 +23,5 @@ fn test_3_week1_exercise22() {
         vec![0, 0, 0, 1],
     ];
     let b = 4;
-    assert_eq!(b, is_friend_circle(a));
+    assert_eq!(b, count_friend_circle(a));
 }
