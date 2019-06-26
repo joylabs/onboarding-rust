3344dda122ed83d40ed85cc0f6eca07fd2b31f57
Nuria Reyes <nuria.reyes@joylabs.com>
2019-06-26 10:43:01 -0600

    Adding changes suggested by Mario to improve readability

--- src/week1/exercise21/mod.rs
+++ src/week1/exercise21/mod.rs
@@ -1,17 +1,17 @@
-struct FriendHelper {
+struct IndirectFriendFinder {
    index_vector: Vec<i32>,
    circle_count: i32,
 }
 
-impl FriendHelper {
-   fn init_helper(matrix_order: i32) -> FriendHelper {
-      FriendHelper {
+impl IndirectFriendFinder {
+   fn init_friend_finder(matrix_order: i32) -> IndirectFriendFinder {
+      IndirectFriendFinder {
          index_vector: (0..matrix_order).map(|x| x).collect(),
          circle_count: matrix_order,
       }
    }
 
-   fn find(&mut self, mut position: usize) -> usize {
+   fn find_friend(&mut self, mut position: usize) -> usize {
       let friend = self.index_vector[position] as usize;
       if position != friend {
          self.index_vector[position] = self.index_vector[position];
@@ -21,32 +21,32 @@ impl FriendHelper {
    }
 }
 
-pub fn is_friend_circle(input: Vec<Vec<i32>>) -> i32 {
+pub fn count_friend_circles(input: Vec<Vec<i32>>) -> i32 {
    let matrix_order = input.len() as i32;
-   let mut friend_helper = FriendHelper::init_helper(matrix_order);
+   let mut indirect_friend_finder = IndirectFriendFinder::init_friend_finder(matrix_order);
 
    input
       .into_iter()
       .enumerate()
       .for_each(|(i, row)| {
-         get_one_count(i, row, &mut friend_helper);
+         get_one_count(i, row, &mut indirect_friend_finder);
       });
 
-   friend_helper.circle_count
+   indirect_friend_finder.circle_count
 }
 
-fn get_one_count(i: usize, row: Vec<i32>, friend_helper: &mut FriendHelper) {
+fn get_one_count(i: usize, row: Vec<i32>, indirect_friend_finder: &mut IndirectFriendFinder) {
    row.into_iter()
       .enumerate()
       .for_each(|(j, element)| {
          println!("element {}", element);
 
-         let a = friend_helper.find(i);
-         let b = friend_helper.find(j);
+         let a = indirect_friend_finder.find_friend(i);
+         let b = indirect_friend_finder.find_friend(j);
 
          if element == 1 && a != b {
-            friend_helper.index_vector[a] = b as i32;
-            friend_helper.circle_count -= 1;
+            indirect_friend_finder.index_vector[a] = b as i32;
+            indirect_friend_finder.circle_count -= 1;
          }
       });
 }
--- tests/week1/exercise21/mod.rs
+++ tests/week1/exercise21/mod.rs
@@ -1,17 +1,17 @@
-use onboarding_rust::week1::exercise21::is_friend_circle;
+use onboarding_rust::week1::exercise21::count_friend_circles;
 
 #[test]
 fn test_week1_exercise21_example1() {
     let input = vec![vec![1, 1, 0], vec![1, 1, 0], vec![0, 0, 1]];
     let expected = 2;
-    assert_eq!(expected, is_friend_circle(input));
+    assert_eq!(expected, count_friend_circles(input));
 }
 
 #[test]
 fn test_week1_exercise21_example2() {
     let input = vec![vec![1, 1, 0], vec![1, 1, 1], vec![0, 1, 1]];
     let expected = 1;
-    assert_eq!(expected, is_friend_circle(input));
+    assert_eq!(expected, count_friend_circles(input));
 }
 
 #[test]
@@ -23,5 +23,5 @@ fn test_week1_exercise21_example3() {
         vec![0, 0, 0, 1],
     ];
     let expected = 4;
-    assert_eq!(expected, is_friend_circle(input));
+    assert_eq!(expected, count_friend_circles(input));
 }
