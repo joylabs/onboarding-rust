0c942e55336ddf76db1cb16897bbe748e73c1cda
Miguel Bovadilla <miguel.bovadilla@joylabs.com>
2019-06-27 14:13:27 -0500

    Functional style applied

--- src/week1/exercise4/mod.rs
+++ src/week1/exercise4/mod.rs
@@ -1,23 +1,16 @@
 pub fn self_dividing_numbers(left: i32, right: i32) -> Vec<i32> {
-    let mut output: Vec<i32> = vec![];
-    for number in left..=right {
-        let number_string: Vec<char> = number.to_string().chars().collect();
-        if number_string.len() > 1 {
-            if check_number(number_string, number) {
-                output.push(number);
-            }
-        } else {
-            output.push(number);
-        }
-    }
-    output
+    let result: Vec<i32> = (left..=right)
+        .filter(|n| check_number(*n))
+        .collect::<Vec<i32>>();
+    result
 }
 
-fn check_number(number_string: Vec<char>, number: i32) -> bool {
+fn check_number(current_number: i32) -> bool {
+    let number_string: Vec<char> = current_number.to_string().chars().collect();
     let mut flag = false;
     for character in number_string {
         let character = character.to_digit(10).unwrap() as i32;
-        if character != 0 && number % character == 0 {
+        if character != 0 && current_number % character == 0 {
             flag = true;
         } else {
             flag = false;
