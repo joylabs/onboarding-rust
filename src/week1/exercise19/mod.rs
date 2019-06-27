96fac18ccd2dd1be53046a54e90bf6a99dfcb5ea
Nuria Reyes <nuria.reyes@joylabs.com>
2019-06-27 12:11:27 -0600

    Rewriting solution for exercise 19 due to many bugs found in past solution

--- src/week1/exercise19/mod.rs
+++ src/week1/exercise19/mod.rs
@@ -1,19 +1,17 @@
 pub fn get_two_sum(input: Vec<i32>, target: i32) -> Vec<i32> {
    let mut index_result: Vec<i32> = vec![];
-   let mut j = 0;
-   let mut element = 0;
 
-   input.into_iter().enumerate().for_each(|(i, x)| {
-      if x < target {
-         if (element + x) == target {
-            index_result.push(j as i32);
+   for (i, x) in input.iter().enumerate() {
+      for (j, y) in input.iter().enumerate().skip(i + 1) {
+
+         if (x + y) == target {
             index_result.push(i as i32);
-         } else {
-            j = i;
-            element = x;
+            index_result.push(j as i32);
+            break;
          }
+
       }
-   });
+   }
 
    index_result
 }
