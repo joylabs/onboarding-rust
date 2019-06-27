c5eaf946e2ed598ff6f71acfa08384ca835f8d82
Miguel Bovadilla <miguel.bovadilla@joylabs.com>
2019-06-27 15:20:48 -0500

    Unnecessary variable deleted, rename of function

--- src/week1/exercise4/mod.rs
+++ src/week1/exercise4/mod.rs
@@ -1,21 +1,17 @@
 pub fn self_dividing_numbers(left: i32, right: i32) -> Vec<i32> {
     let result: Vec<i32> = (left..=right)
-        .filter(|n| check_number(*n))
+        .filter(|n| is_self_dividing_number(*n))
         .collect::<Vec<i32>>();
     result
 }
 
-fn check_number(current_number: i32) -> bool {
+fn is_self_dividing_number(current_number: i32) -> bool {
     let number_string: Vec<char> = current_number.to_string().chars().collect();
-    let mut flag = false;
     for character in number_string {
         let character = character.to_digit(10).unwrap() as i32;
-        if character != 0 && current_number % character == 0 {
-            flag = true;
-        } else {
-            flag = false;
-            break;
+        if character == 0 || current_number % character != 0 {
+            return false;
         }
     }
-    flag
+    true
 }
