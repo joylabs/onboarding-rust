2cd7b0712463e6f310dbf9263a4c211407fd0856
Nuria Reyes <nuria.reyes@joylabs.com>
2019-06-27 15:18:18 -0600

    Adding partial solution to exercise 22

--- src/week1/exercise22/mod.rs
+++ src/week1/exercise22/mod.rs
@@ -1,4 +1,54 @@
 pub fn count_islands(input: Vec<Vec<char>>) -> i32 {
+   if input.is_empty() {
+      return 0;
+   }
 
-   0
+   let mut past_element_horizontal = '0';
+   let mut past_element_vertical = '0';
+   let mut next_element_horizontal;
+   let mut next_element_vertical;
+   let mut island_count = 0;
+
+   for (i, x) in input.iter().enumerate() {
+      for (j, y) in x.iter().enumerate() {
+
+         if i > 0 {
+            past_element_vertical = input[i - 1][j];
+         }
+
+         if j > 0 {
+            past_element_horizontal = input[i][j - 1];
+         }
+
+         if input.len() > (i + 1) {
+            next_element_vertical = input[i + 1][j];
+         } else {
+            next_element_vertical = '0';
+         }
+
+         if x.len() > (j + 1) {
+            next_element_horizontal = input[i][j + 1];
+         } else {
+            next_element_horizontal = '0';
+         }
+
+         if *y == '1' && past_element_horizontal == '0' && past_element_vertical == '0'
+         //&& next_element_horizontal == '0'
+         //&& next_element_vertical == '0'
+         {
+            island_count += 1;
+            println!(
+               "p_v: {}, p_h: {}, n_v: {}, n_h: {}, (i: {}, j: {})",
+               past_element_vertical,
+               past_element_horizontal,
+               next_element_vertical,
+               next_element_horizontal,
+               i,
+               j
+            );
+         }
+      }
+   }
+
+   island_count
 }
--- tests/week1/exercise22/mod.rs
+++ tests/week1/exercise22/mod.rs
@@ -3,10 +3,10 @@ use onboarding_rust::week1::exercise22::count_islands;
 #[test]
 fn test_week1_exercise22_example1() {
     let input = vec![
-        vec!['1','1','1','1','0'],
-        vec!['1','1','0','1','0'],
-        vec!['1','1','0','0','0'],
-        vec!['0','0','0','0','0']
+        vec!['1', '1', '1', '1', '0'],
+        vec!['1', '1', '0', '1', '0'],
+        vec!['1', '1', '0', '0', '0'],
+        vec!['0', '0', '0', '0', '0'],
     ];
     let expected = 1;
     assert_eq!(expected, count_islands(input));
@@ -15,11 +15,90 @@ fn test_week1_exercise22_example1() {
 #[test]
 fn test_week1_exercise22_example2() {
     let input = vec![
-        vec!['1','1','0','0','0'],
-        vec!['1','1','0','0','0'],
-        vec!['0','0','1','0','0'],
-        vec!['0','0','0','1','1']
+        vec!['1', '1', '0', '0', '0'],
+        vec!['1', '1', '0', '0', '0'],
+        vec!['0', '0', '1', '0', '0'],
+        vec!['0', '0', '0', '1', '1'],
     ];
     let expected = 3;
+    assert_eq!(expected, count_islands(input));
+}
+
+#[test]
+fn test_week1_exercise22_example3() {
+    let input = vec![
+        vec!['1', '1', '1'],
+        vec!['0', '1', '0'],
+        vec!['1', '1', '1'],
+    ];
+    let expected = 1;
+    assert_eq!(expected, count_islands(input));
+}
+
+#[test]
+fn test_week1_exercise22_example4() {
+    let input = vec![vec!['1'], vec!['1']];
+    let expected = 1;
+    assert_eq!(expected, count_islands(input));
+}
+
+#[test]
+fn test_week1_exercise22_example5() {
+    let input = vec![
+        vec!['1', '0', '1', '1', '1'],
+        vec!['1', '0', '1', '0', '1'],
+        vec!['1', '1', '1', '0', '1'],
+    ];
+    let expected = 1;
+    assert_eq!(expected, count_islands(input));
+}
+
+#[test]
+fn test_week1_exercise22_example6() {
+    let input = vec![];
+    let expected = 0;
+    assert_eq!(expected, count_islands(input));
+}
+
+#[test]
+fn test_week1_exercise22_example7() {
+    let input = vec![vec!['1']];
+    let expected = 1;
+    assert_eq!(expected, count_islands(input));
+}
+
+#[test]
+fn test_week1_exercise22_example8() {
+    let input = vec![vec!['1', '0']];
+    let expected = 1;
+    assert_eq!(expected, count_islands(input));
+}
+
+#[test]
+fn test_week1_exercise22_example9() {
+    let input = vec![vec!['0', '1']];
+    let expected = 1;
+    assert_eq!(expected, count_islands(input));
+}
+
+#[test]
+fn test_week1_exercise22_example10() {
+    let input = vec![vec!['0']];
+    let expected = 0;
+    assert_eq!(expected, count_islands(input));
+}
+
+#[test]
+fn test_week1_exercise22_example11() {
+    let input = vec![
+        vec!['1', '1', '1', '1', '1', '1', '1'],
+        vec!['0', '0', '0', '0', '0', '0', '1'],
+        vec!['1', '1', '1', '1', '1', '0', '1'],
+        vec!['1', '0', '0', '0', '1', '0', '1'],
+        vec!['1', '0', '1', '0', '1', '0', '1'],
+        vec!['1', '0', '1', '1', '1', '0', '1'],
+        vec!['1', '1', '1', '1', '1', '1', '1'],
+    ];
+    let expected = 1;
     assert_eq!(expected, count_islands(input));
 }
