7c97a6267268c87b623343eab01624ae2840a111
Nuria Reyes <nuria.reyes@joylabs.com>
2019-06-27 10:42:05 -0600

    Adding solution to exercise 19

--- src/week1/exercise19/mod.rs
+++ src/week1/exercise19/mod.rs
@@ -1,3 +1,19 @@
 pub fn get_two_sum(input: Vec<i32>, target: i32) -> Vec<i32> {
-   input
+   let mut index_result: Vec<i32> = vec![];
+   let mut j = 0;
+   let mut element = 0;
+
+   input.into_iter().enumerate().for_each(|(i, x)| {
+      if x < target {
+         if (element + x) == target {
+            index_result.push(j as i32);
+            index_result.push(i as i32);
+         } else {
+            j = i;
+            element = x;
+         }
+      }
+   });
+
+   index_result
 }
--- tests/week1/exercise19/mod.rs
+++ tests/week1/exercise19/mod.rs
@@ -1,7 +1,7 @@
 use onboarding_rust::week1::exercise19::get_two_sum;
 
 #[test]
-fn test_week1_exercise16_example1() {
+fn test_week1_exercise19_example1() {
     let input = vec![2, 7, 11, 15];
     let target = 9;
     let expected = vec![0, 1];
