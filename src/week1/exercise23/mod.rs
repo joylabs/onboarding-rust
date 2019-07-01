12eb062f9bc6cb4aba8b97c22185ab0d78717aee
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-07-01 12:04:50 -0500

    refactoring code with Andres

--- src/week1/exercise23/mod.rs
+++ src/week1/exercise23/mod.rs
@@ -1,68 +1,39 @@
 
 pub fn number_of_islands(grid: Vec<Vec<char>>) -> i32 {
 
-    let mut vector_lands: Vec<(i32, i32)> = grid
-        .iter()
-        .enumerate()
-        .map(|(i, c)| {
-            c.iter()
-                .enumerate()
-                .filter(|(_, c)| **c == '1')
-                .map(|(j, _)| (i as i32, j as i32))
-                .collect::<Vec<_>>()
-        })
-        .flatten()
-        .collect();
+    let mut lands = grid;
 
-    let mut islas: Vec<(_, _)> = vec![];
-    while !vector_lands.is_empty() {
-        islas.push(vector_lands.remove(0));
-        recursiva_searching_sorrounded_lands(*islas.last().unwrap(), &mut vector_lands);
-    }
+    (0..lands.len()).fold(0, |mut islas, i| {
+        islas += (0..lands[0].len()).fold(0, |isla, j| {
+            if lands[i][j] == '1' {
+                recursiva_searching_sorrounded_lands_2(&mut lands, i, j);
+                isla + 1
+            } else {
+                isla
+            }
+        });
+        islas
+    })
 
-    islas.len() as i32
 }
 
-fn recursiva_searching_sorrounded_lands(isla: (i32, i32), lands: &mut Vec<(i32, i32)>) {
+fn recursiva_searching_sorrounded_lands_2(lands: &mut Vec<Vec<char>>, i: usize, j: usize) {
 
-    let mut send;
-    let le = lands.len();
+    lands[i][j] = '0';
 
-    //  looking horizontal left
-    for i in 0..le {
-        if isla.0 == lands[i].0 && isla.1 == lands[i].1 - 1 {
-            send = lands.remove(i);
-            recursiva_searching_sorrounded_lands(send, lands);
-            break;
-        }
+    if j + 1 < lands[0].len() && lands[i][j + 1] == '1' {
+        recursiva_searching_sorrounded_lands_2(lands, i, j + 1);
+    }
+    if i + 1 < lands.len() && lands[i + 1][j] == '1' {
+        recursiva_searching_sorrounded_lands_2(lands, i + 1, j);
     }
 
-    let le = lands.len();
-    //looking vertical up
-    for i in 0..le {
-        if isla.1 == lands[i].1 && isla.0 == lands[i].0 - 1 {
-            send = lands.remove(i);
-            recursiva_searching_sorrounded_lands(send, lands);
-            break;
-        }
+    if j > 0 && lands[i][j - 1] == '1' {
+        recursiva_searching_sorrounded_lands_2(lands, i, j - 1);
     }
 
-    let le = lands.len();
-    // looking horizontal right
-    for i in 0..le {
-        if isla.0 == lands[i].0 && isla.1 == lands[i].1 + 1 {
-            send = lands.remove(i);
-            recursiva_searching_sorrounded_lands(send, lands);
-            break;
-        }
-    }
-    // looking vertical down
-    let le = lands.len();
-    for i in 0..le {
-        if isla.1 == lands[i].1 && isla.0 == lands[i].0 + 1 {
-            send = lands.remove(i);
-            recursiva_searching_sorrounded_lands(send, lands);
-            break;
-        }
+    if i > 0 && lands[i - 1][j] == '1' {
+        recursiva_searching_sorrounded_lands_2(lands, i - 1, j);
     }
 }
+
