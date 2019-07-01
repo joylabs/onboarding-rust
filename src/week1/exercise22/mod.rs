8388c0ed196c2a0e5905984ec4279d19d968e692
Andres Morales Reyes <sharker@Andress-MacBook-Pro.local>
2019-07-01 14:15:08 -0600

    Improved performance (peer with Citlalli)

--- src/week1/exercise22/mod.rs
+++ src/week1/exercise22/mod.rs
@@ -1,61 +1,38 @@
 pub fn number_of_islands(grid: Vec<Vec<char>>) -> i32 {
 
-    let mut vector_unos: Vec<(i32, i32)> = grid
-        .iter()
-        .enumerate()
-        .map(|(i, c)| {
-            c.iter()
-                .enumerate()
-                .filter(|(_, c)| **c == '1')
-                .map(|(j, _)| (i as i32, j as i32))
-                .collect::<Vec<_>>()
-        })
-        .flatten()
-        .collect();
+    let mut lands = grid.clone();
 
-    let mut islas: Vec<(_, _)> = vec![];
-    while !vector_unos.is_empty() {
-        islas.push(vector_unos.remove(0));
-        recursiva(*islas.last().unwrap(), &mut vector_unos);
-    }
-    //print!("islas: {:?}, resto_islas: {:?} ", islas, vector_unos);
-    islas.len() as i32
+    (0..lands.len()).fold(0, |islas, i| {
+        islas
+            + (0..lands[0].len()).fold(0, |isla, j| {
+                if lands[i][j] == '1' {
+                    searching_land(&mut lands, i, j);
+                    isla + 1
+                } else {
+                    isla
+                }
+            })
+    })
+
 }
 
-fn recursiva(isla: (i32, i32), unos: &mut Vec<(i32, i32)>) {
 
-    let mut send;
-    let le = unos.len();
-    for i in 0..le {
-        if isla.0 == unos[i].0 && isla.1 == unos[i].1 - 1 {
-            send = unos.remove(i);
-            recursiva(send, unos);
-            break;
-        }
+fn searching_land(lands: &mut Vec<Vec<char>>, i: usize, j: usize) {
+
+    lands[i][j] = '0';
+
+    if j + 1 < lands[0].len() && lands[i][j + 1] == '1' {
+        searching_land(lands, i, j + 1);
     }
-    let le = unos.len();
-    for i in 0..le {
-        if isla.1 == unos[i].1 && isla.0 == unos[i].0 - 1 {
-            send = unos.remove(i);
-            recursiva(send, unos);
-            break;
-        }
+    if i + 1 < lands.len() && lands[i + 1][j] == '1' {
+        searching_land(lands, i + 1, j);
     }
-    let le = unos.len();
-    for i in 0..le {
-        if isla.0 == unos[i].0 && isla.1 == unos[i].1 + 1 {
-            send = unos.remove(i);
-            recursiva(send, unos);
-            break;
-        }
+
+    if j > 0 && lands[i][j - 1] == '1' {
+        searching_land(lands, i, j - 1);
     }
-    let le = unos.len();
-    for i in 0..le {
-        if isla.1 == unos[i].1 && isla.0 == unos[i].0 + 1 {
-            send = unos.remove(i);
-            recursiva(send, unos);
-            break;
-        }
+
+    if i > 0 && lands[i - 1][j] == '1' {
+        searching_land(lands, i - 1, j);
     }
 }
-
