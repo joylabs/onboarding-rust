0f76b9874b090b040db02e84c80b4c50192acde0
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-07-02 16:59:18 -0500

    fixed clippy error

--- src/week2/exercise28/mod.rs
+++ src/week2/exercise28/mod.rs
@@ -3,9 +3,8 @@ use std::cmp;
 use std::collections::HashSet;
 
 pub fn distribute_candies(candies: Vec<i32>) -> i32 {
-    
+
     let distance = candies.len();
     let set: HashSet<i32> = candies.into_iter().map(|kind| kind).collect();
-    cmp::min((distance/2), set.len()) as i32
-
+    cmp::min(distance / 2, set.len()) as i32
 }
