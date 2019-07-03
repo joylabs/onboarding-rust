c001cfa1e4c7442b251c5d4a2ccd495964bd8a85
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-02 22:21:18 -0600

    Adding partial solution for exercise 23

--- src/week1/exercise23/mod.rs
+++ src/week1/exercise23/mod.rs
@@ -1,3 +1,59 @@
 pub fn capture_surrounded_regions(input: &mut Vec<Vec<char>>) {
-   
+   if !input.is_empty() && (input.len() > 2) && (input[0].len() > 2) {
+      let last_row_index = input.len() - 1;
+      let last_element_index = input[0].len() - 1;
+      let input_clone = input.clone();
+
+      for (i, row) in input.iter_mut().enumerate().skip(1) {
+         for (j, element) in row.iter_mut().enumerate().skip(1) {
+            if i < last_row_index && j < last_element_index {
+               let is_surrounded =
+                  is_surrounded(i, j, last_row_index, last_element_index, &input_clone);
+
+               if is_surrounded {
+                  *element = 'X';
+               }
+            }
+         }
+      }
+   }
+}
+
+fn is_surrounded(
+   i: usize,
+   j: usize,
+   last_row_index: usize,
+   last_element_index: usize,
+   board: &[Vec<char>],
+) -> bool {
+   let element_index = last_element_index - 1;
+   let row_index = last_row_index - 1;
+
+   if i == 1 {
+
+      if j == 1 {
+         board[i][0] == 'X' && board[0][j] == 'X'
+      } else if j == element_index {
+         board[i][last_element_index] == 'X' && board[0][j] == 'X'
+      } else {
+         board[0][j] == 'X'
+      }
+
+   } else if i == row_index {
+
+      if j == 1 {
+         board[i][0] == 'X' && board[last_row_index][j] == 'X'
+      } else if j == element_index {
+         board[i][last_element_index] == 'X' && board[last_row_index][j] == 'X'
+      } else {
+         board[last_row_index][j] == 'X'
+      }
+
+   } else if j == 1 {
+      board[i][0] == 'X'
+   } else if j == element_index {
+      board[i][last_element_index] == 'X'
+   } else {
+      false
+   }
 }
--- tests/week1/exercise23/mod.rs
+++ tests/week1/exercise23/mod.rs
@@ -17,3 +17,39 @@ fn test_week1_exercise23_example1() {
     capture_surrounded_regions(&mut input);
     assert_eq!(expected, input);
 }
+
+#[test]
+fn test_week1_exercise23_example2() {
+    let mut input = vec![
+        vec!['O', 'O', 'O'],
+        vec!['O', 'O', 'O'],
+        vec!['O', 'O', 'O'],
+    ];
+    let expected = vec![
+        vec!['O', 'O', 'O'],
+        vec!['O', 'O', 'O'],
+        vec!['O', 'O', 'O'],
+    ];
+    capture_surrounded_regions(&mut input);
+    assert_eq!(expected, input);
+}
+
+#[test]
+fn test_week1_exercise23_example3() {
+    let mut input = vec![
+        vec!['O', 'X', 'X', 'O', 'X'],
+        vec!['X', 'O', 'O', 'X', 'O'],
+        vec!['X', 'O', 'X', 'O', 'X'],
+        vec!['O', 'X', 'O', 'O', 'O'],
+        vec!['X', 'X', 'O', 'X', 'O'],
+    ];
+    let expected = vec![
+        vec!['O', 'X', 'X', 'O', 'X'],
+        vec!['X', 'X', 'X', 'X', 'O'],
+        vec!['X', 'X', 'X', 'O', 'X'],
+        vec!['O', 'X', 'O', 'O', 'O'],
+        vec!['X', 'X', 'O', 'X', 'O'],
+    ];
+    capture_surrounded_regions(&mut input);
+    assert_eq!(expected, input);
+}
