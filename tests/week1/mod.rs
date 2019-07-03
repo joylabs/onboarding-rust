b3d475ce6cd4680e976c76286617a711c1925a34
Miguel Bovadilla <miguel.bovadilla@joylabs.com>
2019-07-03 01:08:51 -0500

    First Commit: Exercise 16 - Hamming Distance

--- tests/week1/mod.rs
+++ tests/week1/mod.rs
@@ -1,4 +1,9 @@
 pub mod exercise1;
+pub mod exercise11;
+pub mod exercise12;
+pub mod exercise13;
+pub mod exercise14;
+pub mod exercise16;
 pub mod exercise2;
 pub mod exercise3;
 pub mod exercise4;
@@ -7,7 +12,3 @@ pub mod exercise6;
 pub mod exercise7;
 pub mod exercise8;
 pub mod exercise9;
-pub mod exercise11;
-pub mod exercise12;
-pub mod exercise13;
-pub mod exercise14;
