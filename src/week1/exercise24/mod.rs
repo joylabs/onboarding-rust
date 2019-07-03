e760b883d11df406dd406166aef30a95afc79f22
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-02 22:51:56 -0600

    Adding solution to exercise 24

--- src/week1/exercise24/mod.rs
+++ src/week1/exercise24/mod.rs
@@ -1,6 +1,8 @@
 pub fn count_jewels(j: String, s: String) -> i32 {
-   // s.chars().fold(0, |acc, c| {
-   //    if j.contains(c.to_string())
-   // })
-   0
+   s.chars().fold(0, |mut acc, c| {
+      if j.contains(c) {
+         acc += 1;
+      }
+      acc
+   })
 }
