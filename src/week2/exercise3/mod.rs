47e859119fb2a08cce67f6b14d6ca2bf706d686a
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-05 12:00:32 -0600

    Adding solution to exercise 3 from week 2 worked with Miguel

--- src/week2/exercise3/mod.rs
+++ src/week2/exercise3/mod.rs
@@ -1,3 +1,21 @@
 pub fn is_happy_number(input: i32) -> bool {
-   false
+   let mut number = get_digit_sum(input);
+   let mut iteration_count = 0;
+
+   while (number != 1) && (iteration_count < 100) {
+      number = get_digit_sum(number);
+      iteration_count += 1;
+   }
+
+   number == 1
+}
+
+fn get_digit_sum(number: i32) -> i32 {
+   let digit_string = number.to_string();
+
+   digit_string.chars().fold(0, |acc, c| {
+      let num = c.to_digit(10).unwrap() as i32;
+      acc + num.pow(2)
+   })
+
 }
