b39d8c13d04f9a630ddbf8f86feb8e0696d5d791
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-05 11:05:08 -0600

    Making some changes to exercise 2 from week 2

--- src/week2/exercise2/mod.rs
+++ src/week2/exercise2/mod.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::collections::HashMap;
 
 pub fn count_unique_morse_words(input: Vec<&str>) -> i32 {
    transform_into_morse(input)
@@ -8,22 +9,24 @@ pub fn count_unique_morse_words(input: Vec<&str>) -> i32 {
 }
 
 fn transform_into_morse(words: Vec<&str>) -> Vec<String> {
-   let morse_code = vec![
+   let morse_code: Vec<&str> = vec![
       ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
       "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",
    ];
 
-   let alphabet = vec![
+   let alphabet: Vec<char> = vec![
       'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
       's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
    ];
+
+   let morse_map: HashMap<_,_> = alphabet.into_iter().zip(morse_code.into_iter()).collect();
 
    words
       .into_iter()
       .map(|w| {
          w.chars()
-            .map(|c| alphabet.iter().position(|x| *x == c).unwrap())
-            .fold("".to_string(), |morse, c| morse + morse_code[c])
+            .map(|c| morse_map.get(&c).unwrap())
+            .fold("".to_string(), |morse, c| morse + c)
       })
       .collect()
 }
