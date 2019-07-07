be5afeab21dee368db8c5c470f382eb2cbcf8345
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-07 07:24:00 -0600

    Adding solution to exercise 8 from week 2

--- src/week2/exercise5/mod.rs
+++ src/week2/exercise5/mod.rs
@@ -1,3 +1,12 @@
+use std::collections::HashSet;
+
 pub fn distribute_candies(input: Vec<i32>) -> i32 {
-   0
+   let cadies_per_person = (input.len() / 2) as i32;
+   let cady_kinds = input.into_iter().collect::<HashSet<i32>>().len() as i32;
+
+   if cady_kinds >= cadies_per_person {
+      cadies_per_person
+   } else {
+      cady_kinds
+   }
 }
