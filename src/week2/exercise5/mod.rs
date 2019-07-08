256e946be0565607d8dab74035e3ba839dd185fd
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-08 11:53:29 -0700

    Correcting typo in variable names

--- src/week2/exercise5/mod.rs
+++ src/week2/exercise5/mod.rs
@@ -1,12 +1,12 @@
 use std::collections::HashSet;
 
 pub fn distribute_candies(input: Vec<i32>) -> i32 {
-   let cadies_per_person = (input.len() / 2) as i32;
-   let cady_kinds = input.into_iter().collect::<HashSet<i32>>().len() as i32;
+   let candies_per_person = (input.len() / 2) as i32;
+   let candy_kinds = input.into_iter().collect::<HashSet<i32>>().len() as i32;
 
-   if cady_kinds >= cadies_per_person {
-      cadies_per_person
+   if candy_kinds >= candies_per_person {
+      candies_per_person
    } else {
-      cady_kinds
+      candy_kinds
    }
 }
