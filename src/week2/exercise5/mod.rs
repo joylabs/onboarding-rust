6265113a0421e06a47a0158977136764ce46dfde
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-08 12:05:36 -0700

    Making requested changes

--- src/week2/exercise5/mod.rs
+++ src/week2/exercise5/mod.rs
@@ -1,12 +1,9 @@
 use std::collections::HashSet;
+use std::cmp;
 
 pub fn distribute_candies(input: Vec<i32>) -> i32 {
    let candies_per_person = (input.len() / 2) as i32;
    let candy_kinds = input.into_iter().collect::<HashSet<i32>>().len() as i32;
 
-   if candy_kinds >= candies_per_person {
-      candies_per_person
-   } else {
-      candy_kinds
-   }
+   cmp::min(candies_per_person, candy_kinds)
 }
