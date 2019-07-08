63e8c721d0b4b8f2adbe5c4806cce629304e4788
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-08 15:52:30 -0700

    Returning result from exercise 4 sorted to avoid the Google Cloud build to break

--- tests/week2/exercise4/mod.rs
+++ tests/week2/exercise4/mod.rs
@@ -4,7 +4,7 @@ use onboarding_rust::week2::exercise4::get_uncommon_words;
 fn test_week2_exercise4_example1() {
     let a = "this apple is sweet".to_string();
     let b = "this apple is sour".to_string();
-    let expected = vec!["sweet".to_string(), "sour".to_string()];
+    let expected = vec!["sour".to_string(), "sweet".to_string()];
     assert_eq!(expected, get_uncommon_words(a, b));
 }
 
