d203001ff1142b41d9153da3a943aec4de9178e1
Miguel Bovadilla <miguel.bovadilla@joylabs.com>
2019-07-09 15:53:52 -0500

    Functional approach

--- src/week1/exercise10/mod.rs
+++ src/week1/exercise10/mod.rs
@@ -1,18 +1,18 @@
 pub fn detect_capital(word: String) -> bool {
-    let mut counter = 0;
-    // Loop over the word
-    for character in word.chars() {
-        // Check if capital letter
-        if (character as i32) < 97 {
-            counter += 1;
-        }
-    }
-    // Check counter and word length
-    if counter == 0
-        || counter == word.len()
-        || counter == 1 && (word.chars().nth(0).unwrap() as i32) < 97
+    let input_capitals = word
+        .chars()
+        .filter(|c| is_capital(*c))
+        .collect::<Vec<char>>();
+    let capital_count = input_capitals.len();
+    if capital_count == 0
+        || capital_count == word.len()
+        || capital_count == 1 && is_capital(word.chars().nth(0).unwrap())
     {
         return true;
     }
     false
+}
+
+fn is_capital(character: char) -> bool {
+    (character as i32) < 97
 }
