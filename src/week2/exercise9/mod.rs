cd66ec5719dcd3fbc3ba3ef72c7d27bd879eb947
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-10 23:11:32 -0700

    Adding ineficient solution to exercise 9 from week 2

--- src/week2/exercise9/mod.rs
+++ src/week2/exercise9/mod.rs
@@ -1,8 +1,41 @@
-use std::collections::HashSet;
+use std::char;
 
 pub fn is_isomorphic(s: String, t: String) -> bool {
-   let s2: Vec<char> = s.chars().collect::<HashSet<char>>().into_iter().collect();
-   let t2: Vec<char> = t.chars().collect::<HashSet<char>>().into_iter().collect();
-   
-   s2.len() == t2.len() 
+   check_pattern(s) == check_pattern(t)
+}
+
+fn check_pattern(word: String) -> String {
+   let w1: Vec<char> = word.chars().collect();
+   let s2: Vec<char> = get_unique(word.chars().collect());
+   let mut my_pattern = String::new();
+
+   println!("w1 {:?}", w1);
+
+   for (i, c) in s2.iter().enumerate() {
+      for w in w1.iter() {
+         if *c == *w {
+            my_pattern = format!("{}{}", my_pattern, i);
+         } else {
+            my_pattern = format!("{}-", my_pattern);
+         }
+      }
+   }
+
+   println!("word {}", my_pattern);
+
+   my_pattern
+}
+
+fn get_unique(mut word: Vec<char>) -> Vec<char> {
+   let mut word2 = Vec::new();
+
+   for c in word.iter() {
+      if !word2.contains(c) {
+         word2.push(*c);
+      }
+   }
+
+   println!("unique {:?}", word2);
+
+   word2
 }
--- tests/week2/exercise9/mod.rs
+++ tests/week2/exercise9/mod.rs
@@ -23,3 +23,43 @@ fn test_week2_exercise9_example3() {
     let expected = true;
     assert_eq!(expected, is_isomorphic(s, t));
 }
+
+#[test]
+fn test_week2_exercise9_example4() {
+    let s = String::from("aba");
+    let t = String::from("baa");
+    let expected = false;
+    assert_eq!(expected, is_isomorphic(s, t));
+}
+
+#[test]
+fn test_week2_exercise9_example5() {
+    let s = String::from("aab");
+    let t = String::from("aaa");
+    let expected = false;
+    assert_eq!(expected, is_isomorphic(s, t));
+}
+
+#[test]
+fn test_week2_exercise9_example6() {
+    let s = String::from("aaaaabbbbbcccccdddddeeeee");
+    let t = String::from("pppppqqqqqrrrrrsssssttttt");
+    let expected = true;
+    assert_eq!(expected, is_isomorphic(s, t));
+}
+
+#[test]
+fn test_week2_exercise9_example7() {
+    let s = String::from("abcdefghijklmnopqrstuvwxyz");
+    let t = String::from("nopqrstuvwxyzabcdefghijklm");
+    let expected = true;
+    assert_eq!(expected, is_isomorphic(s, t));
+}
+
+#[test]
+fn test_week2_exercise9_example8() {
+    let s = String::from("abba");
+    let t = String::from("abab");
+    let expected = false;
+    assert_eq!(expected, is_isomorphic(s, t));
+}
