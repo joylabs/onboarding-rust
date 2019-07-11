4b30798389555bebc59e2f9c6ec8f2212c6c2879
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-11 08:44:43 -0700

    Adding solution worked with Citlalli to exercise 9 from week 2

--- src/week2/exercise9/mod.rs
+++ src/week2/exercise9/mod.rs
@@ -1,41 +1,14 @@
-use std::char;
+use std::collections::HashMap;
 
 pub fn is_isomorphic(s: String, t: String) -> bool {
    check_pattern(s) == check_pattern(t)
 }
 
 fn check_pattern(word: String) -> String {
-   let w1: Vec<char> = word.chars().collect();
-   let s2: Vec<char> = get_unique(word.chars().collect());
-   let mut my_pattern = String::new();
+   let mut map = HashMap::new();
 
-   println!("w1 {:?}", w1);
-
-   for (i, c) in s2.iter().enumerate() {
-      for w in w1.iter() {
-         if *c == *w {
-            my_pattern = format!("{}{}", my_pattern, i);
-         } else {
-            my_pattern = format!("{}-", my_pattern);
-         }
-      }
-   }
-
-   println!("word {}", my_pattern);
-
-   my_pattern
-}
-
-fn get_unique(mut word: Vec<char>) -> Vec<char> {
-   let mut word2 = Vec::new();
-
-   for c in word.iter() {
-      if !word2.contains(c) {
-         word2.push(*c);
-      }
-   }
-
-   println!("unique {:?}", word2);
-
-   word2
+   word.chars()
+      .enumerate()
+      .map(|(i, ch)| map.entry(ch).or_insert(i).to_string())
+      .collect::<String>()
 }
