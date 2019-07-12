410c8db8cc8271c08451eff9462bc38d36751e63
Andres Morales Reyes <sharker@Andress-MacBook-Pro.local>
2019-07-12 15:17:06 -0600

    Hashmap, no sort, faster implementation

--- src/week1/exercise18/mod.rs
+++ src/week1/exercise18/mod.rs
@@ -1,14 +1,11 @@
 use std::collections::HashMap;
 
 pub fn single_number(nums: Vec<i32>) -> i32 {
-    let mut input = nums;
-
-    input.sort();
 
     let mut counter = HashMap::new();
 
 
-    input.iter().for_each(|x| {
+    nums.iter().for_each(|x| {
         *counter.entry(x).or_insert(0) += 1;
     });
     print!("{:?}", counter);
