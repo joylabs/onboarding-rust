b4dcc212ae6e6d3f6ad3cec4fe9ad03c104baa6c
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-15 12:55:04 -0600

    Adding solution to exercise 10 from week 2

--- src/week2/exercise10/mod.rs
+++ src/week2/exercise10/mod.rs
@@ -1,3 +1,25 @@
+use std::collections::HashMap;
+
 pub fn word_pattern(pattern: String, str: String) -> bool {
-   false
+   get_pattern(pattern) == check_pattern(str)
+}
+
+fn get_pattern(word: String) -> String {
+   let mut map = HashMap::new();
+
+   word
+      .chars()
+      .enumerate()
+      .map(|(i, ch)| map.entry(ch).or_insert(i).to_string())
+      .collect::<String>()
+}
+
+fn check_pattern(sentence: String) -> String {
+   let mut map = HashMap::new();
+
+   sentence
+      .split_whitespace()
+      .enumerate()
+      .map(|(i, ch)| map.entry(ch).or_insert(i).to_string())
+      .collect::<String>()
 }
