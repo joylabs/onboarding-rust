382d2080f66ff8c6ee656516239bf712920f5a27
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-16 15:45:27 -0600

    Adding solution worked with Citlalli to exercise 12 from week 2

--- src/week2/exercise12/mod.rs
+++ src/week2/exercise12/mod.rs
@@ -1,17 +1,37 @@
 #[derive(Default, Debug)]
-pub struct MyHashSet {}
+pub struct MyHashSet {
+      key: Vec<i32>,
+}
 
 
 impl MyHashSet {
       pub fn new() -> Self {
-            unimplemented!();
+            Self { key: Vec::new() }
       }
 
-      pub fn add(&mut self, key: i32) {}
+      pub fn add(&mut self, key: i32) {
+            match self.key.iter().position(|x| *x == key) {
+                  None => {
+                        self.key.push(key);
+                  }
+                  _ => {
+                        println!("Value {} already exists", key);
+                  }
+            };
+      }
 
-      pub fn remove(&mut self, key: i32) {}
+      pub fn remove(&mut self, key: i32) {
+            match self.key.iter().position(|x| *x == key) {
+                  Some(i) => {
+                        self.key.remove(i);
+                  }
+                  None => {
+                        println!("No element {} to delete", key);
+                  }
+            };
+      }
 
       pub fn contains(&self, key: i32) -> bool {
-            false
+            self.key.iter().any(|x| *x == key)
       }
 }
