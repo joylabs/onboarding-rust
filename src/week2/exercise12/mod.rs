63254da89ca11ecbcabda06eeef37deae59e80a9
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-16 13:38:34 -0600

    Adding files to work on exercise 12 from week 2

---
+++ src/week2/exercise12/mod.rs
@@ -0,0 +1,17 @@
+#[derive(Default, Debug)]
+pub struct MyHashSet {}
+
+
+impl MyHashSet {
+      pub fn new() -> Self {
+            unimplemented!();
+      }
+
+      pub fn add(&mut self, key: i32) {}
+
+      pub fn remove(&mut self, key: i32) {}
+
+      pub fn contains(&self, key: i32) -> bool {
+            false
+      }
+}
