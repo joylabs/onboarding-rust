5e1a85761ccafd9b1b1e8ccc9b9bd909fe9f0b3f
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-07-17 09:50:23 -0500

    designd hashset first commit

---
+++ tests/week2/exercise34/mod.rs
@@ -0,0 +1,17 @@
+use onboarding_rust::week2::exercise34::MyHashSet;
+
+#[test]
+fn test_week2_exercise34_example1() {
+    let mut hash_set: MyHashSet = MyHashSet::new();
+    hash_set.add(1);
+    hash_set.add(2);
+    assert_eq!(true, hash_set.contains(1));
+    assert_eq!(false, hash_set.contains(3));
+
+    hash_set.add(2);
+    assert_eq!(true, hash_set.contains(2));
+
+    hash_set.remove(2);
+    assert_eq!(false, hash_set.contains(2));
+}
+
--- tests/week2/mod.rs
+++ tests/week2/mod.rs
@@ -3,3 +3,5 @@ pub mod exercise28;
 pub mod exercise29;
 pub mod exercise31;
 pub mod exercise32;
+pub mod exercise34;
+
