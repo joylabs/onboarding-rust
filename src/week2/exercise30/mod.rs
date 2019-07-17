30f52bf03be159e93be51f1ed8126173bfba31dc
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-07-17 11:47:43 -0500

    add custom assert

--- src/week2/exercise30/mod.rs
+++ src/week2/exercise30/mod.rs
@@ -2,13 +2,10 @@ use std::collections::HashSet;
 
 pub fn intersection_of_two(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
 
-    let mut vec = nums1
+    nums1
         .into_iter()
         .filter(|x| nums2.contains(x))
         .collect::<HashSet<i32>>()
         .into_iter()
-        .collect::<Vec<i32>>();
-
-    vec.sort();
-    vec
+        .collect::<Vec<i32>>()
 }
--- tests/week2/exercise30/mod.rs
+++ tests/week2/exercise30/mod.rs
@@ -5,7 +5,7 @@ fn one_intersection_of_two() {
     let nums1 = vec![1, 2, 2, 1];
     let nums2 = vec![2, 2];
     let output = vec![2];
-    assert_eq!(output, intersection_of_two(nums1, nums2));
+    assert!(contains_all(output, intersection_of_two(nums1, nums2) ));
 }
 
 #[test]
@@ -13,7 +13,7 @@ fn two_intersection_of_two() {
     let nums1 = vec![4, 9, 5];
     let nums2 = vec![9, 4, 9, 8, 4];
     let output = vec![4, 9];
-    assert_eq!(output, intersection_of_two(nums1, nums2));
+    assert!(contains_all(output, intersection_of_two(nums1, nums2) ));
 }
 
 #[test]
@@ -21,6 +21,9 @@ fn three_intersection_of_two() {
     let nums1 = vec![4, 9, 5, 1, 8];
     let nums2 = vec![8, 6, 3, 4, 9];
     let output = vec![4, 8, 9];
-    assert_eq!(output, intersection_of_two(nums1, nums2));
+    assert!(contains_all(output, intersection_of_two(nums1, nums2) ));
 }
 
+fn contains_all(a: Vec<i32>, b: Vec<i32>) -> bool {
+    a.len() == b.len() && a.iter().all(|x| b.contains(x))
+}
