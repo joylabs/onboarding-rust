255e42e435a2482824698e06b9a25627131b2a3a
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-18 10:55:39 -0600

    Adding inneficient solution to exercise 13 from week 2 worked with Citlalli

--- src/week2/exercise13/mod.rs
+++ src/week2/exercise13/mod.rs
@@ -1,3 +1,49 @@
+use std::collections::HashMap;
+
 pub fn find_anagrams(s: &str, p: &str) -> Vec<i32> {
-      unimplemented!();
+      let mut anagram_index: Vec<i32> = Vec::new();
+      let s_length = s.len();
+      let p_len = p.len();
+
+      if s_length < p_len {
+            return anagram_index;
+      }
+
+      for mut i in 0..=(s_length - p_len) {
+            let j = i + p_len;
+            println!("p = {:?}, slice = {}, index = {}", p, &s[i..j], i);
+            if contains_all(&s[i..j], p) {
+                  anagram_index.push(i as i32);
+                  i = j + 1;
+            }
+      }
+
+      anagram_index
+}
+
+fn contains_all(s: &str, p: &str) -> bool {
+      // let mut s: Vec<char> = s.chars().collect();
+
+      // for x in p.chars() {
+      //       if s.contains(&x) {
+      //             if let Some(item) = s.iter().position(|&y| y == x) {
+      //                   s.remove(item);
+      //             }
+      //       }
+      // }
+
+      // s.is_empty()
+      let mut map1 = HashMap::new();
+      let mut map2 = HashMap::new();
+
+      for (x, y) in s.chars().zip(p.chars()) {
+            let count1 = map1.entry(x).or_insert(0);
+            *count1 += 1;
+            let count2 = map2.entry(y).or_insert(0);
+            *count2 += 1;
+      }
+
+      println!("map1: {:?}, map2: {:?}", map1, map2);
+
+      map1 == map2
 }
--- tests/week2/exercise13/mod.rs
+++ tests/week2/exercise13/mod.rs
@@ -5,7 +5,7 @@ fn test_week2_exercise13_example1() {
     let s = "cbaebabacd";
     let p = "abc";
     let expected = vec![0, 6];
-    
+
     assert_eq!(expected, find_anagrams(s, p));
 }
 
@@ -14,6 +14,42 @@ fn test_week2_exercise13_example2() {
     let s = "abab";
     let p = "ab";
     let expected = vec![0, 1, 2];
-    
+
+    assert_eq!(expected, find_anagrams(s, p));
+}
+
+#[test]
+fn test_week2_exercise13_example3() {
+    let s = "baa";
+    let p = "aa";
+    let expected = vec![1];
+
+    assert_eq!(expected, find_anagrams(s, p));
+}
+
+#[test]
+fn test_week2_exercise13_example4() {
+    let s = "ababababab";
+    let p = "aab";
+    let expected = vec![0, 2, 4, 6];
+
+    assert_eq!(expected, find_anagrams(s, p));
+}
+
+#[test]
+fn test_week2_exercise13_example5() {
+    let s = "acdcaeccde";
+    let p = "c";
+    let expected = vec![1, 3, 6, 7];
+
+    assert_eq!(expected, find_anagrams(s, p));
+}
+
+#[test]
+fn test_week2_exercise13_example6() {
+    let s = "aecbabedce";
+    let p = "a";
+    let expected = vec![0, 4];
+
     assert_eq!(expected, find_anagrams(s, p));
 }
