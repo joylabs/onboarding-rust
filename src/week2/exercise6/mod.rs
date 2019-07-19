5ea334b1fafaa5ef680e5333e862cc25f774068e
Andres Morales Reyes <sharker@Andress-MacBook-Pro.local>
2019-07-19 16:02:25 -0600

    solved

--- src/week2/exercise6/mod.rs
+++ src/week2/exercise6/mod.rs
@@ -1,19 +1,29 @@
-pub fn get_group_eq_strings(input: Vec<&str>) -> i32 {
+pub fn group_eq_strings(input: Vec<&str>) -> i32 {
    let mut strings: Vec<String> = input.iter().map(|w| w.to_string()).collect();
-   
+
    if input[0].len() > 2 {
       strings = strings.iter_mut().map(|word| sort_string(word)).collect();
    }
 
    strings.sort();
-   strings.dedup(); 
+   strings.dedup();
 
    strings.len() as i32
 }
 
 fn sort_string(str1: &str) -> String {
-   let mut odd: Vec<char> = str1.chars().enumerate().filter(|c| c.0 % 2 == 1).map(|c| c.1).collect();
-   let mut even: Vec<char> = str1.chars().enumerate().filter(|c| c.0 % 2 == 0).map(|c| c.1).collect();
+   let mut odd: Vec<char> = str1
+      .chars()
+      .enumerate()
+      .filter(|c| c.0 % 2 == 1)
+      .map(|c| c.1)
+      .collect();
+   let mut even: Vec<char> = str1
+      .chars()
+      .enumerate()
+      .filter(|c| c.0 % 2 == 0)
+      .map(|c| c.1)
+      .collect();
    odd.sort();
    even.sort();
    odd.iter().collect::<String>() + &even.iter().collect::<String>()
--- tests/week2/exercise6/mod.rs
+++ tests/week2/exercise6/mod.rs
@@ -1,8 +1,26 @@
-use onboarding_rust::week2::exercise6::get_group_eq_strings;
+use onboarding_rust::week2::exercise6::group_eq_strings;
 
 #[test]
 fn test_week1_exercise9_example1() {
     let input = vec!["a", "b", "c", "a", "c", "c"];
     let expected = 3;
-    assert_eq!(expected, get_group_eq_strings(input));
+    assert_eq!(expected, group_eq_strings(input));
+}
+#[test]
+fn test_week1_exercise9_example2() {
+    let input = vec!["aa","bb","ab","ba"];
+    let expected = 4;
+    assert_eq!(expected, group_eq_strings(input));
+}
+#[test]
+fn test_week1_exercise9_example3() {
+    let input = vec!["abc","acb","bac","bca","cab","cba"];
+    let expected = 3;
+    assert_eq!(expected, group_eq_strings(input));
+}
+#[test]
+fn test_week1_exercise9_example4() {
+    let input = vec!["abcd","cdab","adcb","cbad"];
+    let expected = 1;
+    assert_eq!(expected, group_eq_strings(input));
 }
