181252c020cd1bf2b7c8f9c066238e2d9a1f833c
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-07-19 10:11:18 -0500

    first attempt_solution

--- src/week2/exercise35/mod.rs
+++ src/week2/exercise35/mod.rs
@@ -1,4 +1,30 @@
-  pub fn find_anagrams(s: String, p: String) -> Vec<i32> {
-        
-        unimplemented!();
+pub fn find_anagrams(s: String, p: &str) -> Vec<i32> {
+  let mut anagram_index: Vec<i32> = Vec::new();
+  let s_length = s.len();
+  let p_len = p.len();
+  let set: Vec<char> = p.chars().collect();
+
+  if s_length < p_len {
+    return anagram_index;
+  }
+
+  for i in 0..=(s_length - p_len) {
+    let j = i + p_len;
+    if contains_all(&s[i..j], &set) {
+      anagram_index.push(i as i32);
     }
+  }
+
+  anagram_index
+}
+
+fn contains_all(s: &str, set_p: &Vec<char>) -> bool {
+  let set_s: Vec<char> = s.chars().collect();
+  let mut new_vector = vec![0; 256];
+
+  for x in 0..set_p.len() {
+    new_vector[set_s[x] as usize] += 1;
+    new_vector[set_p[x] as usize] -= 1;
+  }
+  !new_vector.into_iter().any(|x| x > 0)
+}
--- tests/week2/exercise35/mod.rs
+++ tests/week2/exercise35/mod.rs
@@ -3,7 +3,7 @@ use onboarding_rust::week2::exercise35::find_anagrams;
 #[test]
 fn find_all_anagrams_one() {
     let s = "cbaebabacd".to_string();
-    let p = "abc".to_string();
+    let p = "abc";
     let output = vec![0, 6];
     assert_eq!(output, find_anagrams(s, p));
 }
@@ -11,7 +11,15 @@ fn find_all_anagrams_one() {
 #[test]
 fn find_all_anagrams_two() {
     let s = "abab".to_string();
-    let p = "ab".to_string();
-    let output = vec![0, 6];
+    let p = "ab";
+    let output = vec![0, 1, 2];
+    assert_eq!(output, find_anagrams(s, p));
+}
+
+#[test]
+fn find_all_anagrams_three() {
+    let s = "aa".to_string();
+    let p = "bb";
+    let output: Vec<i32> = vec![];
     assert_eq!(output, find_anagrams(s, p));
 }
