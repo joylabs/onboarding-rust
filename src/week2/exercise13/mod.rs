06cc53bb07114b65e7d005a08dbb786ce9cad572
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-19 16:34:16 -0600

    Adding solution worked with Citlalli to exercise 13 from week 2

--- src/week2/exercise13/mod.rs
+++ src/week2/exercise13/mod.rs
@@ -1,49 +1,30 @@
-use std::collections::HashMap;
-
 pub fn find_anagrams(s: &str, p: &str) -> Vec<i32> {
       let mut anagram_index: Vec<i32> = Vec::new();
-      let s_length = s.len();
+      let mut alphabet_positions: Vec<i32> = vec![0; 26];
+      let s_chars: Vec<char> = s.chars().collect();
+      let s_len = s.len();
       let p_len = p.len();
+      const OFFSET: usize = 'a' as usize;
 
-      if s_length < p_len {
-            return anagram_index;
-      }
+      p.chars().for_each(|c| {
+            alphabet_positions[(c as usize) - OFFSET] += 1;
+      });
 
-      for mut i in 0..=(s_length - p_len) {
-            let j = i + p_len;
-            println!("p = {:?}, slice = {}, index = {}", p, &s[i..j], i);
-            if contains_all(&s[i..j], p) {
-                  anagram_index.push(i as i32);
-                  i = j + 1;
+      for i in 0..s_len {
+            alphabet_positions[(s_chars[i] as usize) - OFFSET] -= 1;
+
+            if i >= p_len {
+                  alphabet_positions[(s_chars[i - p_len] as usize) - OFFSET] += 1;
+            }
+
+            if i >= (p_len - 1) && is_anagram(&alphabet_positions) {
+                  anagram_index.push((i + 1 - p_len) as i32);
             }
       }
 
       anagram_index
 }
 
-fn contains_all(s: &str, p: &str) -> bool {
-      // let mut s: Vec<char> = s.chars().collect();
-
-      // for x in p.chars() {
-      //       if s.contains(&x) {
-      //             if let Some(item) = s.iter().position(|&y| y == x) {
-      //                   s.remove(item);
-      //             }
-      //       }
-      // }
-
-      // s.is_empty()
-      let mut map1 = HashMap::new();
-      let mut map2 = HashMap::new();
-
-      for (x, y) in s.chars().zip(p.chars()) {
-            let count1 = map1.entry(x).or_insert(0);
-            *count1 += 1;
-            let count2 = map2.entry(y).or_insert(0);
-            *count2 += 1;
-      }
-
-      println!("map1: {:?}, map2: {:?}", map1, map2);
-
-      map1 == map2
+fn is_anagram(alphabet_positions: &[i32]) -> bool {
+      [0; 26] == alphabet_positions
 }
