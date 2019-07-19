a06ebb8ba20fbc45cf6367d72c6b0da8d7170bec
Dave Taylor <david.taylor@joylabs.com>
2019-07-18 22:02:50 -0700

    this is a little faster but .reverse() is easier

--- src/week1/exercise9/mod.rs
+++ src/week1/exercise9/mod.rs
@@ -1,3 +1,7 @@
 pub fn reverse_string(s: &mut Vec<char>) {
-        s.reverse();
-    }
+        let l = s.len() - 1;
+        s.clone().iter().enumerate().rfold(s, |v, (i, &c)| {
+                v[l - i] = c;
+                v
+        });
+}
