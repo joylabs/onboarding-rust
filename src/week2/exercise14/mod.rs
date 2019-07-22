7511bdc699513641d64f721024989aaa301201f0
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-22 16:41:20 -0600

    Changing for-loop to for_each

--- src/week2/exercise14/mod.rs
+++ src/week2/exercise14/mod.rs
@@ -3,10 +3,10 @@ use std::collections::HashMap;
 pub fn first_unique_char(input: String) -> i32 {
       let mut char_map = HashMap::new();
 
-      for c in input.chars() {
+      input.chars().for_each(|c| {
             let counter = char_map.entry(c).or_insert(0);
             *counter += 1;
-      }
+      });
 
       let unique_char_map: HashMap<char, i32> = char_map.into_iter().filter(|x| x.1 == 1).collect();
 
