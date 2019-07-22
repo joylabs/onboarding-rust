11fdf3f80877ae496e5d9bbe48a5942abecb3305
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-22 16:09:58 -0600

    Adding first solution to exercise 14 from week 2 worked with Andres

--- src/week2/exercise14/mod.rs
+++ src/week2/exercise14/mod.rs
@@ -1,3 +1,20 @@
-pub fn first_unique_char(input: &str) -> i32 {
-      0
+use std::collections::HashMap;
+
+pub fn first_unique_char(input: String) -> i32 {
+      let mut char_map = HashMap::new();
+
+      for c in input.chars() {
+            let counter = char_map.entry(c).or_insert(0);
+            *counter += 1;
+      }
+
+      let unique_char_map: HashMap<char, i32> = char_map.into_iter().filter(|x| x.1 == 1).collect();
+
+      for (i, c) in input.char_indices() {
+            if unique_char_map.contains_key(&c) {
+                  return i as i32;
+            }
+      }
+
+      -1
 }
