9c384ac9acd679efc50205f51c33d5aacd3f64a1
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-23 12:03:46 -0600

    Adding solution for exercise 16 from week 2

--- src/week2/exercise16/mod.rs
+++ src/week2/exercise16/mod.rs
@@ -1,3 +1,21 @@
+use std::collections::HashMap;
+
 pub fn find_the_difference(s: String, t: String) -> char {
-      unimplemented!()
+      let mut t_char_map = HashMap::new();
+
+      t.chars().for_each(|c| {
+            let counter = t_char_map.entry(c).or_insert(0);
+            *counter += 1;
+      });
+
+      s.chars().for_each(|c| {
+            t_char_map.entry(c).and_modify(|e| *e -= 1);
+      });
+
+      t_char_map.iter().fold('0', |mut acc, (&k, &v)| {
+            if v == 1 {
+                  acc = k
+            }
+            acc
+      })
 }
--- tests/week2/exercise16/mod.rs
+++ tests/week2/exercise16/mod.rs
@@ -1,9 +1,25 @@
 use onboarding_rust::week2::exercise16::find_the_difference;
 
 #[test]
-fn test_week2_exercise14_example1() {
+fn test_week2_exercise16_example1() {
     let s = "abcd".to_string();
     let t = "abcde".to_string();
     let output = 'e';
+    assert_eq!(output, find_the_difference(s, t));
+}
+
+#[test]
+fn test_week2_exercise16_example2() {
+    let s = String::new();
+    let t = "y".to_string();
+    let output = 'y';
+    assert_eq!(output, find_the_difference(s, t));
+}
+
+#[test]
+fn test_week2_exercise16_example3() {
+    let s = "a".to_string();
+    let t = "aa".to_string();
+    let output = 'a';
     assert_eq!(output, find_the_difference(s, t));
 }
