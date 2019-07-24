dcdd4e9d068e533cc2da738905e7c6658447d4fa
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-07-24 08:52:01 -0500

    refactoring solution

--- src/week2/exercise37/mod.rs
+++ src/week2/exercise37/mod.rs
@@ -1,23 +1,19 @@
 use std::collections::HashMap;
 
 pub fn find_the_difference(s: &str, t: &str) -> char {
-
     let mut map = HashMap::new();
-    t.chars().for_each(|ch| {
-        let count = map.entry(ch).or_insert(0);
-        *count += 1;
-    });
-    for ch in s.chars() {
-        let value = map.entry(ch).or_insert(0);
-        if *value != 0 {
-            *value -= 1;
-        } else {
-            return ch;
-        }
+    let last_char = t.to_string().pop().unwrap();
+    map.insert(last_char, -1);
+
+    for (s, t) in s.chars().zip(t.chars()) {
+        let sum = map.entry(s).or_insert(0);
+        *sum += 1;
+        let less = map.entry(t).or_insert(0);
+        *less -= 1;
     }
 
     map.into_iter()
-        .find(|(_, i)| *i != 0)
+        .find(|(_, i)| *i == -1)
         .map(|(ch, _)| ch)
         .unwrap()
 }
--- tests/week2/exercise37/mod.rs
+++ tests/week2/exercise37/mod.rs
@@ -1,8 +1,7 @@
 use onboarding_rust::week2::exercise37::find_the_difference;
 
-
 #[test]
-fn one_word_pattern() {
+fn one_find_the_letter() {
     let s = "abcd";
     let t = "abcde";
     let ch = 'e';
@@ -10,9 +9,9 @@ fn one_word_pattern() {
 }
 
 #[test]
-fn two_word_pattern() {
-    let s = "abbaba";
-    let t = "ababa";
+fn two_find_the_letter() {
+    let s = "ababa";
+    let t = "abbaba";
     let ch = 'b';
     assert_eq!(ch, find_the_difference(s, t));
 }
