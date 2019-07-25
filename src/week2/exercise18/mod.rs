475841fcb676a4f23bc674ffcbd1c8a7070aaf13
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-25 15:23:45 -0600

    Adding solution to exercise 18 from week 2

--- src/week2/exercise18/mod.rs
+++ src/week2/exercise18/mod.rs
@@ -1,5 +1,19 @@
 use std::collections::HashMap;
+use std::iter::FromIterator;
 
 pub fn frequency_sort(s: String) -> String {
-      unimplemented!()
+      let mut letter_map = HashMap::new();
+
+      s.chars().for_each(|c| {
+            let count = letter_map.entry(c).or_insert(0);
+            *count += 1;
+      });
+
+      let mut letter_vec = Vec::from_iter(letter_map);
+      letter_vec.sort_by(|&(_, a), &(_, b)| b.cmp(&a));
+
+      letter_vec.iter().fold("".to_string(), |mut acc, (letter, value)| {
+            (0..*value).for_each(|_| acc.push(*letter));
+            acc
+      })
 }
