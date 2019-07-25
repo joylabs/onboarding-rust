17bf3d9065f642835eeb9a64d7216c49df572ad5
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-24 18:30:01 -0600

    Adding first solution to exercise 15 from week 2 worked with Citlalli

--- src/week2/exercise15/mod.rs
+++ src/week2/exercise15/mod.rs
@@ -1,3 +1,36 @@
-pub fn subdomain_visits(input: Vec<&str>) -> Vec<&str> {
-      unimplemented!()
+use std::collections::HashMap;
+
+pub fn subdomain_visits(input: Vec<String>) -> Vec<String> {
+      let mut sub_domain_map: HashMap<String, i32> = HashMap::new();
+
+      input.iter().for_each(|word| {
+            get_domains(&word).iter().for_each(|dom| {
+                  let domain: Vec<&str> = dom.split_whitespace().collect();
+                  let visits: i32 = domain[0].parse().unwrap();
+
+                  let counter = sub_domain_map.entry(domain[1].to_string()).or_insert(0);
+                  *counter += visits;
+            });
+      });
+
+      sub_domain_map
+            .iter()
+            .map(|(k, v)| format!("{} {}", v, &k))
+            .collect::<Vec<String>>()
+}
+
+fn get_domains(input: &str) -> Vec<String> {
+      let mut string_pieces = input.split_whitespace();
+      let visits: i32 = string_pieces.next().unwrap().parse().unwrap();
+      let domain: String = string_pieces.next().unwrap().to_string();
+
+      let mut domains = vec![input.to_string()];
+
+      for (i, c) in domain.chars().enumerate() {
+            if c == '.' {
+                  domains.push(format!("{} {}", visits, &domain[(i + 1)..]));
+            }
+      }
+
+      domains
 }
--- tests/week2/exercise15/mod.rs
+++ tests/week2/exercise15/mod.rs
@@ -2,14 +2,35 @@ use onboarding_rust::week2::exercise15::subdomain_visits;
 
 #[test]
 fn test_week2_exercise15_example1() {
-    let input = vec!["9001 discuss.leetcode.com"];
-    let output = vec!["9001 discuss.leetcode.com", "9001 leetcode.com", "9001 com"];
-    assert_eq!(output, subdomain_visits(input));
+    let input = vec!["9001 discuss.leetcode.com".to_string()];
+    let output = vec![
+        "9001 discuss.leetcode.com".to_string(),
+        "9001 leetcode.com".to_string(),
+        "9001 com".to_string(),
+    ];
+    assert!(contains_all(output, subdomain_visits(input)));
 }
 
 #[test]
 fn test_week2_exercise15_example2() {
-    let input = vec!["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"];
-    let output = vec!["901 mail.com","50 yahoo.com","900 google.mail.com","5 wiki.org","5 org","1 intel.mail.com","951 com"];
-    assert_eq!(output, subdomain_visits(input));
+    let input = vec![
+        "900 google.mail.com".to_string(),
+        "50 yahoo.com".to_string(),
+        "1 intel.mail.com".to_string(),
+        "5 wiki.org".to_string(),
+    ];
+    let output = vec![
+        "901 mail.com".to_string(),
+        "50 yahoo.com".to_string(),
+        "900 google.mail.com".to_string(),
+        "5 wiki.org".to_string(),
+        "5 org".to_string(),
+        "1 intel.mail.com".to_string(),
+        "951 com".to_string(),
+    ];
+    assert!(contains_all(output, subdomain_visits(input)));
+}
+
+fn contains_all(a: Vec<String>, b: Vec<String>) -> bool {
+    a.len() == b.len() && a.iter().all(|x| b.contains(x))
 }
