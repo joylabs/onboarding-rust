689eeccc544193b019728c60b6c03edeae1ce43a
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-26 13:02:23 -0600

    Adding solution to exercise 19 from week 2

--- src/week2/exercise19/mod.rs
+++ src/week2/exercise19/mod.rs
@@ -1,17 +1,24 @@
-pub fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
-      let correct_nums: Vec<i32> = ((1 as i32)..=(nums.len() as i32)).collect();
-      let mut nums = nums.clone();
-      nums.sort();
+use std::collections::HashMap;
 
+pub fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
+      let mut nums_map: HashMap<i32, i32> = HashMap::new();
       let mut duplicated_num = 0;
       let mut missing_num = 0;
-      for (i, n) in correct_nums.iter().enumerate() {
-            if !nums.contains(n) {
-                  missing_num = *n;
+
+      for (i, n) in nums.iter().enumerate() {
+            if !nums.contains(&(i as i32 + 1)) {
+                  missing_num = i as i32 + 1;
             }
 
-            if i + 1 < nums.len() && nums[i] == nums[i + 1] {
-                  duplicated_num = nums[i];
+            let count = nums_map.entry(*n).or_insert(0);
+            *count += 1;
+
+            if *count > 1 {
+                  duplicated_num = *n;
+            }
+
+            if duplicated_num > 0 && missing_num > 0 {
+                  break;
             }
       }
 
