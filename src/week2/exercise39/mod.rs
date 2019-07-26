a5235008d4e77b4ca042b2b91083c40a45ef975a
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-07-26 12:23:14 -0500

    first solution

--- src/week2/exercise39/mod.rs
+++ src/week2/exercise39/mod.rs
@@ -1,13 +1,25 @@
 use std::collections::HashMap;
 
-pub fn most_common(paragraph: String, banned: Vec<&str>) -> &str {
+pub fn most_common(paragraph: String, banned: Vec<String>) -> String {
     let mut count_words = HashMap::new();
 
     paragraph.split_whitespace().for_each(|word| {
-        let count = count_words.entry(word).or_insert(0);
+        let count = count_words.entry(valid_word(word)).or_insert(0);
         *count += 1;
     });
+    count_words
+        .iter()
+        .filter(|(key, _)| !banned.contains(*key))
+        .map(|(key, value)| (value, key))
+        .max()
+        .map(|(_, key)| key)
+        .unwrap()
+        .to_string()
+}
 
-   
-    "hola"
+fn valid_word(word: &str) -> String {
+    word.chars()
+        .filter(|c| c.is_alphanumeric())
+        .collect::<String>()
+        .to_lowercase()
 }
--- tests/week2/exercise39/mod.rs
+++ tests/week2/exercise39/mod.rs
@@ -3,8 +3,8 @@ use onboarding_rust::week2::exercise39::most_common;
 #[test]
 fn one_most_common() {
     let paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.".to_string();
-    let banned = vec!["hit"];
-    let output = "ball";
+    let banned = vec!["hit".to_string()];
+    let output = "ball".to_string();
     assert_eq!(output, most_common(paragraph, banned));
 }
 
