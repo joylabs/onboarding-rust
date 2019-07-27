db0875896b238f2b0f156e3af99d91f6150d66c1
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-27 14:39:17 -0600

    Adding refactored solution to exercise 20 from week 2

--- src/week2/exercise15/mod.rs
+++ src/week2/exercise15/mod.rs
@@ -1,36 +1,31 @@
 use std::collections::HashMap;
 
 pub fn subdomain_visits(input: Vec<String>) -> Vec<String> {
-      let mut sub_domain_map: HashMap<String, i32> = HashMap::new();
+    let mut sub_domain_map: HashMap<String, i32> = HashMap::new();
 
-      input.iter().for_each(|word| {
-            get_domains(&word).iter().for_each(|dom| {
-                  let domain: Vec<&str> = dom.split_whitespace().collect();
-                  let visits: i32 = domain[0].parse().unwrap();
+    input.iter().for_each(|word| {
+        get_domains(&word, &mut sub_domain_map);
+    });
 
-                  let counter = sub_domain_map.entry(domain[1].to_string()).or_insert(0);
-                  *counter += visits;
-            });
-      });
-
-      sub_domain_map
-            .iter()
-            .map(|(k, v)| format!("{} {}", v, &k))
-            .collect::<Vec<String>>()
+    sub_domain_map
+        .iter()
+        .map(|(k, v)| v.to_string() + " " + &k)
+        .collect::<Vec<String>>()
 }
 
-fn get_domains(input: &str) -> Vec<String> {
-      let mut string_pieces = input.split_whitespace();
-      let visits: i32 = string_pieces.next().unwrap().parse().unwrap();
-      let domain: String = string_pieces.next().unwrap().to_string();
+fn get_domains(input: &str, map: &mut HashMap<String, i32>) {
+    let mut string_pieces = input.split_whitespace();
+    let visits: i32 = string_pieces.next().unwrap().parse().unwrap();
+    let domain: String = string_pieces.next().unwrap().to_string();
 
-      let mut domains = vec![input.to_string()];
+    let input = map.entry(domain.clone()).or_insert(0);
+    *input += visits;
 
-      for (i, c) in domain.chars().enumerate() {
-            if c == '.' {
-                  domains.push(format!("{} {}", visits, &domain[(i + 1)..]));
-            }
-      }
+    for (i, c) in domain.chars().enumerate() {
+        if c == '.' {
+            let count = map.entry((&domain[(i + 1)..]).to_string()).or_insert(0);
+            *count += visits;
+        }
+    }
 
-      domains
 }
