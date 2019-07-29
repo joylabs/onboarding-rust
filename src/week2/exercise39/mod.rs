67e572b98b339fabaaad66d230c3e0edd7bf85c5
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-07-29 08:19:45 -0500

    refactoring solution

--- src/week2/exercise39/mod.rs
+++ src/week2/exercise39/mod.rs
@@ -1,21 +1,18 @@
 use std::collections::HashMap;
 
-pub fn most_common(paragraph: String, banned: Vec<String>) -> String {
+pub fn most_common_not_banned(paragraph: String, banned: Vec<String>) -> String {
     let mut count_words = HashMap::new();
-    let mut word: String = String::from("");
 
-    for i in paragraph.chars() {
-        if i.is_alphabetic() {
-            word.push(i);
-        } else {
-            if !word.is_empty() {
-                let count = count_words.entry(word.clone().to_lowercase()).or_insert(0);
-                *count += 1;
-            }
-            word.clear();
-        }
-    }
-    count_words.insert(word, 1);
+    paragraph
+        .to_lowercase()
+        .chars()
+        .map(|ch| if ch.is_alphabetic() { ch } else { ' ' })
+        .collect::<String>()
+        .split_whitespace()
+        .for_each(|word| {
+            let count = count_words.entry(word.to_string()).or_insert(0);
+            *count += 1;
+        });
 
     count_words
         .iter()
--- tests/week2/exercise39/mod.rs
+++ tests/week2/exercise39/mod.rs
@@ -1,11 +1,11 @@
-use onboarding_rust::week2::exercise39::most_common;
+use onboarding_rust::week2::exercise39::most_common_not_banned;
 
 #[test]
 fn one_most_common() {
     let paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.".to_string();
     let banned = vec!["hit".to_string()];
     let output = "ball".to_string();
-    assert_eq!(output, most_common(paragraph, banned));
+    assert_eq!(output, most_common_not_banned(paragraph, banned));
 }
 
 #[test]
@@ -13,13 +13,13 @@ fn two_most_common() {
     let paragraph = "Bob hit,a, ball, the,hit,  BALL, flew ,far ,after it was hit.".to_string();
     let banned = vec!["hit".to_string()];
     let output = "ball";
-    assert_eq!(output, most_common(paragraph, banned));
+    assert_eq!(output, most_common_not_banned(paragraph, banned));
 }
 
 #[test]
 fn three_most_common() {
     let paragraph = "Bob".to_string();
     let banned = vec![];
-    let output = "Bob";
-    assert_eq!(output, most_common(paragraph, banned));
+    let output = "bob";
+    assert_eq!(output, most_common_not_banned(paragraph, banned));
 }
