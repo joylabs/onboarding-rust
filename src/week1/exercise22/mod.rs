a3269a8009dad79f83dc559cc63f1c27406ce26e
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-29 18:28:20 -0600

    Adding solution to exercise 22 from week 1

--- src/week1/exercise22/mod.rs
+++ src/week1/exercise22/mod.rs
@@ -3,52 +3,42 @@ pub fn count_islands(input: Vec<Vec<char>>) -> i32 {
       return 0;
    }
 
-   let mut past_element_horizontal = '0';
-   let mut past_element_vertical = '0';
-   let mut next_element_horizontal;
-   let mut next_element_vertical;
-   let mut island_count = 0;
+   let mut trace_grid = input.clone();
+   let mut land_count = 0;
 
-   for (i, x) in input.iter().enumerate() {
-      for (j, y) in x.iter().enumerate() {
-
-         if i > 0 {
-            past_element_vertical = input[i - 1][j];
-         }
-
-         if j > 0 {
-            past_element_horizontal = input[i][j - 1];
-         }
-
-         if input.len() > (i + 1) {
-            next_element_vertical = input[i + 1][j];
-         } else {
-            next_element_vertical = '0';
-         }
-
-         if x.len() > (j + 1) {
-            next_element_horizontal = input[i][j + 1];
-         } else {
-            next_element_horizontal = '0';
-         }
-
-         if *y == '1' && past_element_horizontal == '0' && past_element_vertical == '0'
-         //&& next_element_horizontal == '0'
-         //&& next_element_vertical == '0'
-         {
-            island_count += 1;
-            println!(
-               "p_v: {}, p_h: {}, n_v: {}, n_h: {}, (i: {}, j: {})",
-               past_element_vertical,
-               past_element_horizontal,
-               next_element_vertical,
-               next_element_horizontal,
-               i,
-               j
-            );
+   for (y, row) in input.iter().enumerate() {
+      for (x, _) in row.iter().enumerate() {
+         if trace_grid[y][x] == '1' {
+            land_count += 1;
+            check_adjacent_land(&mut trace_grid, x as i32, y as i32);
          }
       }
    }
 
-   island_count
+   land_count
+}
+
+fn check_adjacent_land(trace_grid: &mut Vec<Vec<char>>, x: i32, y: i32) {
+   let width = trace_grid[0].len() as i32;
+   let height = trace_grid.len() as i32;
+
+   if (x + 1) < width && trace_grid[y as usize][x as usize + 1] == '1' {
+      trace_grid[y as usize][x as usize + 1] = '0';
+      check_adjacent_land(trace_grid, x + 1, y);
+   }
+
+   if (x - 1) >= 0 && trace_grid[y as usize][x as usize - 1] == '1' {
+      trace_grid[y as usize][x as usize - 1] = '0';
+      check_adjacent_land(trace_grid, x - 1, y);
+   }
+
+   if (y + 1) < height && trace_grid[y as usize + 1][x as usize] == '1' {
+      trace_grid[y as usize + 1][x as usize] = '0';
+      check_adjacent_land(trace_grid, x, y + 1);;
+   }
+
+   if (y - 1) >= 0 && trace_grid[y as usize - 1][x as usize] == '1' {
+      trace_grid[y as usize - 1][x as usize] = '0';
+      check_adjacent_land(trace_grid, x, y - 1);;
+   }
 }
