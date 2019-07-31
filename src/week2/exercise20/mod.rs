1129db84cffb99c03c742040ec12daf1cc7d79c2
Nuria Reyes <nuria.reyes@joylabs.com>
2019-07-30 23:44:41 -0600

    Adding some functions to help solve exercise

--- src/week2/exercise20/mod.rs
+++ src/week2/exercise20/mod.rs
@@ -1,3 +1,45 @@
+use std::collections::BTreeMap;
+
 pub fn count_of_atoms(formula: String) -> String {
-      unimplemented!()
+      let mut atoms_map = get_atom_names(&formula);
+      count_atoms(&formula, &mut atoms_map);
+
+      atoms_map
+            .iter()
+            .fold("".to_string(), |mut formula, (key, &value)| {
+                  formula += key;
+                  if value > 1 {
+                        formula += &value.to_string();
+                  }
+
+                  formula
+            })
+}
+
+fn get_atom_names(formula: &str) -> BTreeMap<String, i32> {
+      let mut atoms_map: BTreeMap<String, i32> = BTreeMap::new();
+      let mut atom = String::new();
+
+      formula
+            .chars()
+            .filter(|c| c.is_ascii_alphabetic())
+            .for_each(|c| {
+                  if c.is_lowercase() || atom.is_empty() {
+                        atom.push(c);
+                  } else {
+                        atoms_map.entry(atom.clone()).or_insert(0);
+                        atom.clear();
+                        atom.push(c);
+                  }
+            });
+
+      atoms_map.entry(atom).or_insert(0);
+
+      println!("atoms_map: {:?}", atoms_map);
+
+      atoms_map
+}
+
+fn count_atoms(formula: &str, atoms_map: &mut BTreeMap<String, i32>) {
+
 }
