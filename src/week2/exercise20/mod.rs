febc0e9e201fccf923d9c22bf1d484a177800ba1
Nuria Reyes <nuria.reyes@joylabs.com>
2019-08-01 11:35:35 -0600

    Adding ineficient solution to exercise 20 from week 2

--- src/week2/exercise20/mod.rs
+++ src/week2/exercise20/mod.rs
@@ -1,10 +1,7 @@
 use std::collections::BTreeMap;
 
 pub fn count_of_atoms(formula: String) -> String {
-      let mut atoms_map = get_atom_names(&formula);
-      count_atoms(&formula, &mut atoms_map);
-
-      atoms_map
+      parse_formula(&formula)
             .iter()
             .fold("".to_string(), |mut formula, (key, &value)| {
                   formula += key;
@@ -16,30 +13,89 @@ pub fn count_of_atoms(formula: String) -> String {
             })
 }
 
-fn get_atom_names(formula: &str) -> BTreeMap<String, i32> {
+fn parse_formula(formula: &str) -> BTreeMap<String, i32> {
       let mut atoms_map: BTreeMap<String, i32> = BTreeMap::new();
+      let mut new_formula: Vec<String> = Vec::new();
       let mut atom = String::new();
+      let mut num_atom = String::new();
 
-      formula
-            .chars()
-            .filter(|c| c.is_ascii_alphabetic())
-            .for_each(|c| {
-                  if c.is_lowercase() || atom.is_empty() {
-                        atom.push(c);
-                  } else {
-                        atoms_map.entry(atom.clone()).or_insert(0);
+      formula.chars().for_each(|c| {
+            if c == '(' || c == ')' {
+                  if !atom.is_empty() {
+                        new_formula.push(atom.clone());
                         atom.clear();
-                        atom.push(c);
                   }
-            });
+                  if !num_atom.is_empty() {
+                        new_formula.push(num_atom.clone());
+                        num_atom.clear();
+                  }
+                  new_formula.push(c.to_string());
+            } else if c.is_digit(10) {
+                  num_atom.push(c);
+            } else if c.is_lowercase() || atom.is_empty() {
+                  atom.push(c);
+            } else {
+                  if !atom.is_empty() {
+                        new_formula.push(atom.clone());
+                        atom.clear();
+                  }
+                  if !num_atom.is_empty() {
+                        new_formula.push(num_atom.clone());
+                        num_atom.clear();
+                  }
+                  atom.push(c);
+            }
+      });
 
-      atoms_map.entry(atom).or_insert(0);
+      if !atom.is_empty() {
+            new_formula.push(atom);
+      }
+      if !num_atom.is_empty() {
+            new_formula.push(num_atom);
+      }
 
-      println!("atoms_map: {:?}", atoms_map);
+      // println!("New formula: {:?}", new_formula);
+
+      let mut num_atom: Vec<i32> = vec![1];
+      let mut is_close_parenthesis = false;
+      let mut is_num = false;
+
+      let mut atoms: Vec<String> = Vec::new();
+
+      new_formula.iter_mut().rev().for_each(|s| {
+            // println!("num_atom: {:?}", num_atom);
+            if s.parse::<i32>().is_ok() {
+                  is_num = true;
+                  let n = num_atom[num_atom.len() - 1];
+                  num_atom.push(s.parse::<i32>().unwrap() * n);
+            } else if s == ")" {
+                  is_close_parenthesis = true;
+                  is_num = false;
+            } else if s == "(" {
+                  if num_atom.len() > 1 {
+                        num_atom.pop();
+                  }
+                  is_close_parenthesis = false;
+                  is_num = false;
+            } else {
+                  let n = num_atom[num_atom.len() - 1];
+                  for _ in 0..n {
+                        atoms.push(s.clone());
+                  }
+                  if num_atom.len() > 1 && is_num {
+                        num_atom.pop();
+                  }
+                  is_close_parenthesis = false;
+                  is_num = false;
+            }
+      });
+
+      // println!("atoms: {:?}", atoms);
+
+      atoms.iter().for_each(|s| {
+            let atom_count = atoms_map.entry(s.clone()).or_insert(0);
+            *atom_count += 1;
+      });
 
       atoms_map
-}
-
-fn count_atoms(formula: &str, atoms_map: &mut BTreeMap<String, i32>) {
-
 }
--- tests/week2/exercise20/mod.rs
+++ tests/week2/exercise20/mod.rs
@@ -20,3 +20,24 @@ fn test_week2_exercise20_example3() {
     let expected = "K4N2O14S4".to_string();
     assert_eq!(expected, count_of_atoms(input));
 }
+
+#[test]
+fn test_week2_exercise20_example4() {
+    let input = "(H12O2)".to_string();
+    let expected = "H12O2".to_string();
+    assert_eq!(expected, count_of_atoms(input));
+}
+
+#[test]
+fn test_week2_exercise20_example5() {
+    let input = "(H2O2)3".to_string();
+    let expected = "H6O6".to_string();
+    assert_eq!(expected, count_of_atoms(input));
+}
+
+// #[test]
+// fn test_week2_exercise20_example6() {
+//     let input = "(((U42Se42Fe10Mc31Rh49Pu49Sb49)49V39Tm50Zr44Og6)33((W2Ga48Tm14Eu46Mt12)23(RuRnMn11)7(Yb15Lu34Ra19CuTb2)47(Md38BhCu48Db15Hf12Ir40)7CdNi21(Db40Zr24Tc27SrBk46Es41DsI37Np9Lu16)46(Zn49Ho19RhClF9Tb30SiCuYb16)15)37(Cr48(Ni31)25(La8Ti17Rn6Ce35)36(Sg42Ts32Ca)37Tl6Nb47Rh32NdGa18Cm10Pt49(Ar37RuSb30Cm32Rf28B39Re7F36In19Zn50)46)38(Rh19Md23No22PoTl35Pd35Hg)41)50".to_string();
+//     let expected = "Ar3233800B3408600Bh37Bk78292Ca1900Cd259Ce63000Cl555Cm2815800Cr2400Cu2378Db68635Ds1702Es69782Eu1058F3151395Fe330Ga35304Hf444Hg2050Ho10545I62974In1660600Ir1480La14400Lu28830Mc1023Md48556Mn77Mt276Nb89300Nd1900Ni39527No45100Np15318Og198Pd71750Po2050Pt93100Pu1617Ra893Re611800Rf2447200Rh101922Rn10807Ru87407Sb2623617Se1386Sg79800Si555Sr1702Tb16744Tc45954Ti30600Tl786150Tm1972Ts60800U1386V162987W46Yb9585Zn4397195Zr42300".to_string();
+//     assert_eq!(expected, count_of_atoms(input));
+// }
