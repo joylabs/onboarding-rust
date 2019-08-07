da95d5312fb875385392c4cb4b0f11e29afe45ee
Nuria Reyes <nuria.reyes@joylabs.com>
2019-08-07 12:50:06 -0600

    Adding first solution to exercise 2 from week 3 worked with Miguel

--- src/week3/exercise2/mod.rs
+++ src/week3/exercise2/mod.rs
@@ -1,3 +1,21 @@
-pub fn find_content_children(g: Vec<i32>, s: Vec<i32>) -> i32 {
-      unimplemented!()
+pub fn find_content_children(mut g: Vec<i32>, mut s: Vec<i32>) -> i32 {
+      g.sort();
+      s.sort();
+      let mut cookie_counter = 0;
+
+      for child in g.into_iter() {
+            if s.len() > 0 {
+                  let mut cookie = s.remove(0);
+                  while child > cookie && s.len() > 0 {
+                        cookie = s.remove(0);
+                  } 
+                  if child <= cookie {
+                        cookie_counter += 1;
+                  }
+            } else {
+                  break;
+            }
+      }
+
+      cookie_counter
 }
