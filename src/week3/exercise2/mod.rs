e21bdba044d667102894f7150a38952bf5335748
Nuria Reyes <nuria.reyes@joylabs.com>
2019-08-07 16:42:30 -0600

    Making clippy happy

--- src/week3/exercise2/mod.rs
+++ src/week3/exercise2/mod.rs
@@ -4,9 +4,9 @@ pub fn find_content_children(mut g: Vec<i32>, mut s: Vec<i32>) -> i32 {
       let mut cookie_counter = 0;
 
       for child in g.into_iter() {
-            if s.len() > 0 {
+            if !s.is_empty() {
                   let mut cookie = s.remove(0);
-                  while child > cookie && s.len() > 0 {
+                  while child > cookie && !s.is_empty() {
                         cookie = s.remove(0);
                   } 
                   if child <= cookie {
