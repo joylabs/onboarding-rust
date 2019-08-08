7cd202edad6bb1edf3343d26c686e088875ce3f9
Miguel Bovadilla <miguel.bovadilla@joylabs.com>
2019-08-07 19:47:57 -0500

    Array.len() replaced with Array.is_empty

--- src/week1/exercise46/mod.rs
+++ src/week1/exercise46/mod.rs
@@ -4,11 +4,11 @@ pub fn find_content_children(mut g: Vec<i32>, mut s: Vec<i32>) -> i32 {
     let mut assigned_cookie_counter = 0;
 
     for child in g.into_iter() {
-        if s.len() > 0 {
+        if !s.is_empty() {
             let mut cookie = s.remove(0);
-            while child > cookie && s.len() > 0 {
+            while child > cookie && !s.is_empty() {
                 cookie = s.remove(0);
-            } 
+            }
             if child <= cookie {
                 assigned_cookie_counter += 1;
             }
@@ -16,6 +16,6 @@ pub fn find_content_children(mut g: Vec<i32>, mut s: Vec<i32>) -> i32 {
             break;
         }
     }
-    
+
     assigned_cookie_counter
 }
