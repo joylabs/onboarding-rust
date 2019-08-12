ab275efd6bde48e2d1860111e54b0fbcab4bb721
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-08-12 08:25:45 -0500

    fixing sintaxis for docker to pass

--- src/week1/exercise2/mod.rs
+++ src/week1/exercise2/mod.rs
@@ -2,8 +2,9 @@ pub fn transpose_matrix(input: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     let mut output = vec![];
     for row_element in 0..input[0].len() {
         let mut row = vec![];
-        for col_element in 0..input.len() {
-            row.push(input[col_element][row_element]);
+        for col_element in &input {
+            let col_element = col_element;
+            row.push(col_element[row_element]);
         }
         output.push(row);
     }
