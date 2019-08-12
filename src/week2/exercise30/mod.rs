1f041e6863a465f8a65a7f465c0591fff50cfb4d
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-08-12 17:32:00 -0500

    change box implementation

--- src/week2/exercise30/mod.rs
+++ src/week2/exercise30/mod.rs
@@ -1,73 +1,61 @@
-
 use std::collections::HashSet;
 
 pub fn valid_sudoku(input: Vec<Vec<char>>) -> bool {
-	    let mut seen: HashSet<char> = HashSet::new();
-	    rows_are_valid(&input, &mut seen)
-	        && columns_are_valid(&input, &mut seen)
-	        && looking_in_box(input, &mut seen)
+	looking_by_box(&input) && rows_are_valid(&input) && columns_are_valid(&input)
+}
+
+fn looking_by_box(input: &[Vec<char>]) -> bool {
+	let mut boxes = vec![];
+
+	(0..9).step_by(3).for_each(|v| {
+		boxes.push(get_box(input, 0, v));
+		boxes.push(get_box(input, 3, v));
+		boxes.push(get_box(input, 6, v));
+	});
+	rows_are_valid(&boxes)
+}
+
+fn get_box(input: &[Vec<char>], start_col: usize, start_row: usize) -> Vec<char> {
+
+	fn row_portion(row: &[char], start_col: usize) -> Vec<char> {
+		row.iter().skip(start_col).take(3).cloned().collect()
 	}
-	
-	fn looking_in_box(input: Vec<Vec<char>>, seen: &mut HashSet<char>) -> bool {
-	    let vectors = input.clone();
-	    let mut vec1: Vec<char> = vec![];
-	    let mut vec2: Vec<char> = vec![];
-	    let mut vec3: Vec<char> = vec![];
-	
-	    for row in vectors {
-	        for (j, el) in row.iter().enumerate() {
-	            if j < 3 {
-	                vec1.push(*el);
-	            } else if j < 6 {
-	                vec2.push(*el);
-	            } else {
-	                vec3.push(*el);
-	            }
-	        }
-	    }
-	    box_by_box(vec1, seen) && box_by_box(vec2, seen) && box_by_box(vec3, seen)
+
+	input
+		.iter()
+		.skip(start_row)
+		.map(|row| row_portion(&row, start_col))
+		.take(3)
+		.flatten()
+		.collect()
+}
+
+fn rows_are_valid(input: &[Vec<char>]) -> bool {
+	let mut seen: HashSet<char> = HashSet::new();
+
+	input.iter().all(|x| {
+		seen.clear();
+		x.iter().all(|y| verify_existing_numbers(*y, &mut seen))
+	})
+}
+
+fn columns_are_valid(input: &[Vec<char>]) -> bool {
+	let mut seen: HashSet<char> = HashSet::new();
+	(0..9).all(|x| {
+		seen.clear();
+		input
+			.iter()
+			.all(|y| verify_existing_numbers(y[x], &mut seen))
+	})
+}
+
+fn verify_existing_numbers(input: char, hash: &mut HashSet<char>) -> bool {
+	if input != '.' {
+		if hash.contains(&input) {
+			return false;
+		} else {
+			hash.insert(input);
+		}
 	}
-	
-	fn box_by_box(vec: Vec<char>, seen: &mut HashSet<char>) -> bool {
-	
-	    seen.clear();
-	    for (i, ch) in vec.iter().enumerate() {
-	        if !verify_existing_numbers(*ch, seen) {
-	            return false;
-	        }
-	
-	        if i == 8 || i == 17 {
-	            seen.clear();
-	        }
-	    }
-	    true
-	}
-	
-	fn rows_are_valid(input: &[Vec<char>], seen: &mut HashSet<char>) -> bool {
-	    seen.clear();
-	
-	    input.iter().all(|x| {
-	        seen.clear();
-	        x.iter().all(|y| verify_existing_numbers(*y, seen))
-	    })
-	}
-	
-	fn columns_are_valid(input: &[Vec<char>], seen: &mut HashSet<char>) -> bool {
-	    seen.clear();
-	
-	    (0..9).all(|x| {
-	        seen.clear();
-	        input.iter().all(|y| verify_existing_numbers(y[x], seen))
-	    })
-	}
-	
-	fn verify_existing_numbers(input: char, hash: &mut HashSet<char>) -> bool {
-	    if input != '.' {
-	        if hash.contains(&input) {
-	            return false;
-	        } else {
-	            hash.insert(input);
-	        }
-	    }
-	    true
-	} 
+	true
+}
