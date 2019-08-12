f9119f6db6dbd130d5e377a003daf6ed1b597b72
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-08-12 12:55:49 -0500

    refactoring code, functional programming

--- src/week1/exercise3/mod.rs
+++ src/week1/exercise3/mod.rs
@@ -1,14 +1,13 @@
 pub fn flip_and_invert_image(input: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    let mut inverted_matrix = vec![];
+    input.iter().map(|vector| {
+        vector
+            .iter()
+            .rev()
+            .map(|element| invert_element(*element))
+            .collect()
+    }).collect()
+}
 
-    for vector in &input {
-        let mut last_index = input[0].len();
-        let mut inverted_vector = vec![];
-        for _element in vector {
-            last_index -= 1;
-            inverted_vector.push(1 - vector[last_index]);
-        }
-        inverted_matrix.push(inverted_vector);
-    }
-    inverted_matrix
+fn invert_element(element: i32) -> i32 {
+    1 - element
 }
