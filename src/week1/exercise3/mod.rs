d6e1f75b358e4b5a7257ef97653f2194944a14cc
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-08-12 11:31:07 -0500

    Removing extra for loops by adding a substraction to change 1 -> 0 and 0 ->1

--- src/week1/exercise3/mod.rs
+++ src/week1/exercise3/mod.rs
@@ -1,26 +1,12 @@
 pub fn flip_and_invert_image(input: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    let mut reversed_matrix: Vec<Vec<i32>> = vec![];
-    let mut inverted_matrix: Vec<Vec<i32>> = vec![];
+    let mut inverted_matrix = vec![];
 
     for vector in &input {
         let mut last_index = input[0].len();
-        let mut reversed_vector: Vec<i32> = vec![];
+        let mut inverted_vector = vec![];
         for _element in vector {
             last_index -= 1;
-            reversed_vector.push(vector[last_index]);
-        }
-        reversed_matrix.push(reversed_vector);
-    }
-
-    for vector in &reversed_matrix {
-        let mut inverted_vector: Vec<i32> = vec![];
-        for element in vector {
-            if *element == 0 {
-                inverted_vector.push(1);
-            } else {
-                inverted_vector.push(0);
-            }
-
+            inverted_vector.push(1 - vector[last_index]);
         }
         inverted_matrix.push(inverted_vector);
     }
