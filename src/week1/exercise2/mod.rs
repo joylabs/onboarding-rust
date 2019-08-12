a505f3a87e154ce2a0291267a39e685375fc002e
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-08-12 09:37:34 -0500

    changing nested for loop

--- src/week1/exercise2/mod.rs
+++ src/week1/exercise2/mod.rs
@@ -3,7 +3,6 @@ pub fn transpose_matrix(input: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     for row_element in 0..input[0].len() {
         let mut row = vec![];
         for col_element in &input {
-            let col_element = col_element;
             row.push(col_element[row_element]);
         }
         output.push(row);
