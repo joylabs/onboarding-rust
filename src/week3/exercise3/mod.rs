7a2f04652d921df25000eedefcc8166b85256bbc
Nuria Reyes <nuria.reyes@joylabs.com>
2019-08-12 23:23:09 -0600

    Adding first solution to exercise 3 from week 3

--- src/week3/exercise3/mod.rs
+++ src/week3/exercise3/mod.rs
@@ -1,3 +1,15 @@
 pub fn is_subsequence(s: String, t: String) -> bool {
-      unimplemented!()
+      let vec_s: Vec<char> = s.chars().collect();
+      let filtered_t: Vec<char> = t.chars().filter(|c| vec_s.contains(c)).collect();
+
+      let mut i: usize = 0;
+      let mut j: usize = 0;
+      while j < vec_s.len() && i < filtered_t.len() {
+            if vec_s[j] == filtered_t[i] {
+                  j += 1;
+            }
+            i += 1;
+      }
+
+      j == vec_s.len()
 }
