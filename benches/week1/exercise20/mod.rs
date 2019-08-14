6fe6c8b352ccbf0773fc8ef0e0fddc1194c6c1f2
Andres Morales Reyes <sharker@Andress-MacBook-Pro.local>
2019-08-14 11:02:05 -0600

    Return to first implementation

--- benches/week1/exercise20/mod.rs
+++ benches/week1/exercise20/mod.rs
@@ -2,7 +2,7 @@
 use criterion::black_box;
 use criterion::Criterion;
 
-use onboarding_rust::week1::exercise20::{valid_anagram, valid_anagram_2};
+use onboarding_rust::week1::exercise20::valid_anagram;
 
 #[allow(dead_code)]
 fn bench_valid_anagram(c: &mut Criterion) {
@@ -16,17 +16,5 @@ fn bench_valid_anagram(c: &mut Criterion) {
     });
 }
 
-#[allow(dead_code)]
-fn bench_valid_anagram_2(c: &mut Criterion) {
-    c.bench_function("valid anagram 2", |b| {
-        b.iter(|| {
-            valid_anagram_2(
-                black_box("´ra&t".to_string()),
-                black_box("&ta´r".to_string()),
-            )
-        })
-    });
-}
-
-criterion_group!(benchmarks, bench_valid_anagram, bench_valid_anagram,);
+criterion_group!(benchmarks, bench_valid_anagram,);
 criterion_main!(benchmarks);
--- src/week1/exercise20/mod.rs
+++ src/week1/exercise20/mod.rs
@@ -1,7 +1,3 @@
-pub fn valid_anagram_2(input_1: String, input_2: String) -> bool {
-    !input_1.chars().any(|c| !input_2.contains(c))
-}
-
 pub fn valid_anagram(input_1: String, input_2: String) -> bool {
     let mut vec_input_1: Vec<char> = input_1.chars().collect();
     let mut vec_input_2: Vec<char> = input_2.chars().collect();
