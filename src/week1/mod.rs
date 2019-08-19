da1e99a36fb3e40dfcc9c3827b3b83205076b38e
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-08-19 15:01:21 -0500

    solution to exercise 12 and renaming exercise 11 test

--- src/week1/mod.rs
+++ src/week1/mod.rs
@@ -1,4 +1,7 @@
 pub mod exercise1;
+pub mod exercise10;
+pub mod exercise11;
+pub mod exercise12;
 pub mod exercise2;
 pub mod exercise3;
 pub mod exercise4;
@@ -7,5 +10,3 @@ pub mod exercise6;
 pub mod exercise7;
 pub mod exercise8;
 pub mod exercise9;
-pub mod exercise10;
-pub mod exercise11;
--- tests/week1/exercise11/mod.rs
+++ tests/week1/exercise11/mod.rs
@@ -1,7 +1,7 @@
 use onboarding_rust::week1::exercise11::reverse_words;
 
 #[test]
-fn test_week1_exercise10_usa() {
+fn test_week1_exercise11_reverse() {
     let input = String::from("Let's take LeetCode contest");
     let output = String::from("s'teL ekat edoCteeL tsetnoc");
     assert_eq!(output, reverse_words(input));
