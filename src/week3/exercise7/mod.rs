db3ea6cbfba50aae1a47bf4751afc2cfe575adaa
Nuria Reyes <nuria.reyes@joylabs.com>
2019-08-23 10:52:11 -0600

    Adding solution to Array Partition exercise

--- src/week3/exercise7/mod.rs
+++ src/week3/exercise7/mod.rs
@@ -1,3 +1,17 @@
 pub fn peak_index_in_mountain_array(a: Vec<i32>) -> i32 {
-      unimplemented!();
+    let (mut min, mut max) = (0, a.len() - 1);
+
+    while min <= max {
+        let middle_index = ((min + max) as f32 / 2.0).ceil() as usize;
+
+        if a[middle_index] > a[middle_index + 1] && a[middle_index] > a[middle_index - 1] {
+            return middle_index as i32;
+        } else if a[middle_index] < a[middle_index + 1] {
+            min = middle_index + 1;
+        } else if a[middle_index] > a[middle_index + 1] {
+            max = middle_index - 1;
+        }
+    }
+
+    0
 }
