21a80db4d2b3945109a25f24c3dc66b8f07cfe45
Nuria Reyes <nuria.reyes@joylabs.com>
2019-08-23 16:37:32 -0600

    Adding some code to help solve exercise

--- src/week3/exercise5/mod.rs
+++ src/week3/exercise5/mod.rs
@@ -1,3 +1,36 @@
 pub fn partition_labels(s: String) -> Vec<i32> {
-      unimplemented!();
+      let mut result: Vec<i32> = Vec::new();
+      let mut indexes: Vec<Vec<i32>> = Vec::new();
+      let mut letters: Vec<char> = Vec::new();
+      
+      s.chars().for_each(|c|{
+            if !letters.contains(&c) {
+                  letters.push(c);
+            }
+      });
+
+      letters.iter().for_each(|c| {
+            
+      });
+      
+      result
+}
+
+fn find_last_index(letter: char, word: &str) -> i32 {
+      word.chars().enumerate().fold(0, |acc, (i, c)| {
+            if letter == c {
+                  i as i32 + 1
+            } else {
+                  acc
+            }
+      })
+}
+
+fn is_greater(index: i32, nums: &[i32]) -> bool {
+      for n in nums.iter() {
+            if *n > index {
+                  return false;
+            }
+      }
+      true
 }
