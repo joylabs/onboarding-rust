afbba99024f450b49b0d6faea8df7a7e68fc15b3
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-08-26 16:01:54 -0500

    use iterators

--- src/week3/exercise8/mod.rs
+++ src/week3/exercise8/mod.rs
@@ -1,12 +1,7 @@
 pub fn array_pair_sum(mut nums: Vec<i32>) -> i32 {
       nums.sort();
-      let mut i = 0;
-      let mut sum = 0;
-
-      while i < nums.len() {
-            sum += nums[i];
-            i += 2;
-      }
-
-      sum
+      nums.iter()
+            .enumerate()
+            .filter(|(i, _)| i % 2 == 0)
+            .fold(0, |acc, n| acc + n.1)
 }
