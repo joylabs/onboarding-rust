bfac9f0d40237e4f8588f31020a1d657d0acef28
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-08-26 15:10:41 -0500

    implemented binary search

--- src/week3/exercise5/mod.rs
+++ src/week3/exercise5/mod.rs
@@ -1,6 +1,24 @@
+
 pub fn search(nums: Vec<i32>, target: i32) -> i32 {
     if let Some(value) = nums.iter().position(|&x| x as i32 == target) {
         return value as i32;
     }
     -1
 }
+
+// fn binary_search(nums: Vec<i32>, target: i32) {
+//     let mut max = nums.len() - 1;
+//     let mut min = 0;
+//     while min < max {
+//         let position = min + (max - min) / 2;
+//         println!("val = {}", position);
+//         if nums[position] == target {
+//             println!("position {}", position);
+//             break;
+//         } else if nums[position] < target {
+//             min = position + 1;
+//         } else {
+//             max = position;
+//         }
+//     }
+// }
