761711668a7e1ef0a972e189970547801702a177
Citlali09 <51176910+Citlali09@users.noreply.github.com>
2019-08-26 15:42:19 -0500

    fix_solution

--- src/week3/exercise5/mod.rs
+++ src/week3/exercise5/mod.rs
@@ -6,19 +6,18 @@ pub fn search(nums: Vec<i32>, target: i32) -> i32 {
     -1
 }
 
-// fn binary_search(nums: Vec<i32>, target: i32) {
-//     let mut max = nums.len() - 1;
+// pub fn binary_search(nums: Vec<i32>, target: i32) -> i32 {
+//     let mut max = nums.len();
 //     let mut min = 0;
 //     while min < max {
-//         let position = min + (max - min) / 2;
-//         println!("val = {}", position);
+//         let position = (max + min) / 2;
 //         if nums[position] == target {
-//             println!("position {}", position);
-//             break;
+//             return position as i32;
 //         } else if nums[position] < target {
 //             min = position + 1;
 //         } else {
 //             max = position;
 //         }
 //     }
+//     -1
 // }
