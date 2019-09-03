f4f2f435635fc55301f92af27ac3c2cbf148bc7b
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-09-03 16:53:19 -0500

    Best Time to Buy and Sell Stock

---
+++ src/week3/exercise10/mod.rs
@@ -0,0 +1,17 @@
+pub fn max_profit(prices: Vec<i32>) -> i32 {
+	if prices.is_empty() {
+		return 0;
+	}
+	let mut max = 0;
+	let mut min = *prices.iter().max().unwrap();
+
+	for v in prices {
+		if min > v {
+			min = v;
+		} else if v - min > max {
+			max = v - min;
+		}
+	}
+	max
+}
+
