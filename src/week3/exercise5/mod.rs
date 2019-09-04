612369c55435d7a803cb95df94b2df743dee52b2
Nuria Reyes <nuria.reyes@joylabs.com>
2019-09-04 17:17:26 -0600

    Refactoring exercise

--- src/week3/exercise5/mod.rs
+++ src/week3/exercise5/mod.rs
@@ -1,44 +1,68 @@
 pub fn partition_labels(s: String) -> Vec<i32> {
-      let mut result: Vec<i32> = Vec::new();
-      let mut letter_indexes: Vec<Vec<i32>> = Vec::new();
-      let mut letters: Vec<char> = Vec::new();
-      
-      s.chars().enumerate().for_each(|(i, c)|{
-            if !letters.contains(&c) {
-                  let last_index = find_last_index(c, &s) as i32;
-                  letters.push(c);
-                  letter_indexes.push(vec![i as i32, last_index]);
-            }
-      });
+    let letter_indexes = get_start_end_letter_indexes(&s);
+    let letter_indexes_len = letter_indexes.len() - 1;
 
-      let mut group_indexes = letter_indexes[0].clone();
-      letter_indexes.iter().skip(1).for_each(|indexes| {
-            if is_intersection(&group_indexes, &indexes) {
-                  if group_indexes[1] < indexes[1] {
+    letter_indexes
+        .into_iter()
+        .enumerate()
+        .fold(
+            (Vec::new(), Vec::new()),
+            |(mut group_indexes, mut result), (i, indexes)| {
+                if i == 0 {
+                    (indexes, Vec::new())
+                } else {
+                    let intersection = is_intersection(&group_indexes, &indexes);
+                    if intersection && group_indexes[1] < indexes[1] {
                         group_indexes[1] = indexes[1];
-                  }
-            } else {
-                  let distance = group_indexes[1] - group_indexes[0] + 1;
-                  result.push(distance);
-                  group_indexes = indexes.clone();
-            }
-      });
-      let distance = group_indexes[1] - group_indexes[0] + 1;
-      result.push(distance);
-      result
+                    }
+
+                    if i == letter_indexes_len {
+                        let distance = group_indexes[1] - group_indexes[0] + 1;
+                        result.push(distance);
+
+                        if !intersection {
+                            let distance = indexes[1] - indexes[0] + 1;
+                            result.push(distance);
+                        }
+
+                        (indexes, result)
+                    } else if intersection {
+                        (group_indexes, result)
+                    } else {
+                        let distance = group_indexes[1] - group_indexes[0] + 1;
+                        result.push(distance);
+                        (indexes, result)
+                    }
+                }
+            },
+        )
+        .1
 }
 
-fn find_last_index(letter: char, word: &str) -> usize {
-      word.chars().enumerate().fold(0, |acc, (i, c)| {
-            if letter == c {
-                  i
-            } else {
-                  acc
-            }
-      })
+fn get_start_end_letter_indexes(word: &str) -> Vec<Vec<i32>> {
+    word.chars()
+        .enumerate()
+        .fold(
+            (Vec::new(), Vec::new()),
+            |(mut letters, mut group_indexes), (i, c)| {
+                if !letters.contains(&c) {
+                    let last_index = find_last_index(c, word);
+                    group_indexes.push(vec![i as i32, last_index]);
+                    letters.push(c);
+                }
+                (letters, group_indexes)
+            },
+        )
+        .1
+}
+
+fn find_last_index(letter: char, word: &str) -> i32 {
+    word.chars()
+        .enumerate()
+        .fold(0, |acc, (i, c)| if letter == c { i as i32 } else { acc })
 }
 
 //Assuming x1 start point is <= x2 start point
 fn is_intersection(x1: &[i32], x2: &[i32]) -> bool {
-      x1[1] >= x2[0]
+    x1[1] >= x2[0]
 }
