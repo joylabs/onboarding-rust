83a8eedf1b7734eb73ed87c2af3328f7e5345582
Andres Morales Reyes <sharker@Andress-MacBook-Pro.local>
2019-09-04 13:06:17 -0600

    Alex implementation, it's faster and clearer

--- src/week1/exercise21/mod.rs
+++ src/week1/exercise21/mod.rs
@@ -1,4 +1,27 @@
 pub fn find_circle_num(input: Vec<Vec<i32>>) -> i32 {
+    let n = input.len();
+    let mut circles = 0;
+    let mut visited = vec![0; n];
+    (0..n).for_each(|i| {
+        if visited[i] == 0 {
+            dfs(i, &input, &mut visited);
+            circles += 1;
+        }
+    });
+    circles
+}
+
+
+fn dfs(i: usize, m: &[Vec<i32>], visited: &mut Vec<i32>) {
+    visited[i] = 1;
+    (0..m.len()).for_each(|j| {
+        if m[i][j] == 1 && visited[j] == 0 {
+            dfs(j, m, visited);
+        }
+    });
+}
+
+pub fn find_circle_num_2(input: Vec<Vec<i32>>) -> i32 {
     let mut friends_relation = input.clone();
 
     (0..friends_relation.len()).fold(0, |circles, i| {
@@ -6,18 +29,14 @@ pub fn find_circle_num(input: Vec<Vec<i32>>) -> i32 {
             + (0..friends_relation[0].len()).fold(0, |circle, j| {
                 if friends_relation[i][j] == 1 {
                     searching_circle(&mut friends_relation, i, j);
-                    println!("i: {}, j: {}, \n {:?}", i, j, friends_relation);
                     circle + 1
                 } else {
-                    println!("i: {}, j: {}, \n {:?}", i, j, friends_relation);
                     circle
                 }
             })
     })
 }
 fn searching_circle(friends_relation: &mut Vec<Vec<i32>>, i: usize, j: usize) {
-
-    println!("S i: {}, j: {}, \n {:?}", i, j, friends_relation);
 
     friends_relation[i][j] = 0;
 
