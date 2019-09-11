b039b0c8712b992b5782f5a9d0767978f15b2214
Nuria Reyes <nuria.reyes@joylabs.com>
2019-09-10 23:14:59 -0600

    Refactoring code followig some recommendations in PR

--- src/week2/exercise23/mod.rs
+++ src/week2/exercise23/mod.rs
@@ -1,47 +1,72 @@
 pub fn find_words(board: Vec<Vec<char>>, words: Vec<String>) -> Vec<String> {
-        if board.is_empty() {
-                return vec![];
-        }
+    if board.is_empty() {
+        return vec![];
+    }
 
-        words.into_iter().filter(|w| find_word(board.clone(), w.chars().collect())).collect()
+    words
+        .into_iter()
+        .filter(|w| find_word(&board, w.chars().collect()))
+        .collect()
 }
 
-fn find_word(mut board: Vec<Vec<char>>, word: Vec<char>) -> bool {
-        for row in 0..board.len() {
-                for col in 0..board[0].len() {
-                        if search_for_word(&mut board, &word, 0, row as i32, col as i32) {
-                                return true;
-                        }
-                }
-        }
+fn find_word(board: &[Vec<char>], word: Vec<char>) -> bool {
+    let mut visited_in_board = vec![vec![false; board[0].len()]; board.len()];
 
-        false
-}
-
-fn search_for_word(mut board: &mut Vec<Vec<char>>, word: &[char], position: usize, row: i32, col: i32) -> bool {
-        if row < 0 || 
-        row >= board.len() as i32 || 
-        col < 0 || 
-        col >= board[0].len() as i32 || 
-        board[row as usize][col as usize] != word[position] || 
-        board[row as usize][col as usize] == '-' {
-                return false;
-        }
-
-        if position + 1 == word.len() {
+    for row in 0..board.len() {
+        for col in 0..board[0].len() {
+            if search_for_word(
+                board,
+                &mut visited_in_board,
+                &word,
+                0,
+                row as i32,
+                col as i32,
+            ) {
                 return true;
+            }
         }
+    }
 
-        let value = board[row as usize][col as usize];
-        board[row as usize][col as usize] = '-';
-        let new_position = position + 1;
+    false
+}
 
-        let found = search_for_word(&mut board, &word, new_position, row + 1, col) ||
-                search_for_word(&mut board, &word, new_position, row - 1, col) ||
-                search_for_word(&mut board, &word, new_position, row, col + 1) ||
-                search_for_word(&mut board, &word, new_position, row, col - 1);
+fn search_for_word(
+    board: &[Vec<char>],
+    visited_in_board: &mut Vec<Vec<bool>>,
+    word: &[char],
+    position: usize,
+    row: i32,
+    col: i32,
+) -> bool {
+    if is_out_of_bounds(row, col, board.len() as i32, board[0].len() as i32)
+        || have_visited(&visited_in_board, row, col)
+        || board[row as usize][col as usize] != word[position]
+    {
+        return false;
+    }
 
-        board[row as usize][col as usize] = value;
-        
-        found
+    let new_position = position + 1;
+
+    if new_position == word.len() {
+        return true;
+    }
+
+    visited_in_board[row as usize][col as usize] = true;
+
+    let found = search_for_word(board, visited_in_board, &word, new_position, row + 1, col)
+        || search_for_word(board, visited_in_board, &word, new_position, row - 1, col)
+        || search_for_word(board, visited_in_board, &word, new_position, row, col + 1)
+        || search_for_word(board, visited_in_board, &word, new_position, row, col - 1);
+
+    visited_in_board[row as usize][col as usize] = false;
+
+    found
+}
+
+fn is_out_of_bounds(row: i32, col: i32, board_heigth: i32, board_width: i32) -> bool {
+    row < 0 || row >= board_heigth || col < 0 || col >= board_width
+}
+
+fn have_visited(visited_in_board: &[Vec<bool>], row: i32, col: i32) -> bool {
+    visited_in_board[row as usize][col as usize]
 }
--- tests/week2/exercise23/mod.rs
+++ tests/week2/exercise23/mod.rs
@@ -3,13 +3,52 @@ use onboarding_rust::week2::exercise23::find_words;
 #[test]
 fn test_week2_exercise23_example1() {
     let board = vec![
-        vec!['o','a','a','n'],
-        vec!['e','t','a','e'],
-        vec!['i','h','k','r'],
-        vec!['i','f','l','v']
+        vec!['o', 'a', 'a', 'n'],
+        vec!['e', 't', 'a', 'e'],
+        vec!['i', 'h', 'k', 'r'],
+        vec!['i', 'f', 'l', 'v'],
     ];
-    let words = vec!["oath".to_string(),"pea".to_string(),"eat".to_string(),"rain".to_string()];
-    let expected = vec!["oath".to_string(),"eat".to_string()];
+    let words = vec![
+        "oath".to_string(),
+        "pea".to_string(),
+        "eat".to_string(),
+        "rain".to_string(),
+    ];
+    let expected = vec!["oath".to_string(), "eat".to_string()];
     assert_eq!(expected, find_words(board, words));
 }
 
+#[test]
+fn test_week2_exercise23_example2() {
+    let board = vec![];
+    let words = vec![
+        "oath".to_string(),
+        "pea".to_string(),
+        "eat".to_string(),
+        "rain".to_string(),
+    ];
+    let expected: Vec<String> = vec![];
+    assert_eq!(expected, find_words(board, words));
+}
+
+#[test]
+fn test_week2_exercise23_example3() {
+    let board = vec![vec!['a', 'b'], vec!['a', 'a']];
+    let words = vec![
+        "aba".to_string(),
+        "baa".to_string(),
+        "bab".to_string(),
+        "aaab".to_string(),
+        "aaa".to_string(),
+        "aaaa".to_string(),
+        "aaba".to_string(),
+    ];
+    let expected = vec![
+        "aba".to_string(),
+        "baa".to_string(),
+        "aaab".to_string(),
+        "aaa".to_string(),
+        "aaba".to_string(),
+    ];
+    assert_eq!(expected, find_words(board, words));
+}
