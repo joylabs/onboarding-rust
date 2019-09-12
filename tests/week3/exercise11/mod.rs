c10816d354af6b1a650c040f2f4e04f3404dd8e1
Nuria Reyes <nuria.reyes@joylabs.com>
2019-09-11 22:31:14 -0600

    Adding solution to Best time to buy and sell stock exercise

--- tests/week3/exercise11/mod.rs
+++ tests/week3/exercise11/mod.rs
@@ -2,14 +2,28 @@ use onboarding_rust::week3::exercise11::max_profit;
 
 #[test]
 fn test_week3_exercise11_example1() {
-    let input = vec![7,1,5,3,6,4];
+    let input = vec![7, 1, 5, 3, 6, 4];
     let expected = 5;
     assert_eq!(expected, max_profit(input));
 }
 
 #[test]
 fn test_week3_exercise11_example2() {
-    let input = vec![7,6,4,3,1];
+    let input = vec![7, 6, 4, 3, 1];
+    let expected = 0;
+    assert_eq!(expected, max_profit(input));
+}
+
+#[test]
+fn test_week3_exercise11_example3() {
+    let input = vec![2, 4, 1];
+    let expected = 2;
+    assert_eq!(expected, max_profit(input));
+}
+
+#[test]
+fn test_week3_exercise11_example4() {
+    let input = vec![];
     let expected = 0;
     assert_eq!(expected, max_profit(input));
 }
