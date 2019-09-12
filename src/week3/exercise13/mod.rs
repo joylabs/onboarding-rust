ef06dbc003e9c3d3a735d63e4c4ec90f8afbcd18
Nuria Reyes <nuria.reyes@joylabs.com>
2019-09-12 16:33:28 -0600

    Adding solution to house robber exercise

--- src/week3/exercise13/mod.rs
+++ src/week3/exercise13/mod.rs
@@ -1,3 +1,39 @@
+use std::cmp::max;
+
 pub fn rob(nums: Vec<i32>) -> i32 {
-    unimplemented!()
+    if nums.len() == 1 {
+        return nums[0];
+    }
+
+    max(
+        get_robbed_money(&nums, Houses::EVEN),
+        get_robbed_money(&nums, Houses::ODD),
+    )
+}
+
+fn get_robbed_money(nums: &[i32], houses_evaluated: Houses) -> i32 {
+    match houses_evaluated {
+        Houses::EVEN => {
+            nums.iter()
+                .rev()
+                .skip(1)
+                .fold((0, 0), |(current, previous), house| {
+                    (max(house + previous, current), current)
+                })
+                .0
+        }
+        Houses::ODD => {
+            nums.iter()
+                .skip(1)
+                .fold((0, 0), |(current, previous), house| {
+                    (max(house + previous, current), current)
+                })
+                .0
+        }
+    }
+}
+
+enum Houses {
+    ODD,
+    EVEN,
 }
--- tests/week3/exercise13/mod.rs
+++ tests/week3/exercise13/mod.rs
@@ -2,14 +2,28 @@ use onboarding_rust::week3::exercise13::rob;
 
 #[test]
 fn test_week3_exercise13_example1() {
-    let input = vec![2,3,2];
+    let input = vec![2, 3, 2];
     let expected = 3;
     assert_eq!(expected, rob(input));
 }
 
 #[test]
 fn test_week3_exercise13_example2() {
-    let input = vec![1,2,3,1];
+    let input = vec![1, 2, 3, 1];
     let expected = 4;
+    assert_eq!(expected, rob(input));
+}
+
+#[test]
+fn test_week3_exercise13_example3() {
+    let input = vec![];
+    let expected = 0;
+    assert_eq!(expected, rob(input));
+}
+
+#[test]
+fn test_week3_exercise13_example4() {
+    let input = vec![1];
+    let expected = 1;
     assert_eq!(expected, rob(input));
 }
