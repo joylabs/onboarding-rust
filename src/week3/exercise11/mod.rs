0a6f52720769c5cf95e8fd8d0d983930b5de3f9e
Nuria Reyes <nuria.reyes@joylabs.com>
2019-09-14 19:46:37 -0600

    Separating conditional into another function

--- src/week3/exercise11/mod.rs
+++ src/week3/exercise11/mod.rs
@@ -9,13 +9,17 @@ pub fn max_profit(prices: Vec<i32>) -> i32 {
         .into_iter()
         .skip(1)
         .fold((0, min_price), |(max_prof, min_price), price| {
-            if price < min_price {
-                (max_prof, price)
-            } else if price - min_price > max_prof {
-                (price - min_price, min_price)
-            } else {
-                (max_prof, min_price)
-            }
+            eval_price(max_prof, min_price, price)
         })
         .0
+}
+
+fn eval_price(max_prof: i32, min_price: i32, price: i32) -> (i32, i32) {
+    if price < min_price {
+        (max_prof, price)
+    } else if price - min_price > max_prof {
+        (price - min_price, min_price)
+    } else {
+        (max_prof, min_price)
+    }
 }
