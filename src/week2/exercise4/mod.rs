4a83936f7e2828bf22164398dfd8f3b501284f19
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-09-25 17:13:27 -0500

    changing to '' instead of

--- src/week2/exercise4/mod.rs
+++ src/week2/exercise4/mod.rs
@@ -4,7 +4,7 @@ pub fn uncommon_from_sentences(a: String, b: String) -> Vec<String> {
     let mut words_count: HashMap<String, i32> = HashMap::new();
     let sentence = format!("{} {}", a, b);
 
-    sentence.split(" ").for_each(|word| {
+    sentence.split(' ').for_each(|word| {
         *words_count.entry(word.to_string()).or_insert(0) += 1;
     });
 
