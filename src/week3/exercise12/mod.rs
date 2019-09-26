b767d50f9d302aaeb97d68eba0a3fd2bca3a132f
Nuria Reyes <nuria.reyes@joylabs.com>
2019-09-26 16:01:24 -0600

    Some progress to the exercise, still not solutioned

--- src/week3/exercise12/mod.rs
+++ src/week3/exercise12/mod.rs
@@ -1,3 +1,12 @@
 pub fn min_distance(word1: String, word2: String) -> i32 {
-        unimplemented!()
+        let mut moves = 0;
+        if word1.len() != word2.len() {
+                moves += (word1.len() as i32 - word2.len() as i32).abs();
+        }
+        let equal_chars = word1
+                .chars()
+                .filter(|&c| word2.contains(c))
+                .collect::<Vec<char>>();
+        println!("equal_chars: {:?}", equal_chars);
+        moves
 }
