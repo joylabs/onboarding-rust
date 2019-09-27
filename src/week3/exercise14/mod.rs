3e7602bc8abe629468e9994b016eadafe1f172f1
Nuria Reyes <nuria.reyes@joylabs.com>
2019-09-26 19:17:15 -0600

    Still 1 test failing

--- src/week3/exercise14/mod.rs
+++ src/week3/exercise14/mod.rs
@@ -11,41 +11,46 @@ pub fn is_match(s: String, p: String) -> bool {
     let mut p: Vec<char> = p.chars().collect();
     let mut s: Vec<char> = s.chars().collect();
 
-    let mut preceding_element = p.remove(0);
-    let mut actual_letter = s.remove(0);
+    let preceding_element = p.remove(0);
+    let actual_letter = s.remove(0);
 
-    while !s.is_empty() && !p.is_empty() {
-        if actual_letter == preceding_element || preceding_element == '.' {
-            actual_letter = s.remove(0);
-            if p[0] != '*' {
-                preceding_element = p.remove(0);
-            }
-        } else if p[0] == '*' {
-            p.remove(0);
-            if !p.is_empty() {
-                preceding_element = p.remove(0);
-            } else {
-                preceding_element = '-';
-            }
+    dfs_pattern(actual_letter, preceding_element, &s, &p)
+}
 
-            if actual_letter == preceding_element || preceding_element == '.' {
-                actual_letter = s.remove(0);
-                if !p.is_empty() && p[0] != '*' {
-                    preceding_element = p.remove(0);
-                }
-            }
-        } else {
-            return false;
-        }
+fn dfs_pattern(
+    mut actual_letter: char,
+    mut preceding_element: char,
+    s: &[char],
+    p: &[char],
+) -> bool {
+    println!(
+        "preceding_element: {}, actual_letter: {}, s: {:?}, p: {:?}",
+        preceding_element, actual_letter, s, p
+    );
 
-        if p.is_empty() {
-            preceding_element = '-';
-        }
+    if !s.is_empty() && p.is_empty() {
+        return false;
     }
 
-    println!("preceding_element: {}, p: {:?}", preceding_element, p);
+    if actual_letter == preceding_element || preceding_element == '.' {
+        if s.is_empty() {
+            return true;
+        }
+        actual_letter = s[0];
+        if !p.is_empty() && p[0] != '*' {
+            preceding_element = p[0];
+            dfs_pattern(actual_letter, preceding_element, &s[1..], &p[1..]);
+        } else {
+            dfs_pattern(actual_letter, preceding_element, &s[1..], p);
+        }
+    } else if !p.is_empty() && p[0] == '*' {
+        if p.len() >= 2 {
+            preceding_element = p[1];
+        }
+        dfs_pattern(actual_letter, preceding_element, s, &p[2..]);
+    } else {
+        return false;
+    }
 
     actual_letter == preceding_element || preceding_element == '.'
 }
-
-fn dfs_pattern() {}
