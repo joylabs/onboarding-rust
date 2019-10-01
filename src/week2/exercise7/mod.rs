3404effd5bc2b047d86ab81fa1aefba1199ca31b
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-10-01 13:47:31 -0500

    changing fold for collect

--- src/week2/exercise7/mod.rs
+++ src/week2/exercise7/mod.rs
@@ -1,14 +1,10 @@
 use std::collections::HashSet;
 
 pub fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
-    let set = nums1
+    nums1
         .into_iter()
         .filter(|n| nums2.contains(n))
-        .fold(&mut HashSet::new(), |acc, num| {
-            acc.insert(num);
-            acc
-        })
-        .clone();
-
-    set.into_iter().collect()
+        .collect::<HashSet<i32>>()
+        .into_iter()
+        .collect::<Vec<i32>>()
 }
