9a15ffedb188d5e964dbc6abdfaa6e97cb86430d
Nuria Reyes <nuria.reyes@joylabs.com>
2019-10-01 22:55:33 -0600

    Adding solution with memoization

--- src/week3/exercise14/mod.rs
+++ src/week3/exercise14/mod.rs
@@ -1,13 +1,28 @@
+use std::collections::HashMap;
+
 pub fn is_match(s: String, p: String) -> bool {
-    if p.is_empty() {
-        return s.is_empty();
-    }
-
-    let first_check = !s.is_empty() && (&p[0..1] == &s[0..1] || &p[0..1] == ".");
-
-    if p.len() >= 2 && &p[1..2] == "*" {
-        is_match(s.clone(), (&p[2..]).to_string()) || (first_check && is_match((&s[1..]).to_string(), p.clone()))
-    } else {
-        first_check && is_match((&s[1..]).to_string(), (&p[1..]).to_string())
-    }
+    let mut mem_results: HashMap<(usize, usize), bool> = HashMap::new();
+    check_pattern(0, 0, &s, &p, &mut mem_results)
 }
+
+fn check_pattern(i: usize, j: usize, s: &str, p: &str, mem_results: &mut HashMap<(usize, usize), bool>) -> bool {
+    if mem_results.contains_key(&(i, j)) {
+        return *(mem_results.get(&(i, j)).unwrap());
+    }
+
+    let result;
+
+    if j == p.len() {
+        result = i == s.len();
+    } else {
+        let first_check = i < s.len() && (&p[j..j+1] == &s[i..i+1] || &p[j..j+1] == ".");
+        if j + 1 < p.len() && &p[j+1..j+2] == "*" {
+            result = check_pattern(i, j + 2, s, p, mem_results) || first_check && check_pattern(i + 1, j, s, p, mem_results);
+        } else {
+            result = first_check && check_pattern(i + 1, j + 1, s, p, mem_results);
+        }
+    }
+    mem_results.insert((i, j), result);
+    result
+}
+
