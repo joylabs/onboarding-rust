dc8b3c3a82ff1e8ab561d18c22c30a14e4df0da5
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-10-02 14:53:20 -0500

    Adding files to match betty-master

--- src/week2/exercise7/mod.rs
+++ src/week2/exercise7/mod.rs
@@ -1,1 +1,10 @@
+use std::collections::HashSet;
 
+pub fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+    nums1
+        .into_iter()
+        .filter(|n| nums2.contains(n))
+        .collect::<HashSet<i32>>()
+        .into_iter()
+        .collect::<Vec<i32>>()
+}
