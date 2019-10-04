08b4c7ca9a63d2012abce1716b899c49feb7663d
Nuria Reyes <nuria.reyes@joylabs.com>
2019-10-04 16:41:28 -0600

    Adding solution to edit distance exercise

--- src/week3/exercise12/mod.rs
+++ src/week3/exercise12/mod.rs
@@ -1,61 +1,28 @@
 use std::collections::HashMap;
 
 pub fn min_distance(word1: String, word2: String) -> i32 {
-    if word1.is_empty() {
-        return word2.len() as i32;
-    } else if word2.is_empty() {
-        return word1.len() as i32;
-    } else if word1 == word2 {
-        return 0;
-    }
-    
-    let mut mem_matches: HashMap<usize, i32> = HashMap::new();
-    find_best_pattern(0, 0, &word1, &word2, 0, 0, &mut mem_matches);
-
-    println!("mem_matches: {:?}", mem_matches);
-
-    if let Some(matches) = mem_matches.values().max() {
-        if word1.len() >= word2.len() {
-            (word1.len() as i32 - matches).abs()
-        } else {
-            (word2.len() as i32 - matches).abs()
-        }
-    } else {
-        (word1.len() as i32 - word2.len() as i32).abs()
-    }
+    let mut mem_matches: HashMap<(usize, usize), i32> = HashMap::new();
+    find_best_pattern(0, 0, &word1, &word2, &mut mem_matches)
 }
 
-fn find_best_pattern(i: usize, j: usize, word1: &str, word2: &str, match_count: i32, first_position: usize, mem_matches: &mut HashMap<usize, i32>) {
-    println!("i: {}, j: {}, word1: {}, word2: {}, match_count: {}, first_pos: {}, mem_matches: {:?}", i, j, word1, word2, match_count, first_position, mem_matches);
-    if i == word1.len() || first_position == word2.len() || j == word2.len() {
-        mem_matches.insert(first_position, match_count);
-        return;
-    }
-
-    if let Some(position) = find_letter_position(&word1[i..], &word2[j..=j]) {
-        println!("found");
-        find_best_pattern(position + 1, j + 1, word1, word2, match_count + 1, first_position, mem_matches);
+fn find_best_pattern(i: usize, j: usize, word1: &str, word2: &str, mem_matches: &mut HashMap<(usize, usize), i32>) -> i32 {
+    if mem_matches.contains_key(&(i, j)) {
+        *mem_matches.get(&(i, j)).unwrap()
+    } else if i == word1.len() && j == word2.len() {
+        0
+    } else if i == word1.len() {
+        1 + find_best_pattern(i, j + 1, word1, word2, mem_matches)
+    } else if j == word2.len() {
+        1 + find_best_pattern(i + 1, j, word1, word2, mem_matches)
+    } else if word1[i..=i] == word2[j..=j] {
+        find_best_pattern(i + 1, j + 1, word1, word2, mem_matches)
     } else {
-        println!("not found");
-        mem_matches.insert(first_position, match_count);
-        find_best_pattern(0, first_position + 1, word1, word2, 0, first_position + 1, mem_matches);
-    }
-}
+        let mut possible_changes: Vec<i32> = Vec::with_capacity(3);
+        possible_changes.push(1 + find_best_pattern(i, j + 1, word1, word2, mem_matches));
+        possible_changes.push(1 + find_best_pattern(i + 1, j, word1, word2, mem_matches));
+        possible_changes.push(1 +  find_best_pattern(i + 1, j + 1, word1, word2, mem_matches));
 
-fn find_letter_position(word: &str, letter: &str) -> Option<usize> {
-    if !word.contains(letter) {
-        None
-    } else {
-        Some(
-            word.char_indices()
-                .fold((0, false), |(position, found), (i, c)| {
-                    if !found && &(c.to_string()) == letter {
-                        (i, true)
-                    } else {
-                        (position, found)
-                    }
-                })
-                .0,
-        )
+        mem_matches.insert((i, j), *possible_changes.iter().min().unwrap());
+        *mem_matches.get(&(i, j)).unwrap()
     }
 }
