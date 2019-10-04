0207ea7e66b3f0a848ab1bf378793800f3d8bec2
Nuria Reyes <nuria.reyes@joylabs.com>
2019-10-04 16:55:07 -0600

    Making clippy happy

--- src/week3/exercise12/mod.rs
+++ src/week3/exercise12/mod.rs
@@ -6,10 +6,10 @@ pub fn min_distance(word1: String, word2: String) -> i32 {
 }
 
 fn find_best_pattern(i: usize, j: usize, word1: &str, word2: &str, mem_matches: &mut HashMap<(usize, usize), i32>) -> i32 {
-    if mem_matches.contains_key(&(i, j)) {
-        *mem_matches.get(&(i, j)).unwrap()
-    } else if i == word1.len() && j == word2.len() {
+    if i == word1.len() && j == word2.len() {
         0
+    } else if mem_matches.contains_key(&(i, j)) {
+        *mem_matches.get(&(i, j)).unwrap()
     } else if i == word1.len() {
         1 + find_best_pattern(i, j + 1, word1, word2, mem_matches)
     } else if j == word2.len() {
@@ -22,7 +22,6 @@ fn find_best_pattern(i: usize, j: usize, word1: &str, word2: &str, mem_matches: 
         possible_changes.push(1 + find_best_pattern(i + 1, j, word1, word2, mem_matches));
         possible_changes.push(1 +  find_best_pattern(i + 1, j + 1, word1, word2, mem_matches));
 
-        mem_matches.insert((i, j), *possible_changes.iter().min().unwrap());
-        *mem_matches.get(&(i, j)).unwrap()
+        *mem_matches.entry((i, j)).or_insert(*possible_changes.iter().min().unwrap())
     }
 }
