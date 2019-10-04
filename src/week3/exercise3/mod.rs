4a6a6651b27ec3c77408a201145234a25cc05cdf
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-10-04 12:28:36 -0500

    change incongruencias

--- src/week3/exercise3/mod.rs
+++ src/week3/exercise3/mod.rs
@@ -1,17 +1,16 @@
 pub fn find_min_arrow_shots(mut points: Vec<Vec<i32>>) -> i32 {
-
     if points.is_empty() {
         return 0;
     }
     points.sort();
     let mut arrow = points[0][1];
 
-    points.iter().fold(1, |mut acc, ball| {
-        if ball[0] <= arrow && arrow >= ball[1] {
-            arrow = ball[1];
-        } else if ball[0] > arrow {
+    points.iter().skip(1).fold(1, |mut acc, balloon| {
+        if arrow >= balloon[1] {
+            arrow = balloon[1];
+        } else if balloon[0] > arrow {
             acc += 1;
-            arrow = ball[1];
+            arrow = balloon[1];
         }
         acc
     })
