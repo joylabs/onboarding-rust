b198050c77154896c17890b2ad6ea9c5ea9d7fb4
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-10-04 11:48:03 -0500

    adding function to validate numbers in sub-boxes

--- src/week2/exercise8/mod.rs
+++ src/week2/exercise8/mod.rs
@@ -21,8 +21,32 @@ fn check_col(board: &Vec<Vec<char>>) -> bool {
 }
 
 fn check_box(board: &Vec<Vec<char>>) -> bool {
-    let mut flag = true;
-    flag
+    (0..9).all(|i| {
+        let sub_box = build_box(
+            board,
+            (i / 3) * 3,
+            (1 + i / 3) * 3,
+            (i % 3) * 3,
+            (1 + i % 3) * 3,
+        );
+        has_unique_elements(sub_box)
+    })
+}
+
+fn build_box(
+    board: &Vec<Vec<char>>,
+    row_start: i32,
+    row_end: i32,
+    col_start: i32,
+    col_end: i32,
+) -> Vec<char> {
+    let mut sub_box = Vec::<char>::new();
+    for row in row_start..row_end {
+        for col in col_start..col_end {
+            sub_box.push(board[row as usize][col as usize]);
+        }
+    }
+    sub_box
 }
 
 fn has_unique_elements(vec: Vec<char>) -> bool {
