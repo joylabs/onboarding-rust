4b4e1a4ef969c248df6cb7c03aa56c612ca8c7bd
Nuria Reyes <nuria.reyes@joylabs.com>
2019-10-04 19:37:35 -0600

    Adding solution with memoization

--- src/week3/exercise15/mod.rs
+++ src/week3/exercise15/mod.rs
@@ -1,20 +1,26 @@
+use std::collections::HashMap;
+
 pub fn my_pow(x: f64, n: i32) -> f64 {
+    let mut pow_mem: HashMap<i32, f64> = HashMap::new();
+
     if n < 0 {
-        trunc_to_five_decimal_precision(1.0 / pow_to_n(x, n.abs()))
+        trunc_to_five_decimal_precision(1.0 / pow_to_n(x, n.abs(), &mut pow_mem))
     } else {
-        trunc_to_five_decimal_precision(pow_to_n(x, n))
+        trunc_to_five_decimal_precision(pow_to_n(x, n, &mut pow_mem))
     }
 }
 
-fn pow_to_n(base: f64, n: i32) -> f64 {
+fn pow_to_n(base: f64, n: i32, pow_mem: &mut HashMap<i32, f64>) -> f64 {
     if n == 0 {
         1.0
+    } else if pow_mem.contains_key(&n) {
+        *pow_mem.get(&n).unwrap()
     } else if (n % 2) == 0 {
-        let val = pow_to_n(base, n / 2);
-        val * val
+        let val = pow_to_n(base, n / 2, pow_mem);
+        *pow_mem.entry(n).or_insert(val * val)
     } else {
-        let val = pow_to_n(base, n / 2);
-        val * val * base
+        let val = pow_to_n(base, n / 2, pow_mem);
+        *pow_mem.entry(n).or_insert(val * val * base)
     }
 }
 
--- tests/week3/exercise15/mod.rs
+++ tests/week3/exercise15/mod.rs
@@ -36,6 +36,33 @@ fn test_week3_exercise15_example4() {
     assert_eq!(convert_to_i32(expected), convert_to_i32(result));
 }
 
+#[test]
+fn test_week3_exercise15_example5() {
+    let x = 2.00000;
+    let n = 1;
+    let expected: f64 = 2.0;
+    let result = my_pow(x, n);
+    assert_eq!(convert_to_i32(expected), convert_to_i32(result));
+}
+
+#[test]
+fn test_week3_exercise15_example6() {
+    let x = 2.00000;
+    let n = 3;
+    let expected: f64 = 8.0;
+    let result = my_pow(x, n);
+    assert_eq!(convert_to_i32(expected), convert_to_i32(result));
+}
+
+#[test]
+fn test_week3_exercise15_example7() {
+    let x = 8.66731;
+    let n = 4;
+    let expected: f64 = 5643.35434;
+    let result = my_pow(x, n);
+    assert_eq!(convert_to_i32(expected), convert_to_i32(result));
+}
+
 //Using 5 decimal precision
 fn convert_to_i32(x: f64) -> i32 {
     let base = (10.0 as f64).powi(5);
