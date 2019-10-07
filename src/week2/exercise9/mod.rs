b189a0b35ef76aeda65db64a64e9c53adf13ba93
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-10-07 11:16:07 -0500

    Adding solution to is_isomorphic

--- src/week2/exercise9/mod.rs
+++ src/week2/exercise9/mod.rs
@@ -1,1 +1,16 @@
+use std::collections::HashMap;
+pub fn is_isomorphic(s: String, t: String) -> bool {
+    build_map(&s, &t) && build_map(&t, &s)
+}
 
+fn build_map(keys: &str, values: &str) -> bool {
+    let mut map: HashMap<char, char> = HashMap::new();
+    keys.chars().zip(values.chars()).all(|(k, v)| {
+        if !map.contains_key(&k) {
+            map.insert(k, v);
+            true
+        } else {
+            map.get(&k) == Some(&v)
+        }
+    })
+}
