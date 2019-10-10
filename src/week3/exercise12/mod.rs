950eca9d44e974d1193e3c812dda09d99077870e
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-10-10 14:38:10 -0500

    change it to recursive solution

--- src/week3/exercise12/mod.rs
+++ src/week3/exercise12/mod.rs
@@ -3,14 +3,22 @@ pub fn max_profit(prices: Vec<i32>) -> i32 {
         return 0;
     }
     //[7, 1, 5, 3, 6, 4]
-    let mut max = 0;
-    let mut min = *prices.iter().max().unwrap();
+    let max = 0;
+    let min = *prices.iter().max().unwrap();
+    compare(max, min, &prices, 0)
+}
 
-    for v in prices {
-        if min > v {
-            min = v;
-        } else if v - min > max {
-            max = v - min;
-        }
-    }    max
+pub fn compare(max: i32, min: i32, vec: &[i32], position: usize) -> i32 {
+    if position >= vec.len() {
+        return max;
+    }
+    if min > vec[position] {
+        let min = vec[position];
+        compare(max, min, vec, position + 1)
+    } else if vec[position] - min > max {
+        let max = vec[position] - min;
+        compare(max, min, vec, position + 1)
+    } else {
+        compare(max, min, vec, position + 1)
+    }
 }
