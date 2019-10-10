88dc068f7fcb3dc06964ae906b3c15bffbd017be
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-10-10 11:31:24 -0500

    Solution to exericse My HashSet

--- src/week2/exercise12/mod.rs
+++ src/week2/exercise12/mod.rs
@@ -1,1 +1,34 @@
+#[derive(Default)]
+pub struct MyHashSet {
+    elements: Vec<i32>,
+}
 
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl MyHashSet {
+    /** Initialize your data structure here. */
+    pub fn new() -> Self {
+        Self {
+            elements: Vec::new(),
+        }
+    }
+
+    pub fn add(&mut self, key: i32) {
+        if !self.elements.contains(&key) {
+            self.elements.push(key);
+        }
+    }
+
+    pub fn remove(&mut self, key: i32) {
+        if self.elements.contains(&key) {
+            let index = self.elements.iter().position(|&x| x == key).unwrap();
+            self.elements.remove(index);
+        }
+    }
+    /** Returns true if this set contains the specified element */
+    pub fn contains(&self, key: i32) -> bool {
+        self.elements.contains(&key)
+    }
+}
