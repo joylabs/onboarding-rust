c8f109e4af7d0b696fd0c40f871f21f9cf1dce1f
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-10-29 17:07:29 -0600

    Solution to exercise Subdomain Visits

--- src/week2/exercise15/mod.rs
+++ src/week2/exercise15/mod.rs
@@ -1,1 +1,23 @@
+use std::collections::HashMap;
 
+pub fn subdomain_visits(cpdomains: Vec<String>) -> Vec<String> {
+    let mut subdomain_count = HashMap::new();
+
+    cpdomains.iter().for_each(|row| {
+        let split = row.split_whitespace().collect::<Vec<&str>>();
+        let domain_split = split[1].split('.').rev().collect::<Vec<&str>>();
+        let mut tmp = String::from("");
+        domain_split.iter().for_each(|d| {
+            let value = split[0].parse::<i32>().unwrap();
+            let key = format!("{}{}", d, tmp);
+            let counter = subdomain_count.entry(key.clone()).or_insert(0);
+            *counter += value;
+            tmp = format!(".{}", key);
+        });
+    });
+
+    subdomain_count
+        .iter()
+        .map(|(key, value)| format!("{} {}", value.to_string(), key.clone()))
+        .collect()
+}
