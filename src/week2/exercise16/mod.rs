88ac1f92b0d14aefce31f72dd0fc9d978960fb7c
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-10-30 11:38:37 -0600

    Solution to exercise find_the_difference

--- src/week2/exercise16/mod.rs
+++ src/week2/exercise16/mod.rs
@@ -1,1 +1,13 @@
+pub fn find_the_difference(s: String, t: String) -> char {
+    let mut s = s.chars().collect::<Vec<char>>();
+    s.sort();
+    let mut t = t.chars().collect::<Vec<char>>();
+    t.sort();
 
+    for (i, c) in s.iter().enumerate() {
+        if *c != t[i] {
+            return t[i];
+        }
+    }
+    t[t.len() - 1]
+}
