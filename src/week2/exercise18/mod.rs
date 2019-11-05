233911e7c3dbc9e73041c9ec5901a384516c535f
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-11-05 17:32:37 -0600

    Solution to exercise frequency-sort

--- src/week2/exercise18/mod.rs
+++ src/week2/exercise18/mod.rs
@@ -1,1 +1,17 @@
+use std::collections::HashMap;
+pub fn frequency_sort(s: String) -> String {
+    let mut count: HashMap<char, u32> = HashMap::new();
+    s.chars().for_each(|c| *count.entry(c).or_insert(0) += 1);
 
+    let mut count_vec: Vec<(&char, &u32)> = count.iter().collect();
+    count_vec.sort_by(|a, b| b.1.cmp(a.1));
+
+    count_vec.iter().fold(String::new(), |mut acc, (c, n)| {
+        acc = format!("{}{}", acc, return_repeated_char(**c, **n));
+        acc
+    })
+}
+
+fn return_repeated_char(c: char, n: u32) -> String {
+    std::iter::repeat(c).take(n as usize).collect::<String>()
+}
--- tests/week2/exercise18/mod.rs
+++ tests/week2/exercise18/mod.rs
@@ -1,1 +1,26 @@
+use onboarding_rust::week2::exercise18::frequency_sort;
 
+#[test]
+fn test_week2_exercise18_tree() {
+    let input = "tree".to_string();
+    let expected = "eert".to_string();
+    assert!(contains_all(expected, frequency_sort(input)));
+}
+
+#[test]
+fn test_week2_exercise18_cccaaa() {
+    let input = "cccaaa".to_string();
+    let expected = "cccaaa".to_string();
+    assert!(contains_all(expected, frequency_sort(input)));
+}
+
+#[test]
+fn test_week2_exercise18_Aabb() {
+    let input = "Aabb".to_string();
+    let expected = "bbAa".to_string();
+    assert!(contains_all(expected, frequency_sort(input)));
+}
+
+fn contains_all(a: String, b: String) -> bool {
+    a.len() == b.len() && a.chars().all(|x| b.contains(x))
+}
