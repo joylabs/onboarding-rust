bf2adee5824a4ceeca6eb48f234d82899c8f482c
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-11-07 11:09:52 -0600

    Solution to exercise Find error nums

--- src/week2/exercise19/mod.rs
+++ src/week2/exercise19/mod.rs
@@ -1,1 +1,20 @@
+//use itertools::Itertools;
+use std::collections::HashSet;
+pub fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
+    let mut result = vec![];
+    let repeated_number = repeated_number(&nums);
+    for (i, _) in nums.iter().enumerate() {
+        if !nums.contains(&(i as i32 + 1)) {
+            result.push(repeated_number);
+            result.push(i as i32 + 1);
+            break;
+        }
+    }
+    result
+}
 
+fn repeated_number(data: &[i32]) -> i32 {
+    let set_sum: i32 = data.iter().cloned().collect::<HashSet<i32>>().iter().sum();
+    let num_sum: i32 = data.iter().sum();
+    num_sum - set_sum
+}
--- tests/week2/exercise19/mod.rs
+++ tests/week2/exercise19/mod.rs
@@ -1,1 +1,15 @@
+use onboarding_rust::week2::exercise19::find_error_nums;
 
+#[test]
+fn test_week2_exercise18_three() {
+    let input = vec![1, 2, 2, 4];
+    let result = vec![2, 3];
+    assert_eq!(result, find_error_nums(input));
+}
+
+#[test]
+fn test_week2_exercise18_one() {
+    let input = vec![3, 2, 3, 4, 6, 5];
+    let result = vec![3, 1];
+    assert_eq!(result, find_error_nums(input));
+}
