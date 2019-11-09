0ab0bac6206c2cefa96cd3322dabdeb5a2360083
Nuria Reyes <nuria.reyes@joylabs.com>
2019-11-08 20:13:38 -0700

    Adding solution to regular expression matching - dynamic exercise

--- src/week3/exercise19/mod.rs
+++ src/week3/exercise19/mod.rs
@@ -1,34 +1,55 @@
 use std::collections::HashMap;
 
 pub fn is_match(s: String, p: String) -> bool {
-    let mut mem_results: HashMap<(usize, usize), bool> = HashMap::new();
-    check_pattern(0, 0, &s, &p, &mut mem_results)
-}
+    let mut mem_results: HashMap<(i32, i32), bool> = HashMap::new();
+    let mut i = s.len() as i32;
+    mem_results.insert((s.len() as i32, p.len() as i32), true);
 
-fn check_pattern(
-    i: usize,
-    j: usize,
-    s: &str,
-    p: &str,
-    mem_results: &mut HashMap<(usize, usize), bool>,
-) -> bool {
-    if mem_results.contains_key(&(i, j)) {
-        return *(mem_results.get(&(i, j)).unwrap());
-    }
-
-    let result;
-
-    if j == p.len() {
-        result = i == s.len();
-    } else {
-        let first_check = i < s.len() && (p[j..=j] == s[i..=i] || &p[j..=j] == ".");
-        if j + 1 < p.len() && &p[j + 1..=j + 1] == "*" {
-            result = check_pattern(i, j + 2, s, p, mem_results)
-                || (first_check && check_pattern(i + 1, j, s, p, mem_results));
+    while i >= 0 {
+        let mut j = if p.is_empty() {
+            -1
         } else {
-            result = first_check && check_pattern(i + 1, j + 1, s, p, mem_results);
+            (p.len() - 1) as i32
+        };
+
+        while j >= 0 {
+            let first_check = (i as usize) < s.len()
+                && p[j as usize..=j as usize] == s[i as usize..=i as usize]
+                || &p[j as usize..=j as usize] == ".";
+
+            if ((j + 1) as usize) < p.len() && &p[(j + 1) as usize..=(j + 1) as usize] == "*" {
+                let r1 = if mem_results.contains_key(&(i, j + 2)) {
+                    *(mem_results.get(&(i, j + 2)).unwrap())
+                } else {
+                    false
+                };
+
+                let r2 = if mem_results.contains_key(&(i + 1, j)) {
+                    *(mem_results.get(&(i + 1, j)).unwrap())
+                } else {
+                    false
+                };
+
+                let result = r1 || first_check && r2;
+                mem_results.insert((i, j), result);
+            } else {
+                let r1 = if mem_results.contains_key(&(i + 1, j + 1)) {
+                    *(mem_results.get(&(i + 1, j + 1)).unwrap())
+                } else {
+                    false
+                };
+
+                let result = first_check && r1;
+                mem_results.insert((i, j), result);
+            }
+            j -= 1;
         }
+        i -= 1;
     }
-    mem_results.insert((i, j), result);
-    result
+
+    if mem_results.contains_key(&(0, 0)) {
+        *(mem_results.get(&(0, 0)).unwrap())
+    } else {
+        false
+    }
 }
