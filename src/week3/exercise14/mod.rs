d2d2cb78a1c52f5621440f45b7e78bbf05608787
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-11-11 09:52:29 -0600

    fixed clippy errors

--- src/week3/exercise14/mod.rs
+++ src/week3/exercise14/mod.rs
@@ -6,17 +6,17 @@ pub fn my_pow(mut x: f64, mut n: i64) -> f64 {
         return 1.0;
     } else {
         if !is_negative {
-            n = n * -1;
+            n *= -1;
         }
         while n > 0 {
             if n % 2 != 0 {
-                result = result * x;
+                result *= x;
             }
             x = x * x;
-            n = n / 2;
+            n /= 2;
         }
         if !is_negative {
-            result = 1 as f64 / result;
+            result = 1_f64 / result;
         }
     }
     result
