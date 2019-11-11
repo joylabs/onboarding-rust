2691f89e2152741036478b439ebf53f67d26bb39
Nuria Reyes <nuria.reyes@joylabs.com>
2019-11-11 16:41:20 -0700

    Adding solution to edit distance - dynamic exercise

--- src/week3/exercise17/mod.rs
+++ src/week3/exercise17/mod.rs
@@ -2,43 +2,29 @@ use std::collections::HashMap;
 
 pub fn min_distance(word1: String, word2: String) -> i32 {
     let mut mem_matches: HashMap<(usize, usize), i32> = HashMap::new();
-    find_best_pattern(0, 0, &word1, &word2, &mut mem_matches)
-}
+    // Base cases
+    (0..=word1.len()).for_each(|i| {
+        mem_matches.insert((i, 0), i as i32);
+    });
+    (0..=word2.len()).for_each(|j| {
+        mem_matches.insert((0, j), j as i32);
+    });
 
-fn find_best_pattern(
-    i: usize,
-    j: usize,
-    word1: &str,
-    word2: &str,
-    mem_matches: &mut HashMap<(usize, usize), i32>,
-) -> i32 {
-    println!("i: {}, j: {}", i, j);
-    if i == word1.len() && j == word2.len() {
-        println!("base");
-        0
-    } else if mem_matches.contains_key(&(i, j)) {
-        println!("memo");
-        *mem_matches.get(&(i, j)).unwrap()
-    } else if i == word1.len() {
-        println!("end of word1");
-        1 + find_best_pattern(i, j + 1, word1, word2, mem_matches)
-    } else if j == word2.len() {
-        println!("end of word2");
-        1 + find_best_pattern(i + 1, j, word1, word2, mem_matches)
-    } else if word1[i..=i] == word2[j..=j] {
-        println!("equal");
-        find_best_pattern(i + 1, j + 1, word1, word2, mem_matches)
-    } else {
-        println!("not the end of any word, not equal");
-        let mut possible_changes: Vec<i32> = Vec::with_capacity(3);
-        possible_changes.push(1 + find_best_pattern(i, j + 1, word1, word2, mem_matches));
-        possible_changes.push(1 + find_best_pattern(i + 1, j, word1, word2, mem_matches));
-        possible_changes.push(1 + find_best_pattern(i + 1, j + 1, word1, word2, mem_matches));
+    (1..=word1.len()).for_each(|i| {
+        (1..=word2.len()).for_each(|j| {
+            if word1[(i - 1)..i] == word2[(j - 1)..j] {
+                let prev_val = *mem_matches.get(&(i - 1, j - 1)).unwrap();
+                mem_matches.insert((i, j), prev_val);
+            } else {
+                let mut possible_changes: Vec<i32> = Vec::with_capacity(3);
+                possible_changes.push(1 + mem_matches.get(&(i, j - 1)).unwrap());
+                possible_changes.push(1 + mem_matches.get(&(i - 1, j)).unwrap());
+                possible_changes.push(1 + mem_matches.get(&(i - 1, j - 1)).unwrap());
 
-        println!("({}, {}) - possible changes: {:?}", i, j, possible_changes);
+                mem_matches.insert((i, j), *possible_changes.iter().min().unwrap());
+            }
+        });
+    });
 
-        *mem_matches
-            .entry((i, j))
-            .or_insert(*possible_changes.iter().min().unwrap())
-    }
+    *mem_matches.entry((word1.len(), word2.len())).or_insert(0)
 }
