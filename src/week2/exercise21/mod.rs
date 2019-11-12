ce962cd0264e999e9879ccb844d4cacff23c2d73
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-11-12 16:18:38 -0600

    Solution to exericse longest-word

--- src/week2/exercise21/mod.rs
+++ src/week2/exercise21/mod.rs
@@ -1,6 +1,20 @@
+use std::collections::HashSet;
 pub fn longest_word(words: Vec<String>) -> String {
-    let mut test = words.clone();
-    test.sort();
-    println!("{:?}", test);
-    "test".to_string()
+    let words2 = words.clone();
+    let word_set: HashSet<String> = words2.into_iter().collect();
+    let mut result = "".to_string();
+    words.iter().for_each(|word| {
+        let mut build_word = word.clone();
+        while word_set.contains(&build_word) {
+            build_word.pop();
+        }
+
+        if build_word.is_empty()
+            && (word.len() > result.len()
+                || (word.len() == result.len() && word.as_bytes() < result.as_bytes()))
+        {
+            result = word.to_string();
+        }
+    });
+    result
 }
