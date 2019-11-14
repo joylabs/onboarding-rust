c68db50627f392147e090fac8a38e5b8f52f90db
Miguel Bovadilla <miguel.bovadilla@joylabs.com>
2019-11-14 00:02:16 -0600

    Find Difference with Functional Approach

--- src/week1/exercise39/mod.rs
+++ src/week1/exercise39/mod.rs
@@ -3,10 +3,17 @@ pub fn find_the_difference(s: String, t: String) -> char {
     s.sort();
     let mut t = t.chars().collect::<Vec<char>>();
     t.sort();
-    for (i, c) in s.iter().enumerate() {
-        if *c != t[i] {
-            return t[i];
-        }
+
+    let result = s
+        .iter()
+        .zip(t.iter())
+        .skip_while(|(a, b)| a == b)
+        .collect::<Vec<(&char, &char)>>();
+
+    if result.is_empty() {
+        *t.last().unwrap()
+    } else {
+        *result[0].1
     }
-    t[t.len() - 1]
+
 }
