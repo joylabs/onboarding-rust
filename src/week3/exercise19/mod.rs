7a58efbdee0e2aba9b1a65a2913aac194d6abddd
Nuria Reyes <nuria.reyes@joylabs.com>
2019-11-19 16:17:52 -0700

    Refactoring code to make it more readable

--- src/week3/exercise19/mod.rs
+++ src/week3/exercise19/mod.rs
@@ -3,53 +3,39 @@ use std::collections::HashMap;
 pub fn is_match(s: String, p: String) -> bool {
     let mut mem_results: HashMap<(i32, i32), bool> = HashMap::new();
     let mut i = s.len() as i32;
-    mem_results.insert((s.len() as i32, p.len() as i32), true);
+    mem_results.insert((i, p.len() as i32), true);
 
     while i >= 0 {
-        let mut j = if p.is_empty() {
-            -1
-        } else {
-            (p.len() - 1) as i32
-        };
+        let mut previous_p_char = '-';
 
-        while j >= 0 {
-            let first_check = (i as usize) < s.len()
-                && p[j as usize..=j as usize] == s[i as usize..=i as usize]
-                || &p[j as usize..=j as usize] == ".";
+        p.char_indices().rev().for_each(|(j, p_char)| {
+            let first_check = (i as usize) < s.len() && p_char == get_char(i, &s) || p_char == '.';
 
-            if ((j + 1) as usize) < p.len() && &p[(j + 1) as usize..=(j + 1) as usize] == "*" {
-                let r1 = if mem_results.contains_key(&(i, j + 2)) {
-                    *(mem_results.get(&(i, j + 2)).unwrap())
-                } else {
-                    false
-                };
+            let result = if previous_p_char == '*' {
+                let r1 = check_mem(i, j + 2, &mem_results);
+                let r2 = check_mem(i + 1, j, &mem_results);
 
-                let r2 = if mem_results.contains_key(&(i + 1, j)) {
-                    *(mem_results.get(&(i + 1, j)).unwrap())
-                } else {
-                    false
-                };
-
-                let result = r1 || first_check && r2;
-                mem_results.insert((i, j), result);
+                r1 || first_check && r2
             } else {
-                let r1 = if mem_results.contains_key(&(i + 1, j + 1)) {
-                    *(mem_results.get(&(i + 1, j + 1)).unwrap())
-                } else {
-                    false
-                };
+                let r1 = check_mem(i + 1, j + 1, &mem_results);
 
-                let result = first_check && r1;
-                mem_results.insert((i, j), result);
-            }
-            j -= 1;
-        }
+                first_check && r1
+            };
+
+            mem_results.insert((i, j as i32), result);
+
+            previous_p_char = p_char;
+        });
         i -= 1;
     }
 
-    if mem_results.contains_key(&(0, 0)) {
-        *(mem_results.get(&(0, 0)).unwrap())
-    } else {
-        false
-    }
+    check_mem(0, 0, &mem_results)
+}
+
+fn check_mem(i: i32, j: usize, mem: &HashMap<(i32, i32), bool>) -> bool {
+    mem.contains_key(&(i, j as i32)) && *(mem.get(&(i, j as i32)).unwrap())
+}
+
+fn get_char(i: i32, s: &str) -> char {
+    s.chars().collect::<Vec<char>>()[i as usize]
 }
