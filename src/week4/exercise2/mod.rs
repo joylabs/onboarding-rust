386f272ad52cc3db297780708bf0527993e91ddd
Nuria Reyes <nuria.reyes@joylabs.com>
2019-11-21 23:14:37 -0700

    Adding more work on exercise (not done yet)

--- src/week4/exercise2/mod.rs
+++ src/week4/exercise2/mod.rs
@@ -6,28 +6,58 @@ pub struct ListNode {
 }
 
 pub fn reverse_between(head: Option<Box<ListNode>>, m: i32, n: i32) -> Option<Box<ListNode>> {
-    let mut previous = head.clone();
-    let mut current = head.unwrap().next;
-    let mut current_position = 1;
+    let mut start = head.clone();
+    let mut previous_start: Option<Box<ListNode>> = None;
+
+    for _ in 0..(m - 1) {
+        previous_start = start.clone();
+        start = start.unwrap().next;
+    }
+
+    let mut end = start.clone();
+    let mut previous_end: Option<Box<ListNode>> = None;
+    let mut tail: Option<Box<ListNode>> = None;
+
+    for _ in m..=n {
+        let mut node = end.unwrap();
+        let following = node.next;
+        tail = following.clone();
+        node.next = previous_end;
+        previous_end = Some(node);
+        end = following;
+    }
+
+    // let mut i = 1;
+    // let mut head  = head;
+    // while head.is_some() {
+
+    //     if i == m {
+
+    //     }
+
+    //     if i == n {
+
+    //     }
+
+    //     i += 1;
+    // }
+
+    println!("previous_end: {:?}, tail: {:?}", previous_end, tail);
+
+    None
+}
+
+fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut previous: Option<Box<ListNode>> = None;
+    let mut current = head;
 
     while current.is_some() {
-        if current_position >= m && current_position < n {
-            let mut node = current.unwrap();
-            let following = node.next;
-            node.next = previous;
-            previous = Some(node);
-            current = following;
-            println!("current_position >= m");
-        } else {
-            let node = current.unwrap();
-            let following = node.next.clone();
-            previous = Some(node);
-            current = following;
-        }
-
-        current_position += 1;
-        println!("--> prev: {:?}", previous.clone());
+        let mut node = current.unwrap();
+        let following = node.next;
+        node.next = previous;
+        previous = Some(node);
+        current = following;
     }
-    
+
     previous
 }
