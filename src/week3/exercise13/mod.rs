efca37a2c33f1ed61e4055f3677194fe76d3623d
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-12-02 14:40:33 -0600

    refactor solution

--- src/week3/exercise13/mod.rs
+++ src/week3/exercise13/mod.rs
@@ -1,5 +1,4 @@
 use std::cmp;
-use std::collections::HashMap;
 
 pub fn rob(nums: Vec<i32>) -> i32 {
     let len = nums.len();
@@ -10,38 +9,18 @@ pub fn rob(nums: Vec<i32>) -> i32 {
     if len == 1 {
         return nums[0];
     }
-    if len == 2 {
-        return cmp::max(nums[0], nums[1]);
-    }
 
-    let mut map: HashMap<usize, i32> = HashMap::new();
-
-    // with the first house
-    let a = rob_dp(0, &nums[1..], &mut map);
-    let b = rob_dp(1, &nums[1..], &mut map);
-    let first_max = cmp::max(a, b);
-    map.clear();
-    // without the first house
-    let a = rob_dp(0, &nums[..len - 1], &mut map);
-    let b = rob_dp(1, &nums[..len - 1], &mut map);
-    let second_max = cmp::max(a, b);
-
-    cmp::max(first_max, second_max)
+    cmp::max(max_rob(&nums[1..]), max_rob(&nums[..len - 1]))
 }
 
-fn rob_dp(item: usize, vec: &[i32], map: &mut HashMap<usize, i32>) -> i32 {
-    if map.get(&item).is_some() {
-        return *map.get(&item).unwrap();
+pub fn max_rob(nums: &[i32]) -> i32 {
+    let mut memo = vec![];
+    memo.push(nums[0]);
+    memo.push(cmp::max(nums[0], nums[1]));
+
+    for i in 2..nums.len() {
+        let sum = nums[i] + memo[i - 2];
+        memo.push(cmp::max(sum, memo[i - 1]));
     }
-    let mut a = vec[item];
-    let mut b = vec[item];
-    if item + 2 < vec.len() {
-        a += rob_dp(item + 2, vec, map);
-    }
-    if item + 3 < vec.len() {
-        b += rob_dp(item + 3, vec, map);
-    }
-    let max = cmp::max(a, b);
-    map.insert(item, max);
-    max
+    memo.pop().unwrap_or(0)
 }
