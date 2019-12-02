26fbef401d939836459433bf1abceb0cab1f416c
citlalli hernandez <citlallihernandez@MacBook-Pro-de-citlalli.local>
2019-12-02 15:14:40 -0600

    change line 13 to a new function

--- src/week3/exercise15/mod.rs
+++ src/week3/exercise15/mod.rs
@@ -7,23 +7,26 @@ pub fn is_match(s: &str, p: &str) -> bool {
 		.filter(|(_, ch)| *ch == '*')
 		.for_each(|(i, _)| matrix[0][i + 1] = matrix[0][i - 1]);
 
-	let mut befor: char = '\0';
-
 	s.chars().enumerate().for_each(|(i, a)| {
-		p.chars().enumerate().for_each(|(j, b)| {
-			if !is_matching(a, b, &mut matrix, befor, i, j) {
-				matrix[i + 1][j + 1] = false;
-			}
-			befor = b;
-		})
+		build_matrix(&mut matrix, p, a, i);
 	});
 	matrix.pop().unwrap().pop().unwrap()
+}
+
+fn build_matrix(map: &mut [Vec<bool>], p: &str, a: char, i: usize) {
+	let mut befor: char = '\0';
+	p.chars().enumerate().for_each(|(j, b)| {
+		if !is_matching(a, b, map, befor, i, j) {
+			map[i + 1][j + 1] = false;
+		}
+		befor = b;
+	})
 }
 
 fn is_matching(
 	a: char,
 	b: char,
-	matrix: &mut Vec<Vec<bool>>,
+	matrix: &mut [Vec<bool>],
 	befor: char,
 	i: usize,
 	j: usize,
