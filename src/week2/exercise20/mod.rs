fbcdd88ce8b35ad31abbe5f82f2ed7e3cd0e13cb
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-12-04 11:44:39 -0600

    finishing exercise 20

--- src/week2/exercise20/mod.rs
+++ src/week2/exercise20/mod.rs
@@ -1,10 +1,182 @@
 use std::collections::HashMap;
 
 pub fn count_of_atoms(formula: String) -> String {
-    let element_count = build_map(formula.chars().collect());
+    let new_formula = add_ones(formula);
+    let formula_vec = to_vec(new_formula);
+    let ranges = build_ranges(&formula_vec);
+    let final_formula = multiply_coefficient(formula_vec, ranges);
+    let element_count = build_map(final_formula);
+    order_formula(element_count)
+}
+
+fn add_ones(formula: String) -> String {
+    let formula_vec: Vec<char> = formula.chars().collect();
+
+    formula
+        .chars()
+        .enumerate()
+        .fold(String::new(), |mut acc, (i, c)| {
+            if c.is_ascii_digit() || c == '(' || c == ')' {
+                acc = format!("{}{}", acc, c);
+            } else if (i + 1 < formula_vec.len()
+                && (!formula_vec[i + 1].is_ascii_digit()
+                    && !formula_vec[i + 1].is_ascii_lowercase()))
+                || (i == formula_vec.len() - 1
+                    && (c.is_ascii_lowercase() || c.is_ascii_uppercase()))
+            {
+                acc = format!("{}{}{}", acc, c, "1");
+            } else {
+                acc = format!("{}{}", acc, c);
+            }
+            acc
+        })
+}
+
+fn to_vec(formula: String) -> Vec<String> {
+    let mut formula_vec: Vec<String> = Vec::new();
+    // iterate over formula
+    let mut waiting = true;
+    let mut state = "none";
+    let mut handle = String::new();
+    for c in formula.chars() {
+        // Two states: waiting or not
+        // Waiting: needs more characters, cannot push
+        if !waiting {
+            formula_vec.push(handle);
+            waiting = true;
+            handle = String::new();
+        }
+        // not waiting: ready to push
+        // Identify symbols
+        // Parentheses, no need to wait for more characters
+        if c == '(' || c == ')' {
+            // Push first what we have in the handle
+            if handle != "" {
+                formula_vec.push(handle);
+                handle = String::new();
+            }
+            handle = c.to_string();
+            waiting = false;
+        }
+        // Numbers, need to wait for more numbers
+        if c.is_digit(10) {
+            // check state
+            if state != "digit" && handle != "" {
+                formula_vec.push(handle);
+                handle = String::new();
+            }
+            handle = format!("{}{}", handle, c);
+            waiting = true;
+            state = "digit";
+        }
+        // Letters, need to wait
+        if c.is_ascii_uppercase() {
+            // check state (from another symbol)
+            // println!("{} is UpperCase", c);
+            if state != "upper" && handle != "" {
+                formula_vec.push(handle);
+                handle = String::new();
+            }
+            // check state (from the same symbol)
+            if waiting && state == "upper" && handle != "" {
+                formula_vec.push(handle.clone());
+                waiting = true;
+                handle = c.to_string();
+            } else {
+                handle = format!("{}{}", handle, c);
+                waiting = true;
+                state = "upper";
+            }
+            // println!("Current handle: {:?}", handle);
+        }
+        // lowercase
+        if c.is_ascii_lowercase() {
+            handle = format!("{}{}", handle, c);
+            waiting = false;
+            state = "lower";
+        }
+    }
+    formula_vec.push(handle);
+    formula_vec
+}
+
+fn build_ranges(formula_vec: &[String]) -> Vec<((usize, usize), u32)> {
+    // [((2, 18), 2), ((5, 11), 2), ((13, 16), 3)]
+    let mut parentheses_ranges: Vec<((usize, usize), u32)> = Vec::new();
+    //let formula_vec: Vec<char> = formula.chars().collect();
+    for (i, c) in formula_vec.iter().enumerate() {
+        if c == "(" {
+            parentheses_ranges.push(((i, 0), 0));
+        }
+        if c == ")" {
+            for range in parentheses_ranges.iter_mut().rev() {
+                if (range.0).1 == 0 {
+                    (range.0).1 = i;
+                    range.1 = formula_vec[i + 1].parse::<u32>().unwrap();
+                    break;
+                }
+            }
+        }
+    }
+
+    parentheses_ranges
+}
+
+fn multiply_coefficient(
+    formula: Vec<String>,
+    parentheses_ranges: Vec<((usize, usize), u32)>,
+) -> Vec<String> {
+    // K4(O1N1(Mg2O3)2(Rg1)3)2
+    // [((2, 21), 2), ((7, 13), 2), ((15, 19), 3)]
+    //(N1B3)33 --[((0, 5), 33)]
+    let mut result: Vec<String> = vec![];
+    for (i, c) in formula.iter().enumerate() {
+        let mut multiplication = 0;
+        // Check if "c" is a number
+        if c.parse::<u32>().is_ok() {
+            // compare "i" to ranges
+            for r in parentheses_ranges.iter() {
+                // ( (izq, der), n)
+                if i >= (r.0).0 && i <= (r.0).1 {
+                    // Multiply "c" by "n"
+                    if multiplication == 0 {
+                        multiplication += c.parse::<u32>().unwrap() * r.1;
+                    } else {
+                        multiplication *= r.1;
+                    }
+                }
+            }
+        }
+        // Push char to Vec<&str>
+        // K, (), multiplication > 0 ? multiplication.to_string : c
+        if multiplication > 0 {
+            result.push(multiplication.to_string());
+        } else {
+            result.push(c.to_string());
+        }
+    }
+
+    result
+        .iter()
+        .enumerate()
+        .filter(|(i, c)| !(c.parse::<u32>().is_ok() && (*i as i32 > 1 && result[i - 1] == ")")))
+        .filter(|(_, c)| (*c != "(" && *c != ")"))
+        .map(|(_, c)| c.to_string())
+        .collect::<Vec<String>>()
+}
+
+fn build_map(vec: Vec<String>) -> HashMap<String, u32> {
+    let mut i = 1;
+    vec.iter().step_by(2).fold(HashMap::new(), |mut acc, s| {
+        *acc.entry(s.to_string()).or_insert(0) += vec[i].parse::<u32>().unwrap();
+        i += 2;
+        acc
+    })
+}
+
+fn order_formula(element_count: HashMap<String, u32>) -> String {
     let mut formula_vec = element_count.into_iter().collect::<Vec<(String, u32)>>();
     formula_vec.sort_by(|a, b| a.0.cmp(&b.0));
-    println!("{:?}", formula_vec);
     formula_vec
         .iter()
         .map(|(element, count)| {
@@ -15,70 +187,4 @@ pub fn count_of_atoms(formula: String) -> String {
             }
         })
         .collect::<String>()
-}
-
-fn remove_parentheses(formula: String) -> String {
-    // Mg3(O2H5)2 -> Mg3H2O2
-    // K4(O1N(Mg2O3)2(Rg)3)2 -> K4O2N2Mg8O12Rg6
-    //[((2,17), 2) , ((5,10), 3), ((12, 15),2)] = range : Vec<((usize, usize), i32)>
-    // range.inter.map(|(i,f), c)| { let x = &fomula[i..f] ->  O1N(Mg2O3)2(Rg)3 ->  O2N(Mg4O6)2(Rg)3 })
-    let mut parentheses_ranges: Vec<((usize, usize), u32)>;
-    for (i, c) in formula.chars().enumerate() {
-        if c == '(' {
-            parentheses_ranges.push(((i, 0), 0));
-        }
-        if c == ')' {
-            for range in parentheses_ranges.iter().rev() {
-                // [ (0,1), x ]
-                if range.0.1 == 0 {
-
-                }
-            }
-        }
-    }
-
-    let mut element = String::new();
-    let mut is_upper = false;
-    formula
-        .chars()
-        .enumerate()
-        .map(|(i, c)| {
-            if c.is_ascii_digit() {
-                let val = c.to_digit(10).unwrap();
-            } else {
-                c
-            }
-        })
-        .collect::<String>()
-}
-
-fn build_map(vec: Vec<char>) -> HashMap<String, u32> {
-    let mut element_count = HashMap::new();
-    let mut element = String::new();
-    let mut is_upper = false;
-    vec.iter().enumerate().for_each(|(i, c)| {
-        if !is_upper || c.is_ascii_lowercase() {
-            if c.is_ascii_uppercase() {
-                element = format!("{}{}", element, c);
-                is_upper = true;
-            } else if c.is_ascii_lowercase() {
-                element = format!("{}{}", element, c);
-            }
-        }
-        if c.is_ascii_digit()
-            || (i + 1 < vec.len() && vec[i + 1].is_ascii_uppercase())
-            || (i == vec.len() - 1)
-        {
-            let count = if c.is_ascii_digit() {
-                // agregar mas de un digito ex. 12
-                c.to_digit(10).unwrap()
-            } else {
-                1
-            };
-            *element_count.entry(element.clone()).or_insert(0) += count;
-            element = "".to_string();
-            is_upper = false;
-        }
-    });
-    element_count
 }
--- tests/week2/exercise20/mod.rs
+++ tests/week2/exercise20/mod.rs
@@ -1,43 +1,43 @@
 use onboarding_rust::week2::exercise20::count_of_atoms;
 
 #[test]
-fn test_week2_exercise20_no_parentheses() {
-    let input = "Mg2K4ONSO3".to_string();
-    let output = "K4Mg2NO4S".to_string();
+fn test_week2_exercise20_test_week2_exercise20_test() {
+    let input = "Mg((H2Mg)334)1".to_string();
+    let output = "H668Mg335".to_string();
     assert_eq!(output, count_of_atoms(input));
 }
 
 #[test]
-fn test_week2_exercise20_no_num() {
-    let input = "MgKNSO".to_string();
-    let output = "KMgNOS".to_string();
+fn test_week2_exercise20_test_week2_exercise20_k4_mg20_n5_o35_rg15() {
+    let input = "K4(ON(Mg2O3)2(Rg)3)5".to_string();
+    let output = "K4Mg20N5O35Rg15".to_string();
     assert_eq!(output, count_of_atoms(input));
 }
 
 #[test]
-fn test_week2_exercise20_parentheses() {
+fn test_week2_exercise20_test_week2_exercise20_h2_o() {
+    let input = "H2O".to_string();
+    let output = "H2O".to_string();
+    assert_eq!(output, count_of_atoms(input));
+}
+
+#[test]
+fn test_week2_exercise20_test_week2_exercise20_mg_oh2() {
     let input = "Mg(OH)2".to_string();
     let output = "H2MgO2".to_string();
     assert_eq!(output, count_of_atoms(input));
 }
 
-// #[test]
-// fn test_week2_exercise20_H2O() {
-//     let input = "H2O".to_string();
-//     let output = "H2O".to_string();
-//     assert_eq!(output, count_of_atoms(input));
-// }
+#[test]
+fn test_week2_exercise20_test_week2_exercise20_k4_onso322() {
+    let input = "K4(ON(SO3)2)2".to_string();
+    let output = "K4N2O14S4".to_string();
+    assert_eq!(output, count_of_atoms(input));
+}
 
-// #[test]
-// fn test_week2_exercise20_MgOH2() {
-//     let input = "Mg(OH)2".to_string();
-//     let output = "H2MgO2".to_string();
-//     assert_eq!(output, count_of_atoms(input));
-// }
-
-// #[test]
-// fn test_week2_exercise20_K4ONSO322() {
-//     let input = "K4(ON(SO3)2)2".to_string();
-//     let output = "K4N2O14S4".to_string();
-//     assert_eq!(output, count_of_atoms(input));
-// }
+#[test]
+fn test_week2_exercise20_test_week2_exercise20_nb333() {
+    let input = "(NB3)33".to_string();
+    let output = "B99N33".to_string();
+    assert_eq!(output, count_of_atoms(input));
+}
